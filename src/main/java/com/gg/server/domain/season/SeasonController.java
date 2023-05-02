736a2c0d14af695baa6fbc7299b819a1a8a6d113
AYoungSn <dudtjs0920@naver.com>
2023-05-02 15:31:39 +0900

    [fix] GGBE-15 response error fix

--- src/main/java/com/gg/server/domain/season/SeasonController.java
+++ src/main/java/com/gg/server/domain/season/SeasonController.java
@@ -1,17 +1,18 @@
 package com.gg.server.domain.season;
 
 import com.gg.server.domain.season.dto.SeasonListResDto;
-import com.gg.server.domain.season.dto.SeasonResDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.*;
-@RestController("/pingpong")
+
 @RequiredArgsConstructor
+@RestController
+@RequestMapping("/pingpong/")
 public class SeasonController {
 
     private final SeasonService seasonService;
-    @GetMapping("/seasons")
+    @GetMapping("seasons")
     public SeasonListResDto seasonList() {
         return new SeasonListResDto(seasonService.seasonList());
     }
