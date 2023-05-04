41ecc08e3f56bec32b6630801e10e9a434fdc0c8
AYoungSn <dudtjs0920@naver.com>
2023-05-04 20:29:09 +0900

    [fix] GGBE-57 error handling fix

--- src/main/java/com/gg/server/domain/game/GameController.java
+++ src/main/java/com/gg/server/domain/game/GameController.java
@@ -4,10 +4,14 @@ import com.gg.server.domain.game.dto.req.GameListReqDto;
 import com.gg.server.domain.game.dto.req.NormalGameListReqDto;
 import com.gg.server.domain.game.dto.GameListResDto;
 import com.gg.server.domain.game.dto.req.RankGameListReqDto;
+import com.gg.server.global.exception.ErrorCode;
+import com.gg.server.global.exception.custom.InvalidParameterException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import org.springframework.validation.BindException;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,7 +20,10 @@ public class GameController {
     private final GameService gameService;
 
     @GetMapping
-    GameListResDto allGameList(@ModelAttribute @Valid GameListReqDto gameReq) {
+    GameListResDto allGameList(@ModelAttribute @Valid GameListReqDto gameReq) throws BindException {
+        if (gameReq.getStatus() != null && !gameReq.getStatus().name().equals("LIVE")) {
+            throw new InvalidParameterException("status not valid", ErrorCode.VALID_FAILED);
+        }
         return gameService.allGameList(gameReq.getPageNum(), gameReq.getPageSize(), gameReq.getStatus());
     }
     @GetMapping("/normal")
--- src/main/java/com/gg/server/global/exception/ErrorCode.java
+++ src/main/java/com/gg/server/global/exception/ErrorCode.java
@@ -13,9 +13,13 @@ public enum ErrorCode {
     INTERNAL_SERVER_ERR(500, "COMMON-ERR-500","INTERNAL SERVER ERROR"),
     //400 잘못된 요청 코드
     BAD_REQUEST(400, "COMMON-ERR-400", "BAD REQUEST"),
-    INVALID_STATUS(400, "GAME-ERR-400" , "허용되지 않는 status 값입니다.")
+    VALID_FAILED(400, "GAME-ERR-400" , "Valid Test Failed.")
     ;
     private int status;
     private String errCode;
     private String message;
+
+    public void setMessage(String msg) {
+        this.message = msg;
+    }
 }
