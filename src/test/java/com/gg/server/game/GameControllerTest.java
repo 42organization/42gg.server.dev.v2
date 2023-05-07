9f234c80c83e97021352286f26244babf84f1759
AYoungSn <dudtjs0920@naver.com>
2023-05-07 18:41:56 +0900

    [test] GGBE-18 rank 게임 결과 등록

--- src/test/java/com/gg/server/game/GameControllerTest.java
+++ src/test/java/com/gg/server/game/GameControllerTest.java
@@ -2,26 +2,39 @@ package com.gg.server.game;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gg.server.domain.game.GameService;
+import com.gg.server.domain.game.data.Game;
 import com.gg.server.domain.game.data.GameRepository;
 import com.gg.server.domain.game.dto.GameListResDto;
+import com.gg.server.domain.game.dto.req.RankResultReqDto;
+import com.gg.server.domain.game.type.Mode;
+import com.gg.server.domain.game.type.StatusType;
+import com.gg.server.domain.season.data.Season;
+import com.gg.server.domain.season.data.SeasonRepository;
+import com.gg.server.domain.team.data.Team;
+import com.gg.server.domain.team.data.TeamRepository;
+import com.gg.server.domain.team.data.TeamUser;
+import com.gg.server.domain.team.data.TeamUserRepository;
+import com.gg.server.domain.user.User;
+import com.gg.server.domain.user.type.RacketType;
+import com.gg.server.domain.user.type.RoleType;
+import com.gg.server.domain.user.type.SnsType;
 import com.gg.server.global.security.jwt.utils.AuthTokenProvider;
 import com.gg.server.utils.TestDataUtils;
 import lombok.RequiredArgsConstructor;
 import org.apache.http.HttpHeaders;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -30,6 +43,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class GameControllerTest {
     @Autowired
     GameRepository gameRepository;
+    @Autowired
+    SeasonRepository seasonRepository;
+
+    @Autowired
+    TeamRepository teamRepository;
+    @Autowired
+    TeamUserRepository teamUserRepository;
     @Autowired
     TestDataUtils testDataUtils;
     @Autowired
@@ -123,5 +143,33 @@ public class GameControllerTest {
         mockMvc.perform(get(url).header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken))
                 .andExpect(status().isBadRequest())
                 .andReturn().getResponse().getContentAsString();
+    }
+
+    @Test
+    void 랭크게임결과입력테스트() throws Exception {
+        String url = "/pingpong/games/rank";
+        // season 추가
+        Season season = seasonRepository.getById(1L);
+        Game game = gameRepository.save(new Game(season, StatusType.WAIT, Mode.RANK, LocalDateTime.now().minusMinutes(15), LocalDateTime.now()));
+        Team team1 = teamRepository.save(new Team(game, -1, false));
+        Team team2 = teamRepository.save(new Team(game, -1, false));
+        User user1 = testDataUtils.createNewUser("test1", "test1@email", "null1", RacketType.NONE, SnsType.EMAIL, RoleType.USER);
+        String ac1 = tokenProvider.createToken(user1.getId());
+        User user2 = testDataUtils.createNewUser("test2", "test2@email", "null1", RacketType.NONE, SnsType.EMAIL, RoleType.USER);
+        String ac2 = tokenProvider.createToken(user2.getId());
+        TeamUser tu1 = teamUserRepository.save(new TeamUser(team1, user1));
+        TeamUser tu2 = teamUserRepository.save(new TeamUser(team2, user2));
+        String content = objectMapper.writeValueAsString(new RankResultReqDto(game.getId(), team1.getId(), 1, team2.getId(), 2));
+        mockMvc.perform(post(url).header(HttpHeaders.AUTHORIZATION, "Bearer " + ac1)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andExpect(status().isCreated())
+                .andReturn().getResponse();
+        content = objectMapper.writeValueAsString(new RankResultReqDto(game.getId(), team2.getId(), 2, team1.getId(), 1));
+        mockMvc.perform(post(url).header(HttpHeaders.AUTHORIZATION, "Bearer " + ac2)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andExpect(status().isCreated())
+                .andReturn().getResponse();
     }
 }
