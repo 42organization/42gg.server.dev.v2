898b02cb8844820670e1d058a9bc76a786e0207c
kmularise <kmula222@gmail.com>
2023-05-10 01:24:44 +0900

    [feat] GGBE-28 add query to repo

--- src/main/java/com/gg/server/domain/rank/redis/RankRedisService.java
+++ src/main/java/com/gg/server/domain/rank/redis/RankRedisService.java
@@ -1,7 +1,12 @@
 package com.gg.server.domain.rank.redis;
 
+import com.gg.server.domain.season.data.SeasonRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class RankRedisService {
+    private final RankRedisRepository rankRedisRepository;
+
 }
