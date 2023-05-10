cb8f37c27433467cdac70d4d2d0bc015cfaebab4
kmularise <kmula222@gmail.com>
2023-05-10 10:59:09 +0900

    [test] add profile only in local

--- src/test/java/com/gg/server/domain/match/service/MatchRedisServiceTest.java
+++ src/test/java/com/gg/server/domain/match/service/MatchRedisServiceTest.java
@@ -33,11 +33,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @Transactional
 @AutoConfigureMockMvc
+@ActiveProfiles("local")
 @RequiredArgsConstructor
 class MatchRedisServiceTest {
     @Autowired
