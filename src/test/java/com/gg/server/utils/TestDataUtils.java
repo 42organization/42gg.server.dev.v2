712ff23a8079a9c1011a4a60bcc2f6a807f9d600
kimjaehyuk <jhkim0097@gmail.com>
2023-05-11 17:37:11 +0900

    [fix] fix intraId over length

--- src/test/java/com/gg/server/utils/TestDataUtils.java
+++ src/test/java/com/gg/server/utils/TestDataUtils.java
@@ -92,7 +92,7 @@ public class TestDataUtils {
     }
 
     public User createNewUser(int totalExp){
-        String randomId = UUID.randomUUID().toString();
+        String randomId = UUID.randomUUID().toString().substring(0, 30);
         User user = User.builder()
                 .eMail("email")
                 .intraId(randomId)
