f3d384afd2a61382c967a8fcaa81e0d11d94d5c0
AYoungSn <dudtjs0920@naver.com>
2023-05-12 17:23:34 +0900

    [fix] GGBE-20 create view season id add

--- createView.sql
+++ createView.sql
@@ -1,4 +1,4 @@
 create or replace view v_teamuser as
-    select team.id teamId, g.id gameId, g.start_time startTime, g.status, g.mode, tu.user_id userId, u.intra_id intraId, u.image_uri image, u.total_exp
+    select team.id teamId, g.id gameId, g.season_id seasonId, g.start_time startTime, g.status, g.mode, tu.user_id userId, u.intra_id intraId, u.image_uri image, u.total_exp
     from team, team_user tu, user u, game g
     where team.id=tu.team_id and u.id=tu.user_id and g.id=team.game_id;
