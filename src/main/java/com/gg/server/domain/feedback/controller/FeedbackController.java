304cb45b6ff8941d58bda2d0e89b7c3d3dcc4f5a
fefe2200 <dlcjf2200@naver.com>
2023-05-19 02:19:16 +0900

    [feat]save user feedback

---
+++ src/main/java/com/gg/server/domain/feedback/controller/FeedbackController.java
@@ -0,0 +1,36 @@
+package com.gg.server.domain.feedback.controller;
+
+
+import com.gg.server.domain.feedback.dto.FeedbackRequestDto;
+import com.gg.server.domain.feedback.service.FeedbackService;
+import com.gg.server.domain.user.User;
+import com.gg.server.domain.user.UserRepository;
+import com.gg.server.domain.user.dto.UserDto;
+import com.gg.server.global.utils.argumentresolver.Login;
+import io.swagger.v3.oas.annotations.Parameter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/pingpong")
+public class FeedbackController {
+    private final FeedbackService feedbackService;
+    private final UserRepository userRepository;
+
+    @PostMapping(value = "/feedback")
+    public ResponseEntity feedbackSave(@Valid @RequestBody FeedbackRequestDto feedbackRequestDto,
+                                       @Parameter(hidden = true) @Login UserDto user) {
+        feedbackService.addFeedback(feedbackRequestDto, user.getId());
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+}
