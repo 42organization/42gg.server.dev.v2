99d9876d4d27f8a95760f1886783c5550d52c7c5
AYoungSn <dudtjs0920@naver.com>
2023-05-18 15:25:38 +0900

    [fix] ErrorCode 수정

--- src/main/java/com/gg/server/global/exception/ErrorCode.java
+++ src/main/java/com/gg/server/global/exception/ErrorCode.java
@@ -7,16 +7,13 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
     // error code 등록
-    /** 500 **/
-    INTERNAL_SERVER_ERR(500, "COMMON-ERR-500","INTERNAL SERVER ERROR"),
-    //404
-    NOT_FOUND(404, "COMMON-ERR-404", "NOT FOUND"),
-    //400 잘못된 요청 코드
-    BAD_REQUEST(400, "COMMON-ERR-400", "BAD REQUEST"),
+    /** Common **/
+    INTERNAL_SERVER_ERR(500, "CM001","INTERNAL SERVER ERROR"),
+    NOT_FOUND(404, "CM002", "NOT FOUND"),
+    BAD_REQUEST(400, "CM003", "BAD REQUEST"),
+    UNAUTHORIZED(401, "CM004", "UNAUTHORIZED"),
+    METHOD_NOT_ALLOWED(405, "CM005", "METHOD NOT ALLOWED"),
     VALID_FAILED(400, "GAME-ERR-400" , "Valid Test Failed."),
-    BAD_ARGU(400, "ARGUMENT-ERR-400", "잘못된 argument 입니다."),
-
-    UNAUTHORIZED(401, "COMMON-ERR-401", "UNAUTHORIZED"),
     SN001(400, "SN001", "요청하신 값은 현 null 입니다"),
     AWS_S3_ERR(500, "CL001", "AWS S3 Error"),
     AWS_SERVER_ERR(500, "CL002", "AWS Error"),
