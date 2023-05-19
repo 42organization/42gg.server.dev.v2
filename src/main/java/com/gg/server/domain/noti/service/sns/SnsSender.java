c9d9586dac41dfdc55737f49202a1a625c76c67d
AYoungSn <dudtjs0920@naver.com>
2023-05-19 11:25:51 +0900

    [fix] GGBE-87 사용하지 않는 인터페이스 삭제

--- src/main/java/com/gg/server/domain/noti/service/sns/SnsSender.java
+++
@@ -1,8 +0,0 @@
-package com.gg.server.domain.noti.service.sns;
-
-import com.gg.server.domain.noti.data.Noti;
-import com.gg.server.domain.noti.dto.UserNotiDto;
-
-public interface SnsSender {
-    void send(UserNotiDto user, Noti noti);
-}
