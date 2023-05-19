d0fe2fb00acbf4e05f0768e118c11f444545b55a
kimjaehyuk <jhkim0097@gmail.com>
2023-05-19 11:34:35 +0900

    [feat] add penalty table

---
+++ src/main/java/com/gg/server/domain/penalty/data/Penalty.java
@@ -0,0 +1,45 @@
+package com.gg.server.domain.penalty.data;
+
+import com.gg.server.domain.noti.type.NotiType;
+import com.gg.server.domain.penalty.data.Type.PenaltyType;
+import com.gg.server.domain.user.User;
+import com.gg.server.global.utils.BaseTimeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.apache.tomcat.jni.Local;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+public class Penalty extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @NotNull
+    @Column(name = "penalty_type", length = 20)
+    @Enumerated(EnumType.STRING)
+    private PenaltyType type;
+
+    @Column(name = "message", length = 100)
+    private String message;
+
+    @NotNull
+    @Column(name = "start_time")
+    private LocalDateTime startTime;
+
+    @NotNull
+    @Column(name = "penalty_time")
+    private Integer penalty_time;
+}
