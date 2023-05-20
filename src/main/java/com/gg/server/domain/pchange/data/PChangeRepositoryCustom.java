8fe80b6202e3194b40241ac58818977f196ba03e
AYoungSn <dudtjs0920@naver.com>
2023-05-20 20:10:00 +0900

    [feat] GGBE-22 GGBE-102 pchange 변화량 조회 쿼리, history 쿼리 수정

--- src/main/java/com/gg/server/domain/pchange/data/PChangeRepositoryCustom.java
+++ src/main/java/com/gg/server/domain/pchange/data/PChangeRepositoryCustom.java
@@ -5,4 +5,6 @@ import java.util.List;
 public interface PChangeRepositoryCustom {
 
     List<PChange> findPChangesHistory(String intraId, Long seasonId);
+    List<PChange> findExpHistory(String intraId, Long gameId);
+    List<PChange> findPPPHistory(String intraId, Long gameId);
 }
--- src/main/java/com/gg/server/domain/pchange/data/PChangeRepositoryImpl.java
+++ src/main/java/com/gg/server/domain/pchange/data/PChangeRepositoryImpl.java
@@ -13,12 +13,38 @@ public class PChangeRepositoryImpl implements PChangeRepositoryCustom{
     @Override
     public List<PChange> findPChangesHistory(String intraId, Long seasonId) {
         String sql = "select p from PChange p join fetch p.game g join g.season s where p.user.intraId = " +
-                ":intra_id and s.id = :season_id order by p.createdAt desc";
+                ":intra_id and s.id = :season_id and p.game.mode = 'RANK' order by p.createdAt desc";
         return em.createQuery(sql, PChange.class)
                 .setParameter("intra_id", intraId)
                 .setParameter("season_id", seasonId)
                 .setFirstResult(0)
                 .setMaxResults(10)
+                .getResultList();
+    }
+
+    @Override
+    public List<PChange> findExpHistory(String intraId, Long gameId) {
+        String sql = "select p from PChange p join fetch p.game g join g.season s where p.user.intraId = " +
+                ":intra_id and p.id <= (select p2.id from PChange p2 where p2.game.id = :gameId) order by p.createdAt desc";
+        return em.createQuery(sql, PChange.class)
+                .setParameter("intra_id", intraId)
+                .setParameter("gameId", gameId)
+                .setFirstResult(0)
+                .setMaxResults(2)
+                .getResultList();
+    }
+
+    @Override
+    public List<PChange> findPPPHistory(String intraId, Long gameId) {
+        String sql = "select p from PChange p join fetch p.game g join g.season s where p.user.intraId = " +
+                ":intra_id and p.id <= (select p2.id from PChange p2 where p2.game.id = :gameId) " +
+                "and g.mode = 'RANK'" +
+                " order by p.createdAt desc";
+        return em.createQuery(sql, PChange.class)
+                .setParameter("intra_id", intraId)
+                .setParameter("gameId", gameId)
+                .setFirstResult(0)
+                .setMaxResults(2)
                 .getResultList();
     }
 }
