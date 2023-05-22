a7f885d571710d10112d10947be4c6bbec795b2b
AYoungSn <dudtjs0920@naver.com>
2023-05-22 19:29:56 +0900

    [fix] GGBE-22 join query 수정

--- src/main/java/com/gg/server/domain/pchange/data/PChangeRepositoryImpl.java
+++ src/main/java/com/gg/server/domain/pchange/data/PChangeRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.gg.server.domain.pchange.data;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -8,12 +9,13 @@ import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
+@Slf4j
 public class PChangeRepositoryImpl implements PChangeRepositoryCustom{
     private final EntityManager em;
     @Override
     public List<PChange> findPChangesHistory(String intraId, Long seasonId) {
-        String sql = "select p from PChange p join fetch p.game g join g.season s where p.user.intraId = " +
-                ":intra_id and s.id = :season_id and p.game.mode = 'RANK' order by p.createdAt desc";
+        String sql = "select p from PChange p join p.game g join p.user u join g.season s where u.intraId = " +
+                ":intra_id and s.id = :season_id and g.mode = 'RANK' order by p.createdAt desc";
         return em.createQuery(sql, PChange.class)
                 .setParameter("intra_id", intraId)
                 .setParameter("season_id", seasonId)
@@ -25,8 +27,7 @@ public class PChangeRepositoryImpl implements PChangeRepositoryCustom{
 
     @Override
     public List<PChange> findExpHistory(Long userId, Long gameId) {
-        System.out.println("userid: " + userId + ", gameId: " + gameId);
-        String sql = "select p from PChange p join fetch p.game g where p.user.id = " +
+        String sql = "select p from PChange p join p.game g where p.user.id = " +
                 ":userId and p.id <= (select p2.id from PChange p2 where p2.game.id = :gameId and p2.user.id = :userId) order by p.createdAt desc";
         return em.createQuery(sql, PChange.class)
                 .setParameter("userId", userId)
@@ -38,10 +39,10 @@ public class PChangeRepositoryImpl implements PChangeRepositoryCustom{
 
     @Override
     public List<PChange> findPPPHistory(Long userId, Long gameId, Long seasonId) {
-        String sql = "select p from PChange p join fetch p.game g join g.season s where p.user.id = " +
+        String sql = "select p from PChange p join p.game g join g.season s where p.user.id = " +
                 ":userId and p.id <= (select p2.id from PChange p2 where p2.game.id = :gameId and p2.user.id =:userId) " +
                 "and p.game.mode = 'RANK' and s.id = :season_id " +
-                " order by p.createdAt desc";
+                "order by p.createdAt desc";
         return em.createQuery(sql, PChange.class)
                 .setParameter("userId", userId)
                 .setParameter("gameId", gameId)
