fe7cff53dafa15a264464214fd44293aefddd08a
fefe2200 <dlcjf2200@naver.com>
2023-06-01 00:51:13 +0900

    [feat]슬롯매니지먼트 POST 기획변경 대응

--- src/main/java/com/gg/server/admin/slotmanagement/service/SlotAdminService.java
+++ src/main/java/com/gg/server/admin/slotmanagement/service/SlotAdminService.java
@@ -1,17 +1,18 @@
 package com.gg.server.admin.slotmanagement.service;
 
-import com.gg.server.admin.season.dto.SeasonAdminDto;
 import com.gg.server.admin.slotmanagement.data.adminSlotManagementRepository;
 import com.gg.server.admin.slotmanagement.dto.SlotAdminDto;
 import com.gg.server.admin.slotmanagement.dto.SlotCreateRequestDto;
 import com.gg.server.admin.slotmanagement.dto.SlotListAdminResponseDto;
-import com.gg.server.domain.season.data.Season;
 import com.gg.server.domain.slotmanagement.SlotManagement;
-import com.gg.server.domain.slotmanagement.exception.SmNotFoundException;
+import com.gg.server.domain.slotmanagement.exception.SlotManagementForbiddenException;
+import com.gg.server.domain.slotmanagement.exception.SlotManagementNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,8 +34,20 @@ public class SlotAdminService {
 
     @Transactional
     public void addSlotSetting(SlotCreateRequestDto requestDto) {
+        updateNowSlotManagementEndTime(requestDto.getStartTime());
         SlotManagement slotManagement = new SlotManagement(requestDto);
 
         adminSlotManagementRepository.save(slotManagement);
+    }
+
+    private void updateNowSlotManagementEndTime(LocalDateTime endTime){
+        SlotManagement nowSlotManagement = adminSlotManagementRepository.findFirstByOrderByIdDesc()
+                .orElseThrow(() -> new SlotManagementNotFoundException());
+        LocalDateTime nowFutureSlotTime = LocalDateTime.now().plusHours(nowSlotManagement.getFutureSlotTime());
+
+        if (nowFutureSlotTime.isAfter(endTime))
+            throw new SlotManagementForbiddenException();
+
+        nowSlotManagement.updateEndTime(endTime);
     }
 }
