996a0ef4a57076fcd26d16cdfd1b084cda8273a5
kmularise <kmula222@gmail.com>
2023-06-05 09:59:21 +0900

    [feat] GGBE-152 add slotmanagement query

--- src/main/java/com/gg/server/domain/slotmanagement/data/SlotManagementRepository.java
+++ src/main/java/com/gg/server/domain/slotmanagement/data/SlotManagementRepository.java
@@ -1,9 +1,23 @@
 package com.gg.server.domain.slotmanagement.data;
 
 import com.gg.server.domain.slotmanagement.SlotManagement;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SlotManagementRepository
     extends JpaRepository<SlotManagement, Long> {
+    @Query("select sm from SlotManagement sm where sm.endTime > :now or sm.endTime is null")
+    List<SlotManagement> findAfter(@Param("now") LocalDateTime now);
+
+    @Query("select sm from SlotManagement sm where (sm.endTime is null"
+            + " or sm.endTime > :now) and sm.startTime <:now")
+    Optional<SlotManagement> findCurrent(@Param("now") LocalDateTime now);
+
     SlotManagement findFirstByOrderByCreatedAtDesc();
+
+
 }
