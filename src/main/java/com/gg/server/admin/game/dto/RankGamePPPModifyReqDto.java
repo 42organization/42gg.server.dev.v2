310ddd6ea59bb63efd50bfda7837c7f863ac8898
AYoungSn <dudtjs0920@naver.com>
2023-06-07 13:20:50 +0900

    [fix] admin game result edit fix

--- src/main/java/com/gg/server/admin/game/dto/RankGamePPPModifyReqDto.java
+++ src/main/java/com/gg/server/admin/game/dto/RankGamePPPModifyReqDto.java
@@ -1,5 +1,6 @@
 package com.gg.server.admin.game.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import javax.validation.constraints.Max;
@@ -8,15 +9,16 @@ import javax.validation.constraints.Positive;
 import javax.validation.constraints.PositiveOrZero;
 
 @Getter
+@AllArgsConstructor
 public class RankGamePPPModifyReqDto {
-    @NotNull(message = "TeamId1 는 필수 값입니다.")
+    @NotNull(message = "Team1Id 는 필수 값입니다.")
     @Positive
     private Long Team1Id;
     @NotNull(message = "Team1Score 는 필수 값입니다.")
     @PositiveOrZero
     @Max(2)
     private int Team1Score;
-    @NotNull(message = "TeamId2 는 필수 값입니다.")
+    @NotNull(message = "Team2Id 는 필수 값입니다.")
     @Positive
     private Long Team2Id;
     @NotNull(message = "Team2Score 는 필수 값입니다.")
