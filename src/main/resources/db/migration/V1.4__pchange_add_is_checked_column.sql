08879c6e27acc1c71de17e4dfdbe319771f76bb7
kimjaehyuk <jhkim0097@gmail.com>
2023-06-13 15:42:08 +0900

    HOTFIX-V1.4 migrationScript

--- src/main/resources/db/migration/V1.4__pchange_add_is_checked_column.sql
+++ src/main/resources/db/migration/V1.4__pchange_add_is_checked_column.sql
@@ -1,3 +1,3 @@
-ALTER TABLE `MAIN`.`pchange`
-    ADD COLUMN `is_checked` BIT(1) NOT NULL DEFAULT 0 AFTER `user_id`;
+ALTER TABLE pchange
+    ADD COLUMN is_checked BIT(1) NOT NULL DEFAULT 0 AFTER user_id;
 UPDATE pchange SET is_checked = 1;
