2af02a8f27163ec6be5587788125077a803bfe25
wken5577 <leehyunkyu@ihyeongyuui-MacBookAir.local>
2023-06-16 10:17:49 +0900

    workflow dockerfile 수정

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,10 @@ FROM openjdk:11-jdk
 
 WORKDIR /app
 
-ARG JAR_FILE=build/libs/ROOT.war
+ARG JAR_FILE=build/libs/server-42gg.jar
+ARG PROFILE=default
 
 COPY ${JAR_FILE} .
 
 EXPOSE 8080
-
-ENTRYPOINT ["java","-jar","ROOT.war"]
+ENTRYPOINT ["java","-jar","server-42gg.jar","--spring.profiles.active=${PROFILE}"]
--- update-yml.sh
+++ update-yml.sh
@@ -2,36 +2,12 @@
 
 # yml 파일 경로
 YML_FILE="./src/main/resources/application.yml"
-TEST_YML_FILE="./src/main/resources/application-test.yml"
-MAIN_YML_FILE="./src/main/resources/application-main.yml"
-
-# yml 파일 읽기
-YML_CONTENT=$(cat "$YML_FILE")
-
-# active 값을 test, main로 변경
-YML_ACTIVE_TEST=$(echo "$YML_CONTENT" | sed 's/active: local/active: test/g')
-YML_ACTIVE_MAIN=$(echo "$YML_CONTENT" | sed 's/active: local/active: main/g')
 
 # 변경된 내용을 임시 파일에 저장
-ENCODED_YML_ACTIVE_TEST=$(mktemp)
-echo "$YML_ACTIVE_TEST" | base64 > "$ENCODED_YML_ACTIVE_TEST"
-
-ENCODED_YML_ACTIVE_MAIN=$(mktemp)
-echo "$YML_ACTIVE_MAIN" | base64 > "$ENCODED_YML_ACTIVE_MAIN"
-
-ENCODED_APP_YEST_YML=$(mktemp)
-cat "$TEST_YML_FILE" | base64 > "$ENCODED_APP_YEST_YML"
-
-ENCODED_APP_MAIN_YML=$(mktemp)
-cat "$MAIN_YML_FILE" | base64 > "$ENCODED_APP_MAIN_YML"
+ENCODED_APPLICATION_YML=$(mktemp)
+cat "$YML_FILE" | base64 > "$ENCODED_APPLICATION_YML"
 
 # github secret에 저장
-cat "$ENCODED_YML_ACTIVE_TEST" | gh secret set -R 42organization/42gg.server.dev.v2 YML_ACTIVE_TEST
-cat "$ENCODED_YML_ACTIVE_MAIN" | gh secret set -R 42organization/42gg.server.dev.v2 YML_ACTIVE_MAIN
-cat "$ENCODED_APP_YEST_YML" | gh secret set -R 42organization/42gg.server.dev.v2 APPLICATION_TEST_YML
-cat "$ENCODED_APP_MAIN_YML" | gh secret set -R 42organization/42gg.server.dev.v2 APPLICATION_MAIN_YML
+cat "$ENCODED_APPLICATION_YML" | gh secret set -R 42organization/42gg.server.dev.v2 APPLICATION_YML
 
-rm "$ENCODED_YML_ACTIVE_TEST"
-rm "$ENCODED_YML_ACTIVE_MAIN"
-rm "$ENCODED_APP_YEST_YML"
-rm "$ENCODED_APP_MAIN_YML"
+rm "$ENCODED_APPLICATION_YML"
