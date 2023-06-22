2a1696e1586900d191ddbd5838e0cf907b39e548
hyunkyle <hyunkyle@student.42seoul.kr>
2023-06-22 22:36:58 +0900

    fix2

--- src/main/java/com/gg/server/global/security/handler/OAuthAuthenticationSuccessHandler.java
+++ src/main/java/com/gg/server/global/security/handler/OAuthAuthenticationSuccessHandler.java
@@ -54,6 +54,7 @@ public class OAuthAuthenticationSuccessHandler extends SimpleUrlAuthenticationSu
         Cookie[] cookies = request.getCookies();
         for (Cookie cookie :cookies) {
             if (cookie.getName().equals(TokenHeaders.REFRESH_TOKEN) ) {
+                System.out.println("has token");
                 Long existUserId = tokenProvider.getUserIdFormRefreshToken(cookie.getValue());
                 System.out.println("existUserId = " + existUserId);
                 cookieUtil.deleteCookie(response, TokenHeaders.REFRESH_TOKEN);
