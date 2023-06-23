59df467e0385dd80d70a8538de875b8b70d67f06
AYoungSn <dudtjs0920@naver.com>
2023-06-23 18:46:15 +0900

    [fix] GGBE-200 enum type null check fix

--- src/main/java/com/gg/server/domain/game/GameController.java
+++ src/main/java/com/gg/server/domain/game/GameController.java
@@ -37,9 +37,9 @@ public class GameController {
         }
         Pageable pageable = PageRequest.of(gameReq.getPage() - 1, gameReq.getSize(), Sort.by(Sort.Direction.DESC, "startTime"));
         if (gameReq.getIntraId() != null) {
-            return gameFindService.allGameListUser(pageable, gameReq.getIntraId(), gameReq.getStatus().name());
+            return gameFindService.allGameListUser(pageable, gameReq.getIntraId(), gameReq.getStatus() != null ? gameReq.getStatus().name() : "NULL");
         }
-        return gameFindService.allGameList(pageable, gameReq.getStatus().name());
+        return gameFindService.allGameList(pageable, gameReq.getStatus() != null ? gameReq.getStatus().name() : "NULL");
     }
 
     @GetMapping("/normal")
