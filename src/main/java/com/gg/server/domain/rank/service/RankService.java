4ffd7dc0605a3cc87f84f2f6470f1d5ab07badfe
hyunkyu <azdlgusrb@naver.com>
2023-07-10 15:39:56 +0900

    [fix] GGBE-209 vip api 수정

--- src/main/java/com/gg/server/domain/rank/service/RankService.java
+++ src/main/java/com/gg/server/domain/rank/service/RankService.java
@@ -41,7 +41,7 @@ public class RankService {
     public ExpRankPageResponseDto getExpRankPage(PageRequest pageRequest, UserDto curUser) {
 
         Long myRank = curUser.getTotalExp() == 0 ? -1 : userRepository.findExpRankingByIntraId(curUser.getIntraId());
-        Page<User> users = userRepository.findAll(pageRequest);
+        Page<User> users = userRepository.findAllByTotalExpGreaterThan(pageRequest, 0);
         if(pageRequest.getPageNumber() + 1 > users.getTotalPages())
             throw new PageNotFoundException("페이지가 존재하지 않습니다.", ErrorCode.PAGE_NOT_FOUND);
 
