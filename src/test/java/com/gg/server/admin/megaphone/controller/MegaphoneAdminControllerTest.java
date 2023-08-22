359618ee5d1e22a6f5fe5541841c439fe26de5d4
류민아 <rmina1030@naver.com>
2023-08-22 11:19:45 +0900

    [TEST] GGBE4-17 확성기 전체 조회 테스트 코드 작성

---
+++ src/test/java/com/gg/server/admin/megaphone/controller/MegaphoneAdminControllerTest.java
@@ -0,0 +1,44 @@
+package com.gg.server.admin.megaphone.controller;
+
+import com.gg.server.global.security.jwt.utils.AuthTokenProvider;
+import com.gg.server.utils.TestDataUtils;
+import lombok.RequiredArgsConstructor;
+import org.apache.http.HttpHeaders;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RequiredArgsConstructor
+@SpringBootTest
+@AutoConfigureMockMvc
+class MegaphoneAdminControllerTest {
+    @Autowired
+    TestDataUtils testDataUtils;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    AuthTokenProvider tokenProvider;
+
+    @Test
+    @DisplayName("[GET] /pingpong/admin/megaphones/history?page={page}&size={pageSize}&intraId={intraId}")
+    void getMegaphoneListTest() throws Exception {
+        String accessToken = testDataUtils.getAdminLoginAccessToken();
+        tokenProvider.getUserIdFromAccessToken(accessToken);
+        String url = "/pingpong/admin/megaphones/history?page=1&size=30";
+
+        String contentAsString = mockMvc.perform(get(url)
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+
+        System.out.println(contentAsString);
+    }
+}
