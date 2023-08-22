3a9bee2503948240e928d2e2b3b416bed289a057
fefe2200 <dlcjf2200@naver.com>
2023-08-23 02:22:50 +0900

    [style] GGBE4-44 로그 제거

--- src/main/java/com/gg/server/domain/coin/service/CoinHistoryService.java
+++ src/main/java/com/gg/server/domain/coin/service/CoinHistoryService.java
@@ -6,7 +6,6 @@ import com.gg.server.domain.coin.data.CoinPolicyRepository;
 import com.gg.server.domain.coin.type.HistoryType;
 import com.gg.server.domain.user.data.User;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
