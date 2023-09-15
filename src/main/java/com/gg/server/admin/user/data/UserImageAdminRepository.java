ac5a034b76926eaf8404a002f525b1234c2c7441
kylew1004 <kylew1004@gmail.com>
2023-09-15 16:41:15 +0900

    [REFACTOR] delete useless import

--- src/main/java/com/gg/server/admin/user/data/UserImageAdminRepository.java
+++ src/main/java/com/gg/server/admin/user/data/UserImageAdminRepository.java
@@ -2,7 +2,6 @@ package com.gg.server.admin.user.data;
 
 import com.gg.server.domain.user.data.User;
 import org.springframework.data.repository.query.Param;
-import org.checkerframework.checker.interning.qual.CompareToMethod;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
