6d9ccb406b5cf9e6ff9c0d8ccc4d606419f1fda0
Sion Shin <kmicety1@gmail.com>
2023-09-17 23:34:59 +0900

    [REFACTOR] Item setVisibility를 서비스단에서 엔티티단으로 변경

--- src/main/java/com/gg/server/domain/tier/data/TierRepository.java
+++ src/main/java/com/gg/server/domain/tier/data/TierRepository.java
@@ -1,13 +1,14 @@
 package com.gg.server.domain.tier.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface TierRepository extends JpaRepository<Tier, Long> {
 
-    default Optional<Tier> findStartTier() {
-        return findById(1L);
-    }
+    @Query("SELECT t FROM Tier t WHERE t.id = 1L")
+    Optional<Tier> findStartTier();
+
     Optional<Tier> findByName(String name);
 }
