c8917534d985020a8ef70baead8f10c685e23271
kylew1004 <kylew1004@gmail.com>
2023-09-19 17:04:59 +0900

    [FIX] UserAlreadyAttendanceException형을 DuplicationException로 상속

--- src/main/java/com/gg/server/domain/user/exception/UserAlreadyAttendanceException.java
+++ src/main/java/com/gg/server/domain/user/exception/UserAlreadyAttendanceException.java
@@ -1,9 +1,9 @@
 package com.gg.server.domain.user.exception;
 
 import com.gg.server.global.exception.ErrorCode;
-import com.gg.server.global.exception.custom.InvalidParameterException;
+import com.gg.server.global.exception.custom.DuplicationException;
 
-public class UserAlreadyAttendanceException extends InvalidParameterException {
+public class UserAlreadyAttendanceException extends DuplicationException {
     public UserAlreadyAttendanceException() {
         super("이미 출석한 유저입니다.", ErrorCode.USER_ALREADY_ATTENDANCE);
     }
