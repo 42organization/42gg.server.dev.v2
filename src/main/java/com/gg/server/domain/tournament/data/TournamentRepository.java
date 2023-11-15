e5d91542faca4fae8e202ebfb22cece488f40dd3
jahlee <jahlee@student.42seoul.kr>
2023-11-15 20:07:08 +0900

    [:sparkles:] TournamentRepository findAllByStatus 매서드 추가

--- src/main/java/com/gg/server/domain/tournament/data/TournamentRepository.java
+++ src/main/java/com/gg/server/domain/tournament/data/TournamentRepository.java
@@ -1,6 +1,9 @@
 package com.gg.server.domain.tournament.data;
 
+import com.gg.server.domain.tournament.type.TournamentStatus;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TournamentRepository extends JpaRepository<Tournament, Long> {
+    List<Tournament> findAllByStatus(TournamentStatus status);
 }
