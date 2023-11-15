0640e9c51dd13ef3593aafc4de200471385b30a8
jahlee <jahlee@student.42seoul.kr>
2023-11-15 20:03:43 +0900

    [:test_tube:] 토너먼트 수정 controller & service 테스트 추가

---
+++ src/test/java/com/gg/server/admin/tournament/controller/TournamentAdminControllerTest.java
@@ -0,0 +1,268 @@
+package com.gg.server.admin.tournament.controller;
+
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gg.server.admin.tournament.dto.TournamentAdminUpdateRequestDto;
+import com.gg.server.admin.tournament.service.TournamentAdminService;
+import com.gg.server.domain.tournament.data.Tournament;
+import com.gg.server.domain.tournament.type.TournamentStatus;
+import com.gg.server.domain.tournament.type.TournamentType;
+import com.gg.server.global.security.jwt.utils.AuthTokenProvider;
+import com.gg.server.utils.TestDataUtils;
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
+import org.apache.http.HttpHeaders;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@RequiredArgsConstructor
+@Transactional
+class TournamentAdminControllerTest {
+
+    @Autowired
+    TestDataUtils testDataUtils;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    AuthTokenProvider tokenProvider;
+
+    @Autowired
+    TournamentAdminService tournamentAdminService;
+
+    @Test
+    @DisplayName("[Patch] /pingpong/admin/tournament/{tournamentId}")
+    void 토너먼트_업데이트_성공() throws Exception {
+        // given
+        String accessToken = testDataUtils.getAdminLoginAccessToken();
+        tokenProvider.getUserIdFromAccessToken(accessToken);
+
+        Tournament tournament = testDataUtils.createTournament(
+            LocalDateTime.now().plusDays(2).plusHours(1),
+            LocalDateTime.now().plusDays(2).plusHours(3),
+            TournamentStatus.BEFORE);
+
+        TournamentAdminUpdateRequestDto updateDto = testDataUtils.createUpdateRequestDto (
+            LocalDateTime.now().plusDays(2).plusHours(2),
+            LocalDateTime.now().plusDays(2).plusHours(4),
+            TournamentType.MASTER);
+
+        String url = "/pingpong/admin/tournament/" + tournament.getId();
+
+        String content = objectMapper.writeValueAsString(updateDto);
+
+        // when, then
+        String contentAsString = mockMvc.perform(patch(url)
+            .content(content)
+            .contentType(MediaType.APPLICATION_JSON)
+            .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken))
+            .andExpect(status().isNoContent())
+            .andReturn().getResponse().getContentAsString();
+
+        System.out.println(contentAsString);
+    }
+
+    @Test
+    void 토너먼트_없는_경우() throws Exception {
+        // given
+        String accessToken = testDataUtils.getAdminLoginAccessToken();
+        tokenProvider.getUserIdFromAccessToken(accessToken);
+
+        TournamentAdminUpdateRequestDto updateDto = testDataUtils.createUpdateRequestDto(
+            LocalDateTime.now().plusDays(2).plusHours(2),
+            LocalDateTime.now().plusDays(2).plusHours(4),
+            TournamentType.MASTER);
+
+        String url = "/pingpong/admin/tournament/" + 1111;
+
+        String content = objectMapper.writeValueAsString(updateDto);
+        // when, then
+        String contentAsString = mockMvc.perform(patch(url)
+            .content(content)
+            .contentType(MediaType.APPLICATION_JSON)
+            .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken))
+            .andExpect(status().isNotFound())
+            .andReturn().getResponse().getContentAsString();
+
+        System.out.println(contentAsString);
+    }
+
+    @Test
+    void 토너먼트_업데이트_기간_겹치는_경우() throws Exception {
+        // given
+        String accessToken = testDataUtils.getAdminLoginAccessToken();
+        tokenProvider.getUserIdFromAccessToken(accessToken);
+
+        Tournament tournamentAlreadyExist = testDataUtils.createTournament(
+            LocalDateTime.now().plusDays(2).plusHours(2),
+            LocalDateTime.now().plusDays(2).plusHours(4),
+            TournamentStatus.BEFORE);
+
+        Tournament tournamentToChange = testDataUtils.createTournament(
+            LocalDateTime.now().plusDays(2).plusHours(5),
+            LocalDateTime.now().plusDays(2).plusHours(7),
+            TournamentStatus.BEFORE);
+
+        // 겹치는 시간 조절
+        TournamentAdminUpdateRequestDto updateDto = testDataUtils.createUpdateRequestDto(
+            tournamentAlreadyExist.getStartTime().plusMinutes(1),
+            tournamentAlreadyExist.getEndTime().plusMinutes(2),
+            TournamentType.MASTER);
+
+        String url = "/pingpong/admin/tournament/" + tournamentToChange.getId();
+
+        String content = objectMapper.writeValueAsString(updateDto);
+
+        // when, then
+        String contentAsString = mockMvc.perform(patch(url)
+            .content(content)
+            .contentType(MediaType.APPLICATION_JSON)
+            .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken))
+            .andExpect(status().isConflict())
+            .andReturn().getResponse().getContentAsString();
+
+        System.out.println(contentAsString);
+    }
+
+    @Test
+    void 이미_시작했거나_종료된_토너먼트_수정() throws Exception {
+        // given
+        String accessToken = testDataUtils.getAdminLoginAccessToken();
+        tokenProvider.getUserIdFromAccessToken(accessToken);
+
+        Tournament liveTournament = testDataUtils.createTournament(
+            LocalDateTime.now().minusHours(1),
+            LocalDateTime.now().plusHours(2),
+            TournamentStatus.LIVE);
+
+        Tournament endedTournament = testDataUtils.createTournament(
+            LocalDateTime.now().minusHours(3),
+            LocalDateTime.now().minusHours(1),
+            TournamentStatus.END);
+
+        TournamentAdminUpdateRequestDto updateTournamentDto = testDataUtils.createUpdateRequestDto(
+            LocalDateTime.now().plusDays(2).plusHours(1),
+            LocalDateTime.now().plusDays(2).plusHours(3),
+            TournamentType.MASTER);
+
+        String url = "/pingpong/admin/tournament/" + liveTournament.getId();
+
+        String content = objectMapper.writeValueAsString(updateTournamentDto);
+
+        // when live tournament test, then
+        String contentAsString = mockMvc.perform(patch(url)
+            .content(content)
+            .contentType(MediaType.APPLICATION_JSON)
+            .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken))
+            .andExpect(status().isBadRequest())
+            .andReturn().getResponse().getContentAsString();
+
+        System.out.println(contentAsString);
+
+        url = "/pingpong/admin/tournament/" + endedTournament.getId();
+
+        // when ended tournament test, then
+        contentAsString = mockMvc.perform(patch(url)
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken))
+            .andExpect(status().isBadRequest())
+            .andReturn().getResponse().getContentAsString();
+
+        System.out.println(contentAsString);
+    }
+
+    @Test
+    void 토너먼트_잘못된_기간() throws Exception {
+        // given
+        String accessToken = testDataUtils.getAdminLoginAccessToken();
+        tokenProvider.getUserIdFromAccessToken(accessToken);
+
+        Tournament tournamentToChange = testDataUtils.createTournament(
+            LocalDateTime.now().plusDays(2).plusHours(1),
+            LocalDateTime.now().plusDays(2).plusHours(3),
+            TournamentStatus.BEFORE);
+
+        TournamentAdminUpdateRequestDto updateDto1 = testDataUtils.createUpdateRequestDto(
+            tournamentToChange.getStartTime(),
+            tournamentToChange.getStartTime(),
+            TournamentType.MASTER);
+
+        TournamentAdminUpdateRequestDto updateDto2 = testDataUtils.createUpdateRequestDto(
+            tournamentToChange.getEndTime(),
+            tournamentToChange.getStartTime(),
+            TournamentType.MASTER);
+
+        String url = "/pingpong/admin/tournament/" + tournamentToChange.getId();
+        // when startTime == endTime, then
+        String content = objectMapper.writeValueAsString(updateDto1);
+
+        String contentAsString = mockMvc.perform(patch(url)
+            .content(content)
+            .contentType(MediaType.APPLICATION_JSON)
+            .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken))
+            .andExpect(status().isBadRequest())
+            .andReturn().getResponse().getContentAsString();
+
+        System.out.println(contentAsString);
+
+        // when startTime > endTime test, then
+        content = objectMapper.writeValueAsString(updateDto2);
+
+        contentAsString = mockMvc.perform(patch(url)
+            .content(content)
+            .contentType(MediaType.APPLICATION_JSON)
+            .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken))
+            .andExpect(status().isBadRequest())
+            .andReturn().getResponse().getContentAsString();
+
+        System.out.println(contentAsString);
+    }
+
+    @Test
+    void 잘못된_dto() throws Exception {
+        // given
+        String accessToken = testDataUtils.getAdminLoginAccessToken();
+        tokenProvider.getUserIdFromAccessToken(accessToken);
+
+        Tournament tournamentToChange = testDataUtils.createTournament(
+            LocalDateTime.now().plusDays(2).plusHours(1),
+            LocalDateTime.now().plusDays(2).plusHours(3),
+            TournamentStatus.BEFORE);
+
+        TournamentAdminUpdateRequestDto updateDto1 = testDataUtils.createUpdateRequestDto(
+            tournamentToChange.getStartTime(),
+            tournamentToChange.getStartTime(),
+            null);
+
+        String url = "/pingpong/admin/tournament/" + tournamentToChange.getId();
+        // when startTime == endTime, then
+        String content = objectMapper.writeValueAsString(updateDto1);
+
+        String contentAsString = mockMvc.perform(patch(url)
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken))
+            .andExpect(status().isBadRequest())
+            .andReturn().getResponse().getContentAsString();
+
+        System.out.println(contentAsString);
+    }
+
+
+
+}
---
+++ src/test/java/com/gg/server/admin/tournament/service/TournamentAdminServiceTest.java
@@ -0,0 +1,185 @@
+package com.gg.server.admin.tournament.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
+
+import com.gg.server.admin.tournament.dto.TournamentAdminUpdateRequestDto;
+import com.gg.server.domain.tournament.data.Tournament;
+import com.gg.server.domain.tournament.data.TournamentRepository;
+import com.gg.server.domain.tournament.exception.TournamentConflictException;
+import com.gg.server.domain.tournament.exception.TournamentNotFoundException;
+import com.gg.server.domain.tournament.exception.TournamentUpdateException;
+import com.gg.server.domain.tournament.type.TournamentStatus;
+import com.gg.server.domain.tournament.type.TournamentType;
+import com.gg.server.global.exception.custom.InvalidParameterException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class TournamentAdminServiceTest {
+    @Mock
+    TournamentRepository tournamentRepository;
+    @InjectMocks
+    TournamentAdminService tournamentAdminService;
+
+    @Test
+    @DisplayName("[Patch] /pingpong/admin/tournament/{tournamentId}")
+    public void 토너먼트_업데이트_성공() {
+        // given
+        List<Tournament> tournamentList = makeTournaments(1L, 2, getTargetTime(2, 1));
+        Tournament tournament = tournamentList.get(0);
+        TournamentAdminUpdateRequestDto updateRequestDto = makeTournamentAdminUpdateRequestDto(
+            getTargetTime(3, 1), getTargetTime(3, 3));
+        given(tournamentRepository.findById(1L)).willReturn(Optional.of(tournament));
+        given(tournamentRepository.findAllByStatus(TournamentStatus.BEFORE)).willReturn(tournamentList);
+        // when
+        Tournament changedTournament = tournamentAdminService.updateTournamentInfo(1L, updateRequestDto);
+        // then
+        assertThat(changedTournament.getId()).isEqualTo(tournament.getId());
+        assertThat(changedTournament.getTitle()).isEqualTo(updateRequestDto.getTitle());
+        assertThat(changedTournament.getContents()).isEqualTo(updateRequestDto.getContents());
+        assertThat(changedTournament.getStartTime()).isEqualTo(updateRequestDto.getStartTime());
+        assertThat(changedTournament.getEndTime()).isEqualTo(updateRequestDto.getEndTime());
+        assertThat(changedTournament.getType()).isEqualTo(updateRequestDto.getType());
+        assertThat(changedTournament.getStatus()).isEqualTo(tournament.getStatus());
+    }
+
+    @Test
+    public void 업데이트_토너먼트_없음() {
+        // given
+        Tournament tournament = makeTournament(1234L, TournamentStatus.BEFORE,
+            getTargetTime(2, 1), getTargetTime(2, 3));
+        TournamentAdminUpdateRequestDto updateRequestDto = makeTournamentAdminUpdateRequestDto(
+            getTargetTime(2, 1), getTargetTime(2, 3));
+
+        given(tournamentRepository.findById(tournament.getId())).willReturn(Optional.empty());
+        // when, then
+        assertThatThrownBy(() -> tournamentAdminService.updateTournamentInfo(tournament.getId(), updateRequestDto))
+            .isInstanceOf(TournamentNotFoundException.class);
+    }
+
+    @Test
+    public void 토너먼트_업데이트_불가_상태() {
+        // given
+        Tournament tournamentLive = makeTournament(1L, TournamentStatus.LIVE,
+            getTargetTime(0, -1), getTargetTime(0, 1));
+        Tournament tournamentEnd = makeTournament(2L, TournamentStatus.END,
+            getTargetTime(0, -3), getTargetTime(0, -1));
+        TournamentAdminUpdateRequestDto updateRequestDto = makeTournamentAdminUpdateRequestDto(
+            getTargetTime(2, 1), getTargetTime(2, 3));
+        given(tournamentRepository.findById(tournamentLive.getId())).willReturn(Optional.of(tournamentLive));
+        given(tournamentRepository.findById(tournamentEnd.getId())).willReturn(Optional.of(tournamentEnd));
+        // when, then
+        assertThatThrownBy(() -> tournamentAdminService.updateTournamentInfo(tournamentLive.getId(), updateRequestDto))
+            .isInstanceOf(TournamentUpdateException.class);
+        assertThatThrownBy(() -> tournamentAdminService.updateTournamentInfo(tournamentEnd.getId(), updateRequestDto))
+            .isInstanceOf(TournamentUpdateException.class);
+    }
+
+    @Test
+    public void 업데이트_토너먼트_Dto_Invalid_Time() {
+        // given
+        Tournament tournament = makeTournament(1L, TournamentStatus.BEFORE,
+            getTargetTime(2, 1), getTargetTime(2, 3));
+        TournamentAdminUpdateRequestDto invalidRequestDto1 = makeTournamentAdminUpdateRequestDto(
+            getTargetTime(2, 3), getTargetTime(2, 1));
+        TournamentAdminUpdateRequestDto invalidRequestDto2 = makeTournamentAdminUpdateRequestDto(
+            invalidRequestDto1.getStartTime(), invalidRequestDto1.getStartTime());
+        TournamentAdminUpdateRequestDto invalidRequestDto3 = makeTournamentAdminUpdateRequestDto(
+            getTargetTime(2, -1), getTargetTime(2, 1));
+
+        given(tournamentRepository.findById(1L)).willReturn(Optional.of(tournament));
+        // when then
+        assertThatThrownBy(() -> tournamentAdminService.updateTournamentInfo(tournament.getId(), invalidRequestDto1))
+            .isInstanceOf(InvalidParameterException.class);
+        assertThatThrownBy(() -> tournamentAdminService.updateTournamentInfo(tournament.getId(), invalidRequestDto2))
+            .isInstanceOf(InvalidParameterException.class);
+        assertThatThrownBy(() -> tournamentAdminService.updateTournamentInfo(tournament.getId(), invalidRequestDto3))
+            .isInstanceOf(InvalidParameterException.class);
+    }
+
+    @Test
+    public void Dto_기간_토너먼트_기간_겹침() {
+        // given
+        List<Tournament> tournamentList = makeTournaments(1L, 2, getTargetTime(2, 1));
+        Tournament tournament = tournamentList.get(0);
+        TournamentAdminUpdateRequestDto updateRequestDto = makeTournamentAdminUpdateRequestDto(
+            LocalDateTime.now().plusDays(2).plusHours(3), LocalDateTime.now().plusDays(2).plusHours(4));
+        given(tournamentRepository.findById(1L)).willReturn(Optional.of(tournament));
+        given(tournamentRepository.findAllByStatus(TournamentStatus.BEFORE)).willReturn(tournamentList);
+        // when, then
+        assertThatThrownBy(() -> tournamentAdminService.updateTournamentInfo(tournament.getId(), updateRequestDto))
+            .isInstanceOf(TournamentConflictException.class);
+    }
+
+    /**
+     * 현재 시간에서 days hours, 만큼 차이나는 시간을 구한다.
+     * @param days
+     * @param hours
+     * @return
+     */
+    LocalDateTime getTargetTime(long days, long hours) {
+        return LocalDateTime.now().plusDays(days).plusHours(hours);
+    }
+
+    /**
+     * 각 매개변수로 초기화 된 토너먼트를 반환
+     * @param id
+     * @param status
+     * @param startTime
+     * @param endTime
+     * @return
+     */
+    Tournament makeTournament(Long id, TournamentStatus status, LocalDateTime startTime, LocalDateTime endTime) {
+        return new Tournament(
+            id,
+            id + "st tournament",
+            "",
+            startTime,
+            endTime,
+            TournamentType.ROOKIE,
+            status);
+    }
+
+    /**
+     * <div>id 부터 cnt개 만큼의 토너먼트 리스트를 반환해준다.<div/>
+     * 각 토너먼트는 1시간 길이이며, 토너먼트간 1시간의 간격이 있다.
+     * @param id
+     * @param cnt
+     * @param startTime
+     * @return
+     */
+    List<Tournament> makeTournaments(Long id, long cnt, LocalDateTime startTime) {
+        List<Tournament> tournamentList = new ArrayList<>();
+        for (long i=0; i<cnt; i++) {
+            tournamentList.add(makeTournament(id++, TournamentStatus.BEFORE,
+                startTime.plusHours(i*2), startTime.plusHours((i*2+1))));
+        }
+        return tournamentList;
+    }
+
+    /**
+     * 각 매개변수로 초기화된 TournamentAdminUpdateRequestDto를 반환
+     * @param startTime
+     * @param endTime
+     * @return
+     */
+    TournamentAdminUpdateRequestDto makeTournamentAdminUpdateRequestDto(LocalDateTime startTime, LocalDateTime endTime) {
+        return new TournamentAdminUpdateRequestDto(
+            "tournament changed",
+            "changed",
+            startTime,
+            endTime,
+            TournamentType.ROOKIE
+        );
+    }
+}
