24412991eb8683c7d42ffe6994ae74c2712c0e0b
jahlee <jahlee@student.42seoul.kr>
2023-11-15 20:13:07 +0900

    [:sparkles:] 토너먼트 관리자 수정 컨트롤러 매서드 추가

---
+++ src/main/java/com/gg/server/admin/tournament/controller/TournamentAdminController.java
@@ -0,0 +1,36 @@
+package com.gg.server.admin.tournament.controller;
+
+import com.gg.server.admin.tournament.dto.TournamentAdminUpdateRequestDto;
+import com.gg.server.admin.tournament.service.TournamentAdminService;
+import javax.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("pingpong/admin")
+@Validated
+public class TournamentAdminController {
+    private final TournamentAdminService tournamentAdminService;
+
+    /**
+     * 토너먼트 정보 수정
+     * @param tournamentId 업데이트 하고자 하는 토너먼트 id
+     * @param tournamentAdminUpdateRequestDto 요청 데이터
+     * @return
+     */
+    @PatchMapping("/tournament/{tournamentId}")
+    public ResponseEntity updateTournamentInfo(@PathVariable Long tournamentId,
+        @Valid @RequestBody TournamentAdminUpdateRequestDto tournamentAdminUpdateRequestDto) {
+        tournamentAdminService.updateTournamentInfo(tournamentId, tournamentAdminUpdateRequestDto);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
