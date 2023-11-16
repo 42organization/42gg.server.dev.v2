3ef9461e697e0cd2aba72dd500b02610652de6ca
jahlee <jahlee@student.42seoul.kr>
2023-11-16 18:53:39 +0900

    :hammer: refactor: @Nested 로 리팩토링 및 매서드 이름 변경

--- src/test/java/com/gg/server/admin/tournament/service/TournamentAdminServiceTest.java
+++ src/test/java/com/gg/server/admin/tournament/service/TournamentAdminServiceTest.java
@@ -19,6 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -32,96 +33,105 @@ class TournamentAdminServiceTest {
     @InjectMocks
     TournamentAdminService tournamentAdminService;
 
-    @Test
+    @Nested
     @DisplayName("[Patch] /pingpong/admin/tournament/{tournamentId}")
-    public void 토너먼트_업데이트_성공() {
-        // given
-        List<Tournament> tournamentList = makeTournaments(1L, 2, getTargetTime(2, 1));
-        Tournament tournament = tournamentList.get(0);
-        TournamentAdminUpdateRequestDto updateRequestDto = makeTournamentAdminUpdateRequestDto(
-            getTargetTime(3, 1), getTargetTime(3, 3));
-        given(tournamentRepository.findById(1L)).willReturn(Optional.of(tournament));
-        given(tournamentRepository.findAllByStatus(TournamentStatus.BEFORE)).willReturn(tournamentList);
-        given(tournamentRepository.save(any(Tournament.class))).willReturn(tournament);
-        // when
-        Tournament changedTournament = tournamentAdminService.updateTournamentInfo(1L, updateRequestDto);
-        // then
-        assertThat(changedTournament.getId()).isEqualTo(tournament.getId());
-        assertThat(changedTournament.getTitle()).isEqualTo(updateRequestDto.getTitle());
-        assertThat(changedTournament.getContents()).isEqualTo(updateRequestDto.getContents());
-        assertThat(changedTournament.getStartTime()).isEqualTo(updateRequestDto.getStartTime());
-        assertThat(changedTournament.getEndTime()).isEqualTo(updateRequestDto.getEndTime());
-        assertThat(changedTournament.getType()).isEqualTo(updateRequestDto.getType());
-        assertThat(changedTournament.getStatus()).isEqualTo(tournament.getStatus());
+    class TournamentAdminServiceUpdateTest {
+        @Test
+        @DisplayName("토너먼트_업데이트_성공")
+        public void success() {
+            // given
+            List<Tournament> tournamentList = makeTournaments(1L, 2, getTargetTime(2, 1));
+            Tournament tournament = tournamentList.get(0);
+            TournamentAdminUpdateRequestDto updateRequestDto = makeTournamentAdminUpdateRequestDto(
+                getTargetTime(3, 1), getTargetTime(3, 3));
+            given(tournamentRepository.findById(1L)).willReturn(Optional.of(tournament));
+            given(tournamentRepository.findAllByStatus(TournamentStatus.BEFORE)).willReturn(tournamentList);
+            given(tournamentRepository.save(any(Tournament.class))).willReturn(tournament);
+            // when
+            Tournament changedTournament = tournamentAdminService.updateTournamentInfo(1L, updateRequestDto);
+            // then
+            assertThat(changedTournament.getId()).isEqualTo(tournament.getId());
+            assertThat(changedTournament.getTitle()).isEqualTo(updateRequestDto.getTitle());
+            assertThat(changedTournament.getContents()).isEqualTo(updateRequestDto.getContents());
+            assertThat(changedTournament.getStartTime()).isEqualTo(updateRequestDto.getStartTime());
+            assertThat(changedTournament.getEndTime()).isEqualTo(updateRequestDto.getEndTime());
+            assertThat(changedTournament.getType()).isEqualTo(updateRequestDto.getType());
+            assertThat(changedTournament.getStatus()).isEqualTo(tournament.getStatus());
+        }
+
+        @Test
+        @DisplayName("타겟_토너먼트_없음")
+        public void tournamentNotFound() {
+            // given
+            Tournament tournament = makeTournament(1234L, TournamentStatus.BEFORE,
+                getTargetTime(2, 1), getTargetTime(2, 3));
+            TournamentAdminUpdateRequestDto updateRequestDto = makeTournamentAdminUpdateRequestDto(
+                getTargetTime(2, 1), getTargetTime(2, 3));
+
+            given(tournamentRepository.findById(tournament.getId())).willReturn(Optional.empty());
+            // when, then
+            assertThatThrownBy(() -> tournamentAdminService.updateTournamentInfo(tournament.getId(), updateRequestDto))
+                .isInstanceOf(TournamentNotFoundException.class);
+        }
+
+        @Test
+        @DisplayName("토너먼트_업데이트_불가_상태")
+        public void canNotUpadate() {
+            // given
+            Tournament tournamentLive = makeTournament(1L, TournamentStatus.LIVE,
+                getTargetTime(0, -1), getTargetTime(0, 1));
+            Tournament tournamentEnd = makeTournament(2L, TournamentStatus.END,
+                getTargetTime(0, -3), getTargetTime(0, -1));
+            TournamentAdminUpdateRequestDto updateRequestDto = makeTournamentAdminUpdateRequestDto(
+                getTargetTime(2, 1), getTargetTime(2, 3));
+            given(tournamentRepository.findById(tournamentLive.getId())).willReturn(Optional.of(tournamentLive));
+            given(tournamentRepository.findById(tournamentEnd.getId())).willReturn(Optional.of(tournamentEnd));
+            // when, then
+            assertThatThrownBy(() -> tournamentAdminService.updateTournamentInfo(tournamentLive.getId(), updateRequestDto))
+                .isInstanceOf(TournamentUpdateException.class);
+            assertThatThrownBy(() -> tournamentAdminService.updateTournamentInfo(tournamentEnd.getId(), updateRequestDto))
+                .isInstanceOf(TournamentUpdateException.class);
+        }
+
+        @Test
+        @DisplayName("업데이트_토너먼트_Dto_Invalid_Time")
+        public void Dto_Invalid_Time() {
+            // given
+            Tournament tournament = makeTournament(1L, TournamentStatus.BEFORE,
+                getTargetTime(2, 1), getTargetTime(2, 3));
+            TournamentAdminUpdateRequestDto invalidRequestDto1 = makeTournamentAdminUpdateRequestDto(
+                getTargetTime(2, 3), getTargetTime(2, 1));
+            TournamentAdminUpdateRequestDto invalidRequestDto2 = makeTournamentAdminUpdateRequestDto(
+                invalidRequestDto1.getStartTime(), invalidRequestDto1.getStartTime());
+            TournamentAdminUpdateRequestDto invalidRequestDto3 = makeTournamentAdminUpdateRequestDto(
+                getTargetTime(2, -1), getTargetTime(2, 1));
+
+            given(tournamentRepository.findById(1L)).willReturn(Optional.of(tournament));
+            // when then
+            assertThatThrownBy(() -> tournamentAdminService.updateTournamentInfo(tournament.getId(), invalidRequestDto1))
+                .isInstanceOf(InvalidParameterException.class);
+            assertThatThrownBy(() -> tournamentAdminService.updateTournamentInfo(tournament.getId(), invalidRequestDto2))
+                .isInstanceOf(InvalidParameterException.class);
+            assertThatThrownBy(() -> tournamentAdminService.updateTournamentInfo(tournament.getId(), invalidRequestDto3))
+                .isInstanceOf(InvalidParameterException.class);
+        }
+
+        @Test
+        @DisplayName("Dto_기간_토너먼트_기간_겹침")
+        public void tournamentTimeConflict() {
+            // given
+            List<Tournament> tournamentList = makeTournaments(1L, 2, getTargetTime(2, 1));
+            Tournament tournament = tournamentList.get(0);
+            TournamentAdminUpdateRequestDto updateRequestDto = makeTournamentAdminUpdateRequestDto(
+                LocalDateTime.now().plusDays(2).plusHours(3), LocalDateTime.now().plusDays(2).plusHours(5));
+            given(tournamentRepository.findById(1L)).willReturn(Optional.of(tournament));
+            given(tournamentRepository.findAllByStatus(TournamentStatus.BEFORE)).willReturn(tournamentList);
+            // when, then
+            assertThatThrownBy(() -> tournamentAdminService.updateTournamentInfo(tournament.getId(), updateRequestDto))
+                .isInstanceOf(TournamentConflictException.class);
+        }
     }
 
-    @Test
-    public void 업데이트_토너먼트_없음() {
-        // given
-        Tournament tournament = makeTournament(1234L, TournamentStatus.BEFORE,
-            getTargetTime(2, 1), getTargetTime(2, 3));
-        TournamentAdminUpdateRequestDto updateRequestDto = makeTournamentAdminUpdateRequestDto(
-            getTargetTime(2, 1), getTargetTime(2, 3));
-
-        given(tournamentRepository.findById(tournament.getId())).willReturn(Optional.empty());
-        // when, then
-        assertThatThrownBy(() -> tournamentAdminService.updateTournamentInfo(tournament.getId(), updateRequestDto))
-            .isInstanceOf(TournamentNotFoundException.class);
-    }
-
-    @Test
-    public void 토너먼트_업데이트_불가_상태() {
-        // given
-        Tournament tournamentLive = makeTournament(1L, TournamentStatus.LIVE,
-            getTargetTime(0, -1), getTargetTime(0, 1));
-        Tournament tournamentEnd = makeTournament(2L, TournamentStatus.END,
-            getTargetTime(0, -3), getTargetTime(0, -1));
-        TournamentAdminUpdateRequestDto updateRequestDto = makeTournamentAdminUpdateRequestDto(
-            getTargetTime(2, 1), getTargetTime(2, 3));
-        given(tournamentRepository.findById(tournamentLive.getId())).willReturn(Optional.of(tournamentLive));
-        given(tournamentRepository.findById(tournamentEnd.getId())).willReturn(Optional.of(tournamentEnd));
-        // when, then
-        assertThatThrownBy(() -> tournamentAdminService.updateTournamentInfo(tournamentLive.getId(), updateRequestDto))
-            .isInstanceOf(TournamentUpdateException.class);
-        assertThatThrownBy(() -> tournamentAdminService.updateTournamentInfo(tournamentEnd.getId(), updateRequestDto))
-            .isInstanceOf(TournamentUpdateException.class);
-    }
-
-    @Test
-    public void 업데이트_토너먼트_Dto_Invalid_Time() {
-        // given
-        Tournament tournament = makeTournament(1L, TournamentStatus.BEFORE,
-            getTargetTime(2, 1), getTargetTime(2, 3));
-        TournamentAdminUpdateRequestDto invalidRequestDto1 = makeTournamentAdminUpdateRequestDto(
-            getTargetTime(2, 3), getTargetTime(2, 1));
-        TournamentAdminUpdateRequestDto invalidRequestDto2 = makeTournamentAdminUpdateRequestDto(
-            invalidRequestDto1.getStartTime(), invalidRequestDto1.getStartTime());
-        TournamentAdminUpdateRequestDto invalidRequestDto3 = makeTournamentAdminUpdateRequestDto(
-            getTargetTime(2, -1), getTargetTime(2, 1));
-
-        given(tournamentRepository.findById(1L)).willReturn(Optional.of(tournament));
-        // when then
-        assertThatThrownBy(() -> tournamentAdminService.updateTournamentInfo(tournament.getId(), invalidRequestDto1))
-            .isInstanceOf(InvalidParameterException.class);
-        assertThatThrownBy(() -> tournamentAdminService.updateTournamentInfo(tournament.getId(), invalidRequestDto2))
-            .isInstanceOf(InvalidParameterException.class);
-        assertThatThrownBy(() -> tournamentAdminService.updateTournamentInfo(tournament.getId(), invalidRequestDto3))
-            .isInstanceOf(InvalidParameterException.class);
-    }
-
-    @Test
-    public void Dto_기간_토너먼트_기간_겹침() {
-        // given
-        List<Tournament> tournamentList = makeTournaments(1L, 2, getTargetTime(2, 1));
-        Tournament tournament = tournamentList.get(0);
-        TournamentAdminUpdateRequestDto updateRequestDto = makeTournamentAdminUpdateRequestDto(
-            LocalDateTime.now().plusDays(2).plusHours(3), LocalDateTime.now().plusDays(2).plusHours(4));
-        given(tournamentRepository.findById(1L)).willReturn(Optional.of(tournament));
-        given(tournamentRepository.findAllByStatus(TournamentStatus.BEFORE)).willReturn(tournamentList);
-        // when, then
-        assertThatThrownBy(() -> tournamentAdminService.updateTournamentInfo(tournament.getId(), updateRequestDto))
-            .isInstanceOf(TournamentConflictException.class);
-    }
 
     /**
      * 현재 시간에서 days hours, 만큼 차이나는 시간을 구한다.
