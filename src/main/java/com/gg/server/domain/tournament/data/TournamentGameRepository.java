fd2c131f08c91f2918b38d5a43c09062dceec4a5
jahlee <jahlee@student.42seoul.kr>
2023-11-20 11:09:56 +0900

    :sparkles: feat: findAllByTournamentId 매서드 추가

--- src/main/java/com/gg/server/domain/tournament/data/TournamentGameRepository.java
+++ src/main/java/com/gg/server/domain/tournament/data/TournamentGameRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TournamentGameRepository extends JpaRepository<TournamentGame, Long> {
     List<TournamentGame> findAllByTournamentId(Long tournamentId);
+
 }
