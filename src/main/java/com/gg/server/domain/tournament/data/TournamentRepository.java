1712e93feb9bd0c00f6e147681bef6fc2e6fbafb
jahlee <jahlee@student.42seoul.kr>
2023-11-21 10:42:39 +0900

    :sparkles: feat: findAllByStatusBeforeAndLive 매서드 추가

--- src/main/java/com/gg/server/domain/tournament/data/TournamentRepository.java
+++ src/main/java/com/gg/server/domain/tournament/data/TournamentRepository.java
@@ -3,7 +3,11 @@ package com.gg.server.domain.tournament.data;
 import com.gg.server.domain.tournament.type.TournamentStatus;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TournamentRepository extends JpaRepository<Tournament, Long> {
     List<Tournament> findAllByStatus(TournamentStatus status);
+
+    @Query(value = "select t from Tournament t where  t.status='before' or t.status='live'")
+    List<Tournament> findAllByStatusBeforeAndLive();
 }
