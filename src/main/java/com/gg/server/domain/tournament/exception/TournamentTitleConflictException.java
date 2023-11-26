62725a908d791baf017a607e1757583e00991909
sujin <sujin@Mac.local>
2023-11-27 02:43:46 +0900

    (:sparkles:) feat: 토너먼트 제목 중복 exception 생성

---
+++ src/main/java/com/gg/server/domain/tournament/exception/TournamentTitleConflictException.java
@@ -0,0 +1,10 @@
+package com.gg.server.domain.tournament.exception;
+
+import com.gg.server.global.exception.ErrorCode;
+import com.gg.server.global.exception.custom.DuplicationException;
+
+public class TournamentTitleConflictException extends DuplicationException {
+    public TournamentTitleConflictException() {
+        super(ErrorCode.TOURNAMENT_TITLE_CONFLICT.getMessage(), ErrorCode.TOURNAMENT_TITLE_CONFLICT);
+    }
+}
