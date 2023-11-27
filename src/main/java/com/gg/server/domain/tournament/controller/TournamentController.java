8e6c838fcb2f678439a3d5a4a2cad710ab237022
kokomong2 <ko9603@naver.com>
2023-11-27 15:21:29 +0900

    :sparkles: feat: Tournament 리스트 조회 Api 작성
    Tournament Controller, Service 에 전체 조회 api 추가
    Tournament data 에 updateWinner 함수 추가

--- src/main/java/com/gg/server/domain/tournament/controller/TournamentController.java
+++ src/main/java/com/gg/server/domain/tournament/controller/TournamentController.java
@@ -1,11 +1,30 @@
 package com.gg.server.domain.tournament.controller;
 
+import com.gg.server.domain.tournament.dto.TournamentFilterRequestDto;
+import com.gg.server.domain.tournament.dto.TournamentListResponseDto;
+import com.gg.server.domain.tournament.service.TournamentService;
+import com.gg.server.global.dto.PageRequestDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/pingpong/tournament")
 public class TournamentController {
+
+    private final TournamentService tournamentService;
+
+    @GetMapping
+    TournamentListResponseDto getAllTournamentList(@Valid PageRequestDto pageRequestDto,
+                                                   @Valid TournamentFilterRequestDto tournamentFilterRequestDto){
+        PageRequest pageRequest = PageRequest.of(pageRequestDto.getPage() - 1, pageRequestDto.getSize());
+
+        return tournamentService.getAllTournamentList(pageRequest, tournamentFilterRequestDto.getType(), tournamentFilterRequestDto.getStatus());
+    }
 }
--- src/main/java/com/gg/server/domain/tournament/service/TournamentService.java
+++ src/main/java/com/gg/server/domain/tournament/service/TournamentService.java
@@ -1,9 +1,60 @@
 package com.gg.server.domain.tournament.service;
 
+import com.gg.server.domain.tournament.data.Tournament;
+import com.gg.server.domain.tournament.data.TournamentRepository;
+import com.gg.server.domain.tournament.data.TournamentUser;
+import com.gg.server.domain.tournament.dto.TournamentListResponseDto;
+import com.gg.server.domain.tournament.dto.TournamentResponseDto;
+import com.gg.server.domain.tournament.type.TournamentStatus;
+import com.gg.server.domain.tournament.type.TournamentType;
+import com.gg.server.domain.user.data.User;
+import com.gg.server.domain.user.dto.UserImageDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class TournamentService {
+    private final TournamentRepository tournamentRepository;
+
+    public TournamentListResponseDto getAllTournamentList(PageRequest pageRequest, String type, String status) {
+
+        Page<TournamentResponseDto> tournaments;
+
+        TournamentType tournamentType = TournamentType.getEnumFromValue(type);
+        TournamentStatus tournamentStatus = TournamentStatus.getEnumFromValue(status);
+
+        if (type == null && status == null) {
+            tournaments = tournamentRepository.findAll(pageRequest).
+                    map(o-> new TournamentResponseDto(o, findTournamentWinner(o), findPlayerCnt(o)));
+        } else if (type == null){
+            tournaments = tournamentRepository.findAllByStatus(tournamentStatus, pageRequest).
+                    map(o-> new TournamentResponseDto(o, findTournamentWinner(o), findPlayerCnt(o)));
+        } else if (status == null) {
+            tournaments = tournamentRepository.findAllByType(tournamentType, pageRequest).
+                    map(o-> new TournamentResponseDto(o, findTournamentWinner(o), findPlayerCnt(o)));
+        } else {
+            tournaments = tournamentRepository.findAllByTypeAndStatus(tournamentType, tournamentStatus, pageRequest).
+                    map(o-> new TournamentResponseDto(o, findTournamentWinner(o), findPlayerCnt(o)));
+        }
+        return new TournamentListResponseDto(tournaments.getContent(), tournaments.getTotalPages());
+    }
+
+    private UserImageDto findTournamentWinner(Tournament tournament) {
+        User winner = tournament.getWinner();
+        return new UserImageDto(winner);
+    }
+
+    private int findPlayerCnt(Tournament tournament) {
+        int player_cnt = 0;
+        for (TournamentUser tournamentUser : tournament.getTournamentUsers()) {
+            if (tournamentUser.isJoined()){
+                player_cnt++;
+            }
+        }
+        return player_cnt;
+    }
 }
