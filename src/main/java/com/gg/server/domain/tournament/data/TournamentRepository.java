25d6af52f40aee2094b0fc8c085a791e5b2f77d4
kokomong2 <ko9603@naver.com>
2023-11-27 15:26:25 +0900

    :sparkles: feat: Tournament list 조회를 위한 Repository, Dto 추가
    TournamentListResponseDto, TournamentResponseDto 추가
    TournamentRepository 에 status나 type 별로 리스트 찾는 jpa 문 추가

--- src/main/java/com/gg/server/domain/tournament/data/TournamentRepository.java
+++ src/main/java/com/gg/server/domain/tournament/data/TournamentRepository.java
@@ -1,12 +1,22 @@
 package com.gg.server.domain.tournament.data;
 
 import com.gg.server.domain.tournament.type.TournamentStatus;
-import java.util.List;
+import com.gg.server.domain.tournament.type.TournamentType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 public interface TournamentRepository extends JpaRepository<Tournament, Long> {
     List<Tournament> findAllByStatus(TournamentStatus status);
 
     List<Tournament> findAllByStatusIsNot(TournamentStatus status);
+
+    Page<Tournament> findAllByTypeAndStatus(@NotNull TournamentType type, @NotNull TournamentStatus status, Pageable pageable);
+
+    Page<Tournament> findAllByStatus(@NotNull TournamentStatus status, Pageable pageable);
+
+    Page<Tournament> findAllByType(@NotNull TournamentType type, Pageable pageable);
 }
