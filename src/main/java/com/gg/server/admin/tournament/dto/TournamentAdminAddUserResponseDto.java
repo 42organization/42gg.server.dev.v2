b614687c1af17395bc270c4bd686075999c2d8a0
이자훈 <jahlee@student.42seoul.kr>
2023-11-30 14:19:59 +0900

    :lipstick: style: 반환 dto 추가

---
+++ src/main/java/com/gg/server/admin/tournament/dto/TournamentAdminAddUserResponseDto.java
@@ -0,0 +1,22 @@
+package com.gg.server.admin.tournament.dto;
+
+import javax.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TournamentAdminAddUserResponseDto {
+    @NotNull
+    private Long userId;
+
+    @NotNull
+    private String intraId;
+
+    @NotNull
+    private boolean isJoined;
+
+}
