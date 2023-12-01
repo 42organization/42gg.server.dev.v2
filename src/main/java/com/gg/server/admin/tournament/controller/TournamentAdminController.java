27b2f3ccc012d1d2f6894979e935a40fb28f16b0
sujin <sujin@Mac.local>
2023-12-01 16:14:35 +0900

    :lipstick: style: 사용하지 않는 import 삭제

--- src/main/java/com/gg/server/admin/tournament/controller/TournamentAdminController.java
+++ src/main/java/com/gg/server/admin/tournament/controller/TournamentAdminController.java
@@ -15,8 +15,6 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.gg.server.admin.tournament.dto.TournamentAdminCreateRequestDto;
 import org.springframework.web.bind.annotation.PostMapping;
-import com.gg.server.admin.tournament.dto.TournamentCreateRequestDto;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
