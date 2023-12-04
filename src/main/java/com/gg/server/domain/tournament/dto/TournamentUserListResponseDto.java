4ce79243698fbcc10fc6e9646b2418af0409bb32
kokomong2 <ko9603@naver.com>
2023-12-04 11:02:44 +0900

    :sparkles: feat: TournamentUserResponseDto와 ListDto 추가

---
+++ src/main/java/com/gg/server/domain/tournament/dto/TournamentUserListResponseDto.java
@@ -0,0 +1,24 @@
+package com.gg.server.domain.tournament.dto;
+
+import com.gg.server.domain.tournament.data.TournamentUser;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+@Getter
+public class TournamentUserListResponseDto {
+    List<TournamentUserResponseDto> users;
+
+    public TournamentUserListResponseDto(List<TournamentUser> tournamentUsers){
+        users = new ArrayList<>();
+        for (TournamentUser tournamentUser : tournamentUsers) {
+            users.add(new TournamentUserResponseDto(tournamentUser));
+        }
+        users.sort(Comparator.comparing(TournamentUserResponseDto::getIsJoined).reversed().
+                thenComparing(TournamentUserResponseDto::getRegisteredDate));
+    }
+}
