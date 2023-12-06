582621a77f3362f5eef1e9445ba24ad612ffa24b
이자훈 <jahlee@student.42seoul.kr>
2023-12-06 14:10:40 +0900

    :sparkles: feat: 토너먼트 유저 참가 상태 responseDto 추가

---
+++ src/main/java/com/gg/server/domain/tournament/dto/TournamentCheckParticipationResponseDto.java
@@ -0,0 +1,14 @@
+package com.gg.server.domain.tournament.dto;
+
+
+import com.gg.server.domain.tournament.type.TournamentUserStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+@Getter
+public class TournamentCheckParticipationResponseDto {
+    private TournamentUserStatus status;
+}
