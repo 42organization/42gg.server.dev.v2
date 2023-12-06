367cb2a0870bc1c5432b6e705817a00ffc05acd4
Kimhan-nah <devHannahKim@gmail.com>
2023-12-06 12:21:33 +0900

    :sparkles: feat: TournamentConflictException default constructor 추가

--- src/main/java/com/gg/server/domain/tournament/exception/TournamentConflictException.java
+++ src/main/java/com/gg/server/domain/tournament/exception/TournamentConflictException.java
@@ -7,4 +7,8 @@ public class TournamentConflictException extends DuplicationException {
     public TournamentConflictException(String message, ErrorCode errorCode) {
         super(message, errorCode);
     }
+
+    public TournamentConflictException() {
+        super("이미 토너먼트가 존재합니다.", ErrorCode.TOURNAMENT_CONFLICT);
+    }
 }
