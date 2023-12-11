90318e51cd8b0704cf7dc38c90e324ecc1fd1cb8
AYoungSn <dudtjs0920@naver.com>
2023-12-11 13:30:36 +0900

    [FIX] #220 controller test 시 jpaauditing handler 의존성 이슈 -> JpaAuditing Config 분리

--- src/test/java/com/gg/server/domain/rank/controller/RankV2ControllerTest.java
+++ src/test/java/com/gg/server/domain/rank/controller/RankV2ControllerTest.java
@@ -1,21 +1,15 @@
 package com.gg.server.domain.rank.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import com.gg.server.domain.rank.data.RankRepository;
 import com.gg.server.domain.rank.dto.ExpRankPageResponseDto;
 import com.gg.server.domain.rank.dto.RankPageResponseDto;
 import com.gg.server.domain.rank.service.RankService;
 import com.gg.server.domain.user.data.User;
 import com.gg.server.domain.user.data.UserRepository;
 import com.gg.server.domain.user.dto.UserDto;
-import com.gg.server.global.dto.PageRequestDto;
+import com.gg.server.global.config.WebConfig;
 import com.gg.server.global.security.config.SecurityConfig;
 import com.gg.server.global.security.jwt.utils.AuthTokenProvider;
 import com.gg.server.global.security.jwt.utils.TokenAuthenticationFilter;
-import com.gg.server.global.security.service.CustomUserDetailsService;
-import com.gg.server.global.utils.querytracker.ApiQueryCounter;
-import com.gg.server.global.utils.querytracker.ApiQueryCounterAop;
 import com.gg.server.global.utils.querytracker.LoggingInterceptor;
 import java.util.ArrayList;
 import java.util.Optional;
@@ -23,22 +17,13 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
-import org.springframework.security.core.parameters.P;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
@@ -48,7 +33,9 @@ import static org.mockito.BDDMockito.*;
 @WebMvcTest(value = RankV2Controller.class,
     excludeFilters = {
         @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class),
-            @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = TokenAuthenticationFilter.class)
+            @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebConfig.class),
+            @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = TokenAuthenticationFilter.class),
+            @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = LoggingInterceptor.class)
     }
 )
 @ExtendWith(MockitoExtension.class)
@@ -61,15 +48,7 @@ class RankV2ControllerTest {
     @MockBean
     private UserRepository userRepository;
     @MockBean
-    private RankRepository rankRepository;
-    @MockBean
     private AuthTokenProvider authTokenProvider;
-    @MockBean
-    private ApiQueryCounter apiQueryCounter;
-    @MockBean
-    private CustomUserDetailsService customUserDetailsService;
-    @MockBean
-    private JpaMetamodelMappingContext jpaMetamodelMappingContext;
 
     @BeforeEach
     void setUp() {
