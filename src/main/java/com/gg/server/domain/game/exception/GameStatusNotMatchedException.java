9f589267142b5831787faa93dce93602f74dc277
sujin <sujin@Mac.local>
2023-12-11 21:10:30 +0900

    :sparkles: feat: GameStatusNotMatchedException 생성

---
+++ src/main/java/com/gg/server/domain/game/exception/GameStatusNotMatchedException.java
@@ -0,0 +1,10 @@
+package com.gg.server.domain.game.exception;
+
+import com.gg.server.global.exception.ErrorCode;
+import com.gg.server.global.exception.custom.BusinessException;
+
+public class GameStatusNotMatchedException extends BusinessException {
+    public GameStatusNotMatchedException() {
+        super("게임 상태 오류입니다.", ErrorCode.GAME_STATUS_NOT_MATCHED);
+    }
+}
