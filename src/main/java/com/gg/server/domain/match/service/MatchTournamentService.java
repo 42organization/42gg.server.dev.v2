dbc077171589fde8d29306a0c76a8277cdaf4c56
Kimhannah <skqkdldhf98@naver.com>
2023-12-12 14:58:03 +0900

    :green_heart: chore: MatchTournamentSerivce 추가

---
+++ src/main/java/com/gg/server/domain/match/service/MatchTournamentService.java
@@ -0,0 +1,11 @@
+package com.gg.server.domain.match.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 토너먼트 진행중 다음 라운드 게임 매칭
+ * 결승전 점수 입력 후 토너먼트 END 상태로 업데이트
+ */
+@Service
+public class MatchTournamentService {
+}
