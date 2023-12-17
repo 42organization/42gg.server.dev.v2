24daaf8bfab38f797e507afdd2970c11c51edd08
SeungChul Chung <wjdtmdcjf5@gmail.com>
2023-12-17 20:24:12 +0900

    🔨 [Refactoring] 개발 서버 ci/cd 설정 (#391)

---
+++ update-test-yml.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# yml 파일 경로
+YML_FILE="./src/main/resources/application-test.yml"
+
+# 변경된 내용을 임시 파일에 저장
+ENCODED_APPLICATION_YML=$(mktemp)
+cat "$YML_FILE" | base64 > "$ENCODED_APPLICATION_YML"
+
+# github secret에 저장
+cat "$ENCODED_APPLICATION_YML" | gh secret set -R 42organization/42gg.server.dev.v2 MIGRATE_APPLICATION_YML
+
+rm "$ENCODED_APPLICATION_YML"
