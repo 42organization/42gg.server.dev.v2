2cf7bae3ee73b8144fe7bbbe7eae2dadd71e7263
Kimhannah <skqkdldhf98@naver.com>
2023-12-17 19:45:44 +0900

    :bug: fix: 사용중인 enum 메서드 되돌리기

--- src/main/java/com/gg/server/domain/tournament/type/TournamentStatus.java
+++ src/main/java/com/gg/server/domain/tournament/type/TournamentStatus.java
@@ -1,7 +1,10 @@
 package com.gg.server.domain.tournament.type;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Locale;
 
 @Getter
 @RequiredArgsConstructor
@@ -12,4 +15,18 @@ public enum TournamentStatus {
 
     private final String code;
     private final String desc;
+
+
+    @JsonCreator
+    public static TournamentStatus getEnumFromValue(String value) {
+        if (value == null) return null;
+        for(TournamentStatus e : values()) {
+            if (e.name().equals(value)) {
+                return e;
+            } else if (e.code.toUpperCase(Locale.ROOT).equals(value.toUpperCase(Locale.ROOT))) {
+                return e;
+            }
+        }
+        return null;
+    }
 }
--- src/main/java/com/gg/server/domain/tournament/type/TournamentType.java
+++ src/main/java/com/gg/server/domain/tournament/type/TournamentType.java
@@ -1,7 +1,10 @@
 package com.gg.server.domain.tournament.type;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Locale;
 
 @Getter
 @RequiredArgsConstructor
@@ -12,4 +15,18 @@ public enum TournamentType {
 
     private final String code;
     private final String desc;
+
+
+    @JsonCreator
+    public static TournamentType getEnumFromValue(String value) {
+        if (value == null) return null;
+        for(TournamentType e : values()) {
+            if (e.name().equals(value)) {
+                return e;
+            } else if (e.code.toUpperCase(Locale.ROOT).equals(value.toUpperCase(Locale.ROOT))) {
+                return e;
+            }
+        }
+        return null;
+    }
 }
