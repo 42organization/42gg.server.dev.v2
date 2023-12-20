63a110e6993b21871a2d3ded1e410debf554a2f6
이자훈 <jahlee@student.42seoul.kr>
2023-12-20 21:00:20 +0900

    :hammer: refactor: 토너먼트 게임은 취소 안되게 로직 방어

--- src/main/java/com/gg/server/domain/match/service/MatchService.java
+++ src/main/java/com/gg/server/domain/match/service/MatchService.java
@@ -3,6 +3,7 @@ package com.gg.server.domain.match.service;
 import com.gg.server.domain.game.data.Game;
 import com.gg.server.domain.game.data.GameRepository;
 import com.gg.server.domain.game.exception.GameAlreadyExistException;
+import com.gg.server.domain.game.type.Mode;
 import com.gg.server.domain.game.type.StatusType;
 import com.gg.server.domain.match.data.RedisMatchTime;
 import com.gg.server.domain.match.data.RedisMatchTimeRepository;
@@ -29,6 +30,8 @@ import com.gg.server.domain.user.data.User;
 import com.gg.server.domain.user.data.UserRepository;
 import com.gg.server.domain.user.dto.UserDto;
 import com.gg.server.domain.user.exception.UserNotFoundException;
+import com.gg.server.global.exception.ErrorCode;
+import com.gg.server.global.exception.custom.BusinessException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -91,8 +94,11 @@ public class MatchService {
             if (enemyTeam.size() > 1) {
                 throw new SlotNotFoundException();
             }
+            if (game.get().getMode().equals(Mode.TOURNAMENT)) {
+                throw new BusinessException(ErrorCode.TOURNAMENT_GAME_CAN_NOT_CANCELED);
+            }
             cancelGame(userDto, startTime, game.get(), enemyTeam);
-        }else {
+        } else {
             deleteUserFromQueue(userDto, startTime);
         };
     }
