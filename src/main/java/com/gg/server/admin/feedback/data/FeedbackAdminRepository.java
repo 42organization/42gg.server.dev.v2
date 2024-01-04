8f66189e1f8d40427220e646643c92af7df7208f
이자훈 <jahlee@student.42seoul.kr>
2024-01-04 14:28:51 +0900

    :hammer: refactor: 페이징 쿼리문 로직 수정

--- src/main/java/com/gg/server/admin/feedback/data/FeedbackAdminRepository.java
+++ src/main/java/com/gg/server/admin/feedback/data/FeedbackAdminRepository.java
@@ -1,8 +1,13 @@
 package com.gg.server.admin.feedback.data;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.gg.server.domain.feedback.data.Feedback;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface FeedbackAdminRepository extends JpaRepository<Feedback, Long>, FeedbackAdminRepositorySearch {
+public interface FeedbackAdminRepository extends JpaRepository<Feedback, Long> {
+    @Query(value = "select f from Feedback f join fetch f.user " +
+        "where f.user.intraId = :intraId order by f.user.intraId asc, f.createdAt asc")
+    List<Feedback> findFeedbacksByUserIntraId(@Param("intraId") String intraId);
 }
