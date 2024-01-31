3db8bf1fd857664d5377dae4e702a8c6197f70d9
이자훈 <jahlee@student.42seoul.kr>
2024-01-31 19:44:26 +0900

    :lipstick: style: naver convention 적용

--- src/test/java/com/gg/server/admin/item/service/ItemAdminServiceUnitTest.java
+++ src/test/java/com/gg/server/admin/item/service/ItemAdminServiceUnitTest.java
@@ -1,11 +1,24 @@
 package com.gg.server.admin.item.service;
 
-import static com.gg.server.utils.ReflectionUtilsForUnitTest.setFieldWithReflection;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.*;
+import static com.gg.server.utils.ReflectionUtilsForUnitTest.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.gg.server.admin.item.data.ItemAdminRepository;
 import com.gg.server.admin.item.dto.ItemUpdateRequestDto;
@@ -19,154 +32,154 @@ import com.gg.server.domain.user.type.RoleType;
 import com.gg.server.domain.user.type.SnsType;
 import com.gg.server.global.utils.aws.AsyncNewItemImageUploader;
 import com.gg.server.utils.annotation.UnitTest;
-import java.util.ArrayList;
-import java.util.Optional;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
-import org.springframework.web.multipart.MultipartFile;
 
 @UnitTest
 @ExtendWith(MockitoExtension.class)
 class ItemAdminServiceUnitTest {
-    @Mock
-    ItemAdminRepository itemAdminRepository;
-    @Mock
-    AsyncNewItemImageUploader asyncNewItemImageUploader;
-    @InjectMocks
-    ItemAdminService itemAdminService;
+	@Mock
+	ItemAdminRepository itemAdminRepository;
+	@Mock
+	AsyncNewItemImageUploader asyncNewItemImageUploader;
+	@InjectMocks
+	ItemAdminService itemAdminService;
 
-    User user;
-    Item item;
+	User user;
+	Item item;
 
-    @BeforeEach
-    void beforeEach() {
-        item = new Item();
-        setFieldWithReflection(item, "isVisible", true);
-        user = new User("testUser", "", "", RacketType.NONE, RoleType.USER, 0, SnsType.NONE, 1L);
-    }
+	@BeforeEach
+	void beforeEach() {
+		item = new Item();
+		setFieldWithReflection(item, "isVisible", true);
+		user = new User("testUser", "", "", RacketType.NONE, RoleType.USER, 0, SnsType.NONE, 1L);
+	}
 
-    @Nested
-    @DisplayName("getAllItemHistoryTest")
-    class GetAllItemHistoryTest {
-        @Test
-        @DisplayName("success")
-        void success() {
-            // given
-            given(itemAdminRepository.findAll(any(Pageable.class))).willReturn(new PageImpl<>(new ArrayList<>()));
-            // when, then
-            itemAdminService.getAllItemHistory(mock(Pageable.class));
-            verify(itemAdminRepository, times(1)).findAll(any(Pageable.class));
-        }
-    }
+	@Nested
+	@DisplayName("getAllItemHistoryTest")
+	class GetAllItemHistoryTest {
+		@Test
+		@DisplayName("success")
+		void success() {
+			// given
+			given(itemAdminRepository.findAll(any(Pageable.class))).willReturn(new PageImpl<>(new ArrayList<>()));
+			// when, then
+			itemAdminService.getAllItemHistory(mock(Pageable.class));
+			verify(itemAdminRepository, times(1)).findAll(any(Pageable.class));
+		}
+	}
 
-    @Nested
-    @DisplayName("updateItemTest 파라미터 4개 짜리")
-    class UpdateItem4ParamsTest {
-        @Test
-        @DisplayName("success")
-        void success() throws Exception {
-            // given
-            given(itemAdminRepository.findById(any(Long.class))).willReturn(Optional.of(item));
-            given(itemAdminRepository.save(any(Item.class))).willReturn(mock(Item.class));
-            // when, then
-            itemAdminService.updateItem(1L, mock(ItemUpdateRequestDto.class), mock(MultipartFile.class), UserDto.from(user));
-            setFieldWithReflection(item, "isVisible", true);
-            itemAdminService.updateItem(1L, mock(ItemUpdateRequestDto.class), null, UserDto.from(user));
-            verify(itemAdminRepository, times(2)).findById(any(Long.class));
-            verify(asyncNewItemImageUploader, times(1)).upload(any(), any());
-            verify(itemAdminRepository, times(2)).save(any(Item.class));
-        }
-        @Test
-        @DisplayName("ItemNotAvailableTest")
-        void itemNotAvailableTest() throws Exception {
-            // given
-            setFieldWithReflection(item, "isVisible", false);
-            given(itemAdminRepository.findById(any(Long.class))).willReturn(Optional.of(item));
-            // when, then
-            assertThatThrownBy(()->itemAdminService.updateItem(1L, mock(ItemUpdateRequestDto.class), mock(MultipartFile.class), UserDto.from(user)))
-                .isInstanceOf(ItemNotAvailableException.class);
-            verify(itemAdminRepository, times(1)).findById(any(Long.class));
-        }
-        @Test
-        @DisplayName("ItemNotFoundTest")
-        void ItemNotFoundTest() throws Exception {
-            // given
-            given(itemAdminRepository.findById(any(Long.class))).willReturn(Optional.empty());
-            // when, then
-            assertThatThrownBy(()->itemAdminService.updateItem(1L, mock(ItemUpdateRequestDto.class), mock(MultipartFile.class), UserDto.from(user)))
-                .isInstanceOf(ItemNotFoundException.class);
-            verify(itemAdminRepository, times(1)).findById(any(Long.class));
-        }
-    }
+	@Nested
+	@DisplayName("updateItemTest 파라미터 4개 짜리")
+	class UpdateItem4ParamsTest {
+		@Test
+		@DisplayName("success")
+		void success() throws Exception {
+			// given
+			given(itemAdminRepository.findById(any(Long.class))).willReturn(Optional.of(item));
+			given(itemAdminRepository.save(any(Item.class))).willReturn(mock(Item.class));
+			// when, then
+			itemAdminService.updateItem(1L, mock(ItemUpdateRequestDto.class),
+				mock(MultipartFile.class), UserDto.from(user));
+			setFieldWithReflection(item, "isVisible", true);
+			itemAdminService.updateItem(1L, mock(ItemUpdateRequestDto.class),
+				null, UserDto.from(user));
+			verify(itemAdminRepository, times(2)).findById(any(Long.class));
+			verify(asyncNewItemImageUploader, times(1)).upload(any(), any());
+			verify(itemAdminRepository, times(2)).save(any(Item.class));
+		}
 
-    @Nested
-    @DisplayName("updateItemTest 파라미터 3개 짜리")
-    class UpdateItem3ParamsTest {
-        @Test
-        @DisplayName("success")
-        void success() throws Exception {
-            // given
-            given(itemAdminRepository.findById(any(Long.class))).willReturn(Optional.of(item));
-            given(itemAdminRepository.save(any(Item.class))).willReturn(mock(Item.class));
-            // when, then
-            itemAdminService.updateItem(1L, mock(ItemUpdateRequestDto.class), UserDto.from(user));
-            assertThat(item.getDeleterIntraId()).isEqualTo(user.getIntraId());
-            verify(itemAdminRepository, times(1)).findById(any(Long.class));
-            verify(itemAdminRepository, times(1)).save(any(Item.class));
-        }
-        @Test
-        @DisplayName("ItemNotAvailableTest")
-        void itemNotAvailableTest() throws Exception {
-            // given
-            setFieldWithReflection(item, "isVisible", false);
-            given(itemAdminRepository.findById(any(Long.class))).willReturn(Optional.of(item));
-            // when, then
-            assertThatThrownBy(()->itemAdminService.updateItem(1L, mock(ItemUpdateRequestDto.class), UserDto.from(user)))
-                .isInstanceOf(ItemNotAvailableException.class);
-            verify(itemAdminRepository, times(1)).findById(any(Long.class));
-        }
-        @Test
-        @DisplayName("ItemNotFoundTest")
-        void ItemNotFoundTest() throws Exception {
-            // given
-            given(itemAdminRepository.findById(any(Long.class))).willReturn(Optional.empty());
-            // when, then
-            assertThatThrownBy(()->itemAdminService.updateItem(1L, mock(ItemUpdateRequestDto.class), UserDto.from(user)))
-                .isInstanceOf(ItemNotFoundException.class);
-            verify(itemAdminRepository, times(1)).findById(any(Long.class));
-        }
-    }
+		@Test
+		@DisplayName("ItemNotAvailableTest")
+		void itemNotAvailableTest() throws Exception {
+			// given
+			setFieldWithReflection(item, "isVisible", false);
+			given(itemAdminRepository.findById(any(Long.class))).willReturn(Optional.of(item));
+			// when, then
+			assertThatThrownBy(
+				() -> itemAdminService.updateItem(1L, mock(ItemUpdateRequestDto.class), mock(MultipartFile.class),
+					UserDto.from(user)))
+				.isInstanceOf(ItemNotAvailableException.class);
+			verify(itemAdminRepository, times(1)).findById(any(Long.class));
+		}
 
-    @Nested
-    @DisplayName("deleteItemTest")
-    class DeleteItemTest {
-        @Test
-        @DisplayName("success")
-        void success() {
-            // given
-            given(itemAdminRepository.findById(any(Long.class))).willReturn(Optional.of(item));
-            // when, then
-            itemAdminService.deleteItem(1L, UserDto.from(user));
-            assertThat(item.getIsVisible()).isFalse();
-            verify(itemAdminRepository, times(1)).findById(any(Long.class));
-        }
-        @Test
-        @DisplayName("ItemNotFoundTest")
-        void itemNotFoundTest() {
-            // given
-            given(itemAdminRepository.findById(any(Long.class))).willReturn(Optional.empty());
-            // when, then
-            assertThatThrownBy(()->itemAdminService.deleteItem(1L, UserDto.from(user)))
-                .isInstanceOf(ItemNotFoundException.class);
-            verify(itemAdminRepository, times(1)).findById(any(Long.class));
-        }
-    }
+		@Test
+		@DisplayName("ItemNotFoundTest")
+		void itemNotFoundTest() throws Exception {
+			// given
+			given(itemAdminRepository.findById(any(Long.class))).willReturn(Optional.empty());
+			// when, then
+			assertThatThrownBy(
+				() -> itemAdminService.updateItem(1L, mock(ItemUpdateRequestDto.class), mock(MultipartFile.class),
+					UserDto.from(user)))
+				.isInstanceOf(ItemNotFoundException.class);
+			verify(itemAdminRepository, times(1)).findById(any(Long.class));
+		}
+	}
+
+	@Nested
+	@DisplayName("updateItemTest 파라미터 3개 짜리")
+	class UpdateItem3ParamsTest {
+		@Test
+		@DisplayName("success")
+		void success() throws Exception {
+			// given
+			given(itemAdminRepository.findById(any(Long.class))).willReturn(Optional.of(item));
+			given(itemAdminRepository.save(any(Item.class))).willReturn(mock(Item.class));
+			// when, then
+			itemAdminService.updateItem(1L, mock(ItemUpdateRequestDto.class), UserDto.from(user));
+			assertThat(item.getDeleterIntraId()).isEqualTo(user.getIntraId());
+			verify(itemAdminRepository, times(1)).findById(any(Long.class));
+			verify(itemAdminRepository, times(1)).save(any(Item.class));
+		}
+
+		@Test
+		@DisplayName("ItemNotAvailableTest")
+		void itemNotAvailableTest() throws Exception {
+			// given
+			setFieldWithReflection(item, "isVisible", false);
+			given(itemAdminRepository.findById(any(Long.class))).willReturn(Optional.of(item));
+			// when, then
+			assertThatThrownBy(
+				() -> itemAdminService.updateItem(1L, mock(ItemUpdateRequestDto.class), UserDto.from(user)))
+				.isInstanceOf(ItemNotAvailableException.class);
+			verify(itemAdminRepository, times(1)).findById(any(Long.class));
+		}
+
+		@Test
+		@DisplayName("ItemNotFoundTest")
+		void itemNotFoundTest() throws Exception {
+			// given
+			given(itemAdminRepository.findById(any(Long.class))).willReturn(Optional.empty());
+			// when, then
+			assertThatThrownBy(
+				() -> itemAdminService.updateItem(1L, mock(ItemUpdateRequestDto.class), UserDto.from(user)))
+				.isInstanceOf(ItemNotFoundException.class);
+			verify(itemAdminRepository, times(1)).findById(any(Long.class));
+		}
+	}
+
+	@Nested
+	@DisplayName("deleteItemTest")
+	class DeleteItemTest {
+		@Test
+		@DisplayName("success")
+		void success() {
+			// given
+			given(itemAdminRepository.findById(any(Long.class))).willReturn(Optional.of(item));
+			// when, then
+			itemAdminService.deleteItem(1L, UserDto.from(user));
+			assertThat(item.getIsVisible()).isFalse();
+			verify(itemAdminRepository, times(1)).findById(any(Long.class));
+		}
+
+		@Test
+		@DisplayName("ItemNotFoundTest")
+		void itemNotFoundTest() {
+			// given
+			given(itemAdminRepository.findById(any(Long.class))).willReturn(Optional.empty());
+			// when, then
+			assertThatThrownBy(() -> itemAdminService.deleteItem(1L, UserDto.from(user)))
+				.isInstanceOf(ItemNotFoundException.class);
+			verify(itemAdminRepository, times(1)).findById(any(Long.class));
+		}
+	}
 }
--- src/test/java/com/gg/server/domain/item/data/ItemUnitTest.java
+++ src/test/java/com/gg/server/domain/item/data/ItemUnitTest.java
@@ -1,48 +1,46 @@
 package com.gg.server.domain.item.data;
 
-import static com.gg.server.utils.ReflectionUtilsForUnitTest.setFieldWithReflection;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static com.gg.server.utils.ReflectionUtilsForUnitTest.*;
+import static org.assertj.core.api.Assertions.*;
 
-import com.gg.server.utils.ReflectionUtilsForUnitTest;
-import com.gg.server.utils.annotation.UnitTest;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.gg.server.utils.annotation.UnitTest;
+
 @UnitTest
 @ExtendWith(MockitoExtension.class)
 class ItemUnitTest {
-    @Nested
-    @DisplayName("imageUpdateTest")
-    class ImageUpdateTest {
-        @Test
-        @DisplayName("success")
-        void success() {
-            Item item = new Item();
-            String after = "after";
-            setFieldWithReflection(item, "imageUri", "before");
-            item.imageUpdate(after);
-            assertThat(item.getImageUri()).isEqualTo(after);
-        }
-    }
+	@Nested
+	@DisplayName("imageUpdateTest")
+	class ImageUpdateTest {
+		@Test
+		@DisplayName("success")
+		void success() {
+			Item item = new Item();
+			String after = "after";
+			setFieldWithReflection(item, "imageUri", "before");
+			item.imageUpdate(after);
+			assertThat(item.getImageUri()).isEqualTo(after);
+		}
+	}
 
-    @Nested
-    @DisplayName("setVisibilityTest")
-    class SetVisibilityTest {
-        @Test
-        @DisplayName("success")
-        void success() {
-            Item item = new Item();
-            String intraId = "intraId";
-            setFieldWithReflection(item, "isVisible", true);
-            item.setVisibility(intraId);
-            assertThat(item.getIsVisible()).isFalse();
-            assertThat(item.getDeleterIntraId()).isEqualTo(intraId);
-        }
-    }
+	@Nested
+	@DisplayName("setVisibilityTest")
+	class SetVisibilityTest {
+		@Test
+		@DisplayName("success")
+		void success() {
+			Item item = new Item();
+			String intraId = "intraId";
+			setFieldWithReflection(item, "isVisible", true);
+			item.setVisibility(intraId);
+			assertThat(item.getIsVisible()).isFalse();
+			assertThat(item.getDeleterIntraId()).isEqualTo(intraId);
+		}
+	}
 
 }
