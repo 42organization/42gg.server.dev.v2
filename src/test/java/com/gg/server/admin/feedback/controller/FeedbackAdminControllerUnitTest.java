5af57294856863539ee0fa2566792740cc15d22b
이자훈 <jahlee@student.42seoul.kr>
2024-01-31 19:36:15 +0900

    :lipstick: style: naver convention 적용

--- src/test/java/com/gg/server/admin/feedback/controller/FeedbackAdminControllerUnitTest.java
+++ src/test/java/com/gg/server/admin/feedback/controller/FeedbackAdminControllerUnitTest.java
@@ -1,13 +1,8 @@
 package com.gg.server.admin.feedback.controller;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
 
-import com.gg.server.admin.feedback.dto.FeedbackAdminPageRequestDto;
-import com.gg.server.admin.feedback.dto.FeedbackListAdminResponseDto;
-import com.gg.server.admin.feedback.service.FeedbackAdminService;
-import com.gg.server.utils.annotation.UnitTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -17,48 +12,55 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Pageable;
 
+import com.gg.server.admin.feedback.dto.FeedbackAdminPageRequestDto;
+import com.gg.server.admin.feedback.dto.FeedbackListAdminResponseDto;
+import com.gg.server.admin.feedback.service.FeedbackAdminService;
+import com.gg.server.utils.annotation.UnitTest;
+
 @UnitTest
 @ExtendWith(MockitoExtension.class)
 @DisplayName("FeedbackAdminControllerUnitTest")
 class FeedbackAdminControllerUnitTest {
-    @Mock
-    FeedbackAdminService feedbackAdminService;
-    @InjectMocks
-    FeedbackAdminController feedbackAdminController;
+	@Mock
+	FeedbackAdminService feedbackAdminService;
+	@InjectMocks
+	FeedbackAdminController feedbackAdminController;
 
-    @Nested
-    @DisplayName("FeedbackAllUnitTest")
-    class FeedbackAllUnitTest {
-        @Test
-        @DisplayName("successWithIntraId")
-        public void successWithIntraId() {
-            //given
-            FeedbackAdminPageRequestDto requestDto = new FeedbackAdminPageRequestDto("intraId", 2, 2);
-            given(feedbackAdminService.findByPartsOfIntraId(any(String.class), any(Pageable.class))).willReturn(mock(FeedbackListAdminResponseDto.class));
-            // when, then
-            feedbackAdminController.feedbackAll(requestDto);
-        }
+	@Nested
+	@DisplayName("FeedbackAllUnitTest")
+	class FeedbackAllUnitTest {
+		@Test
+		@DisplayName("successWithIntraId")
+		public void successWithIntraId() {
+			//given
+			FeedbackAdminPageRequestDto requestDto = new FeedbackAdminPageRequestDto("intraId", 2, 2);
+			given(feedbackAdminService.findByPartsOfIntraId(any(String.class), any(Pageable.class)))
+				.willReturn(mock(FeedbackListAdminResponseDto.class));
+			// when, then
+			feedbackAdminController.feedbackAll(requestDto);
+		}
 
-        @Test
-        @DisplayName("successWithoutIntraId")
-        public void successWithoutIntraId() {
-            //given
-            FeedbackAdminPageRequestDto requestDto = new FeedbackAdminPageRequestDto(null, 2, 2);
-            given(feedbackAdminService.findAllFeedback(any(Pageable.class))).willReturn(mock(FeedbackListAdminResponseDto.class));
-            // when, then
-            feedbackAdminController.feedbackAll(requestDto);
-        }
-    }
+		@Test
+		@DisplayName("successWithoutIntraId")
+		public void successWithoutIntraId() {
+			//given
+			FeedbackAdminPageRequestDto requestDto = new FeedbackAdminPageRequestDto(null, 2, 2);
+			given(feedbackAdminService.findAllFeedback(any(Pageable.class)))
+				.willReturn(mock(FeedbackListAdminResponseDto.class));
+			// when, then
+			feedbackAdminController.feedbackAll(requestDto);
+		}
+	}
 
-    @Nested
-    @DisplayName("FeedbackIsSolvedToggleUnitTest")
-    class FeedbackIsSolvedToggleUnitTest {
-        @Test
-        @DisplayName("success")
-        public void success() {
-            // given
-            // when, then
-            feedbackAdminController.feedbackIsSolvedToggle(1L);
-        }
-    }
+	@Nested
+	@DisplayName("FeedbackIsSolvedToggleUnitTest")
+	class FeedbackIsSolvedToggleUnitTest {
+		@Test
+		@DisplayName("success")
+		public void success() {
+			// given
+			// when, then
+			feedbackAdminController.feedbackIsSolvedToggle(1L);
+		}
+	}
 }
--- src/test/java/com/gg/server/domain/feedback/controller/FeedbackControllerUnitTest.java
+++ src/test/java/com/gg/server/domain/feedback/controller/FeedbackControllerUnitTest.java
@@ -1,11 +1,7 @@
 package com.gg.server.domain.feedback.controller;
 
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
-import com.gg.server.domain.feedback.dto.FeedbackRequestDto;
-import com.gg.server.domain.feedback.service.FeedbackService;
-import com.gg.server.domain.user.dto.UserDto;
-import com.gg.server.utils.annotation.UnitTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -14,25 +10,30 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.gg.server.domain.feedback.dto.FeedbackRequestDto;
+import com.gg.server.domain.feedback.service.FeedbackService;
+import com.gg.server.domain.user.dto.UserDto;
+import com.gg.server.utils.annotation.UnitTest;
+
 @UnitTest
 @ExtendWith(MockitoExtension.class)
 @DisplayName("FeedbackAdminServiceUnitTest")
 class FeedbackControllerUnitTest {
-    @Mock
-    FeedbackService feedbackService;
-    @InjectMocks
-    FeedbackController feedbackController;
+	@Mock
+	FeedbackService feedbackService;
+	@InjectMocks
+	FeedbackController feedbackController;
 
-    @Nested
-    @DisplayName("FeedbackSaveUnitTest")
-    class FeedbackSaveUnitTest {
-        @Test
-        @DisplayName("success")
-        void success() {
-            // given
-            UserDto userDto = UserDto.builder().id(1L).build();
-            // when, then
-            feedbackController.feedbackSave(mock(FeedbackRequestDto.class), userDto);
-        }
-    }
+	@Nested
+	@DisplayName("FeedbackSaveUnitTest")
+	class FeedbackSaveUnitTest {
+		@Test
+		@DisplayName("success")
+		void success() {
+			// given
+			UserDto userDto = UserDto.builder().id(1L).build();
+			// when, then
+			feedbackController.feedbackSave(mock(FeedbackRequestDto.class), userDto);
+		}
+	}
 }
