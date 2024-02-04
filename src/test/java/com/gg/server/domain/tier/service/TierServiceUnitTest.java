fbfc9730516cad8f6238481bab0fb229b06c10b4
middlefitting <wjdtmdcjf2@gmail.com>
2024-02-04 23:08:59 +0900

    test: updateAllTier 전체 경우 테스트

--- src/test/java/com/gg/server/domain/tier/service/TierServiceUnitTest.java
+++ src/test/java/com/gg/server/domain/tier/service/TierServiceUnitTest.java
@@ -2,6 +2,7 @@ package com.gg.server.domain.tier.service;
 
 import static org.mockito.Mockito.*;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -81,7 +82,7 @@ class TierServiceUnitTest {
 		void top3(int size) {
 			//Arrange
 			List<Rank> rankList = IntStream.range(0, size)
-				.mapToObj((i) -> new Rank(mock(User.class), season, 900 + 10 * i, 1, 0, "", mock(Tier.class)))
+				.mapToObj((i) -> new Rank(mock(User.class), season, 900 + 10 * i, 0, 1, "", mock(Tier.class)))
 				.sorted(Comparator.comparing(Rank::getPpp).reversed())
 				.collect(Collectors.toList());
 			when(rankRepository.findAllBySeasonIdOrderByPppDesc(any())).thenReturn(rankList);
@@ -95,6 +96,77 @@ class TierServiceUnitTest {
 			Assertions.assertThat(cnt).isEqualTo(Math.min(3, size));
 		}
 
+		/**
+		 * 개선 필요: 최상위 3명이 기본 ppp보다 낮을 경우 6번 티어가 0명이 된다.
+		 * 게임상 재현은 불가능하지만 코드상 버그로 판단.
+		 */
+		@ParameterizedTest
+		@DisplayName("전체 티어 조건 검증")
+		@ValueSource(ints = {100})
+		@MockitoSettings(strictness = Strictness.LENIENT)
+		void allTierCondition(int size) {
+			//Arrange
+			ArrayList<Rank> rankArrayList = IntStream.range(0, size)
+				.mapToObj((i) -> new Rank(mock(User.class), season, 900 + 10 * i, 1, 0, "", mock(Tier.class)))
+				.collect(Collectors.toCollection(ArrayList::new));
+			when(rankRepository.countRealRankPlayers(any())).thenReturn((long)size);
+
+			IntStream.range(0, 100)
+				.mapToObj((i) -> new Rank(mock(User.class), season, 1000, 0, 0, "", mock(Tier.class)))
+				.forEach(rankArrayList::add);
+			when(rankRepository.countRealRankPlayers(any())).thenReturn((long)size);
+
+			List<Rank> rankList = rankArrayList.stream()
+				.sorted(Comparator.comparing(Rank::getPpp).reversed())
+				.collect(Collectors.toList());
+			when(rankRepository.findAllBySeasonIdOrderByPppDesc(any())).thenReturn(rankList);
+
+			//Act
+			tierService.updateAllTier(season);
+
+			List<Rank> tier0 = rankList.stream()
+				.filter((rank) -> rank.getTier().equals(tiers.get(0)))
+				.collect(Collectors.toList());
+
+			List<Rank> tier1 = rankList.stream()
+				.filter((rank) -> rank.getTier().equals(tiers.get(1)))
+				.collect(Collectors.toList());
+
+			List<Rank> tier2 = rankList.stream()
+				.filter((rank) -> rank.getTier().equals(tiers.get(2)))
+				.collect(Collectors.toList());
+
+			List<Rank> tier3 = rankList.stream()
+				.filter((rank) -> rank.getTier().equals(tiers.get(3)))
+				.collect(Collectors.toList());
+
+			List<Rank> tier4 = rankList.stream()
+				.filter((rank) -> rank.getTier().equals(tiers.get(4)))
+				.sorted(Comparator.comparing(Rank::getPpp))
+				.collect(Collectors.toList());
+			Integer tier4MinPPP = tier4.get(0).getPpp();
+
+			List<Rank> tier5 = rankList.stream()
+				.filter((rank) -> rank.getTier().equals(tiers.get(5)))
+				.sorted(Comparator.comparing(Rank::getPpp))
+				.collect(Collectors.toList());
+			Integer tier5MinPPP = tier5.get(0).getPpp();
+
+			List<Rank> tier6 = rankList.stream()
+				.filter((rank) -> rank.getTier().equals(tiers.get(6)))
+				.sorted(Comparator.comparing(Rank::getPpp))
+				.collect(Collectors.toList());
+			Integer tier6MinPPP = tier6.get(0).getPpp();
+
+			//Assert
+			Assertions.assertThat(tier0).allMatch((rank) -> rank.getWins() == 0 && rank.getLosses() == 0);
+			Assertions.assertThat(tier1).allMatch((rank) -> rank.getPpp() < 970);
+			Assertions.assertThat(tier2).allMatch((rank) -> rank.getPpp() < 1010);
+			Assertions.assertThat(tier3).allMatch((rank) -> rank.getPpp() < tier4MinPPP);
+			Assertions.assertThat(tier4).allMatch((rank) -> rank.getPpp() < tier5MinPPP);
+			Assertions.assertThat(tier5).allMatch((rank) -> rank.getPpp() <= tier6MinPPP);
+			Assertions.assertThat(tier6.size()).isEqualTo(Math.min(3, size));
+		}
 	}
 
 }
