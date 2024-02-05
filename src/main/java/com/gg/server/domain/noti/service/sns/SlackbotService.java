34e533a928f7b1bc4b9a6307c5bd89d867efa9e1
Hyunkyu Lee <88573971+wken5577@users.noreply.github.com>
2024-02-05 21:24:17 +0900

    [Fix] refactoring api call slack api (#493)

--- src/main/java/com/gg/server/domain/noti/service/sns/SlackbotService.java
+++ src/main/java/com/gg/server/domain/noti/service/sns/SlackbotService.java
@@ -6,49 +6,36 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gg.server.data.noti.Noti;
 import com.gg.server.domain.noti.dto.UserNotiDto;
-import com.gg.server.domain.noti.exception.SlackJsonParseException;
 import com.gg.server.domain.noti.exception.SlackSendException;
-import com.gg.server.domain.noti.exception.SlackUserGetFailedException;
 import com.gg.server.domain.noti.service.NotiService;
 import com.gg.server.domain.user.dto.UserDto;
+import com.gg.server.global.utils.external.ApiUtil;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class SlackbotService {
 	@Value("${slack.xoxbToken}")
 	private String authenticationToken;
 
-	private final RestTemplate restTemplate;
-	private final ObjectMapper objectMapper;
 	private final NotiService notiService;
+	private final ApiUtil apiUtil;
 
-	public SlackbotService(RestTemplateBuilder builder, ObjectMapper objectMapper, NotiService notiService) {
-		this.restTemplate = builder.build();
-		this.objectMapper = objectMapper;
-		this.notiService = notiService;
-	}
-
-	private String getSlackUserId(String intraId) throws SlackUserGetFailedException {
+	private String getSlackUserId(String intraId) {
 		String userEmail = intraId + intraEmailSuffix;
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
@@ -57,40 +44,24 @@ public class SlackbotService {
 		MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
 		parameters.add("email", userEmail);
 
-		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(parameters, headers);
-
-		ResponseEntity<SlackUserInfoResponse> responseEntity = restTemplate
-			.exchange(userIdGetUrl, HttpMethod.POST, request, SlackUserInfoResponse.class);
-		if (!responseEntity.getBody().ok) {
-			throw new SlackUserGetFailedException();
-		}
-		return responseEntity.getBody().user.id;
+		SlackUserInfoResponse res = apiUtil.apiCall(userIdGetUrl, SlackUserInfoResponse.class,
+			headers, parameters, HttpMethod.POST);
+		return res.user.id;
 	}
 
-	private String getDmChannelId(String slackUserId) throws SlackJsonParseException {
+	private String getDmChannelId(String slackUserId) {
 		HttpHeaders httpHeaders = new HttpHeaders();
 		httpHeaders.add(HttpHeaders.AUTHORIZATION,
 			authenticationPrefix + authenticationToken);
 		httpHeaders.setContentType(MediaType.APPLICATION_JSON);
 
-		Map<String, String> map = new HashMap<>();
-		map.put("users", slackUserId);
-		String contentBody = null;
-		try {
-			contentBody = objectMapper.writeValueAsString(map);
-		} catch (JsonProcessingException e) {
-			throw new SlackJsonParseException();
-		}
+		Map<String, String> bodyMap = new HashMap<>();
+		bodyMap.put("users", slackUserId);
 
-		HttpEntity<String> entity = new HttpEntity<>(contentBody, httpHeaders);
+		ConversationResponse res = apiUtil.apiCall(conversationsUrl, ConversationResponse.class,
+			httpHeaders, bodyMap, HttpMethod.POST);
 
-		ResponseEntity<ConversationResponse> responseEntity = restTemplate
-			.exchange(conversationsUrl, HttpMethod.POST, entity, ConversationResponse.class);
-		if (!responseEntity.getBody().ok) {
-			log.error("fail to get user dm channel id");
-			throw new SlackUserGetFailedException();
-		}
-		return responseEntity.getBody().channel.id;
+		return res.channel.id;
 	}
 
 	@Async("asyncExecutor")
@@ -113,7 +84,7 @@ public class SlackbotService {
 		}
 	}
 
-	private void startSendNoti(String intraId, Noti noti) throws SlackSendException {
+	private void startSendNoti(String intraId, Noti noti) {
 		String slackUserId = getSlackUserId(intraId);
 		String slackChannelId = getDmChannelId(slackUserId);
 		String message = notiService.getMessage(noti);
@@ -126,21 +97,7 @@ public class SlackbotService {
 		Map<String, String> map = new HashMap<>();
 		map.put("channel", slackChannelId);
 		map.put("text", message);
-		String contentBody = null;
-		try {
-			contentBody = objectMapper.writeValueAsString(map);
-		} catch (JsonProcessingException e) {
-			log.error("start send Slack Noti", e);
-			throw new SlackJsonParseException();
-		}
-
-		HttpEntity<String> entity = new HttpEntity<>(contentBody, httpHeaders);
-
-		ResponseEntity<String> respEntity = restTemplate
-			.exchange(sendMessageUrl, HttpMethod.POST, entity, String.class);
-		if (respEntity.getStatusCode() != HttpStatus.OK) {
-			throw new SlackSendException();
-		}
+		apiUtil.apiCall(sendMessageUrl, String.class, httpHeaders, map, HttpMethod.POST);
 	}
 
 	@Getter
