defbad6474ffa9897b0038166d0db9c63af78c52
middlefitting <wjdtmdcjf2@gmail.com>
2024-02-09 01:22:07 +0900

    docs: UserAuthenticationService.regenerate 문서화

--- src/main/java/com/gg/server/domain/user/service/UserAuthenticationService.java
+++ src/main/java/com/gg/server/domain/user/service/UserAuthenticationService.java
@@ -14,6 +14,12 @@ public class UserAuthenticationService {
 	private final JwtRedisRepository jwtRedisRepository;
 	private final AuthTokenProvider tokenProvider;
 
+	/**
+	 * Refresh 토큰으로 Access 토큰을 재발급.
+	 * @throws TokenNotValidException 일치하는 Refresh 토큰 없는 경우
+	 * @param refreshToken Refresh 토큰
+	 * @return String Access 토큰
+	 */
 	public String regenerate(String refreshToken) {
 		Long userId = jwtRedisRepository.getUserIdFromRefToken(refreshToken);
 		if (userId == null) {
