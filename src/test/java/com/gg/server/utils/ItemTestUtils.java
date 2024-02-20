e1e1f1e9e9eab5b76bd08b5f0b2b43374351edf5
hannkim <skqkdldhf98@gmail.com>
2024-02-20 19:34:42 +0900

    🔨 [Refactor] Item entity에 dto 의존성 제거 (#586)

--- src/test/java/com/gg/server/utils/ItemTestUtils.java
+++ src/test/java/com/gg/server/utils/ItemTestUtils.java
@@ -50,11 +50,7 @@ public class ItemTestUtils {
 	 * 아이템을 생성한다.
 	 */
 	public Item createItem(User creator, ItemUpdateRequestDto updateRequestDto) {
-		Item item = Item.builder()
-			.creatorIntraId(creator.getIntraId())
-			.itemImageUri("42gg-s3")
-			.updateRequestDto(updateRequestDto)
-			.build();
+		Item item = updateRequestDto.toItem(creator.getIntraId(), "42gg-s3");
 		itemRepository.save(item);
 		return item;
 	}
