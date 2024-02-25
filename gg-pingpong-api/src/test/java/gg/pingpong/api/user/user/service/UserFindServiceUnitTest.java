4c5e265fdb72ee30bf5685cf9f386da22ee6b28a
middlefitting <wjdtmdcjf2@gmail.com>
2024-02-25 23:09:06 +0900

    test(UserFindService): getUserStatusMessage

--- gg-pingpong-api/src/test/java/gg/pingpong/api/user/user/service/UserFindServiceUnitTest.java
+++ gg-pingpong-api/src/test/java/gg/pingpong/api/user/user/service/UserFindServiceUnitTest.java
@@ -1,6 +1,7 @@
 package gg.pingpong.api.user.user.service;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
 
 import java.util.Optional;
 
@@ -13,10 +14,14 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import gg.pingpong.api.user.season.service.SeasonFindService;
+import gg.pingpong.data.rank.redis.RankRedis;
+import gg.pingpong.data.season.Season;
 import gg.pingpong.data.user.User;
 import gg.pingpong.repo.rank.redis.RankRedisRepository;
 import gg.pingpong.repo.user.UserRepository;
+import gg.pingpong.utils.RedisKeyManager;
 import gg.pingpong.utils.annotation.UnitTest;
+import gg.pingpong.utils.exception.rank.RedisDataNotFoundException;
 import gg.pingpong.utils.exception.user.UserNotFoundException;
 
 @UnitTest
@@ -53,7 +58,7 @@ class UserFindServiceUnitTest {
 		@DisplayName("유저 조회 실패 후 UserNotFoundException 반환")
 		void userNotFound() {
 			//Arrange
-			Mockito.when(userRepository.findById(Mockito.any(Long.class))).thenReturn(Optional.empty());
+			Mockito.when(userRepository.findById(any(Long.class))).thenReturn(Optional.empty());
 
 			//Act, Assert
 			assertThatThrownBy(() -> userFindService.findUserById(1L))
@@ -84,11 +89,65 @@ class UserFindServiceUnitTest {
 		void userNotFound() {
 			//Arrange
 			String intraId = "dummy";
-			Mockito.when(userRepository.findByIntraId(Mockito.any(String.class))).thenReturn(Optional.empty());
+			Mockito.when(userRepository.findByIntraId(any(String.class))).thenReturn(Optional.empty());
 
 			//Act, Assert
 			assertThatThrownBy(() -> userFindService.findByIntraId(intraId))
 				.isInstanceOf(UserNotFoundException.class);
+		}
+	}
+
+	@Nested
+	@DisplayName("GetUserStatusMessage")
+	class GetUserStatusMessage {
+		@Test
+		@DisplayName("조회 성공시 상태 메시지 반환")
+		void getStatusMessageSuccess() {
+			//Arrange
+			Long userId = 1L;
+			User targetUser = Mockito.mock(User.class);
+			Mockito.when(targetUser.getId()).thenReturn(userId);
+
+			Long seasonID = 1L;
+			Season currentSeason = Mockito.mock(Season.class);
+
+			String statusMessage = "helloWorld";
+			RankRedis rankRedis = Mockito.mock(RankRedis.class);
+			Mockito.when(rankRedis.getStatusMessage()).thenReturn(statusMessage);
+
+			Mockito.when(currentSeason.getId()).thenReturn(seasonID);
+			Mockito.when(seasonFindService.findCurrentSeason(any())).thenReturn(currentSeason);
+			Mockito.when(rankRedisRepository.findRankByUserId(RedisKeyManager.getHashKey(seasonID), userId))
+				.thenReturn(rankRedis);
+
+			//Act
+			String result = userFindService.getUserStatusMessage(targetUser);
+
+			//Assert
+			Assertions.assertThat(result).isEqualTo(statusMessage);
+		}
+
+		@Test
+		@DisplayName("랭크 조회 실패시 빈 문자열 반환")
+		void getStatusMessageFail() {
+			//Arrange
+			Long userId = 1L;
+			User targetUser = Mockito.mock(User.class);
+			Mockito.when(targetUser.getId()).thenReturn(userId);
+
+			Long seasonID = 1L;
+			Season currentSeason = Mockito.mock(Season.class);
+
+			Mockito.when(currentSeason.getId()).thenReturn(seasonID);
+			Mockito.when(seasonFindService.findCurrentSeason(any())).thenReturn(currentSeason);
+			Mockito.when(rankRedisRepository.findRankByUserId(RedisKeyManager.getHashKey(seasonID), userId))
+				.thenThrow(RedisDataNotFoundException.class);
+
+			//Act
+			String result = userFindService.getUserStatusMessage(targetUser);
+
+			//Assert
+			Assertions.assertThat(result).isEqualTo("");
 		}
 	}
 }
