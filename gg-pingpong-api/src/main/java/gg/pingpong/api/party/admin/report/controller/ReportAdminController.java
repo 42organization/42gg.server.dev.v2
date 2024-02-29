96b56c0b4d4441b5483393f069f92dbec40e2b50
yes-ee <yes0112@naver.com>
2024-02-29 18:43:44 +0900

    [FIX] pingpong에 중복된 party 폴더 삭제 (#632)

--- gg-pingpong-api/src/main/java/gg/pingpong/api/party/admin/report/controller/ReportAdminController.java
+++
@@ -1,7 +0,0 @@
-package gg.pingpong.api.party.admin.report.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class ReportAdminController {
-}
--- gg-pingpong-api/src/main/java/gg/pingpong/api/party/user/report/controller/ReportController.java
+++
@@ -1,7 +0,0 @@
-package gg.pingpong.api.party.user.report.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class ReportController {
-}
