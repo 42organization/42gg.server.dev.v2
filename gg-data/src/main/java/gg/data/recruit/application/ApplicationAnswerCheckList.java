32b9748b54de3acfe1cff7389f994c1e79b5d281
wken5577 <wken5577@hanmail.net>
2024-02-29 11:59:49 +0900

    [FEAT] #657 application domain entity

---
+++ gg-data/src/main/java/gg/data/recruit/application/ApplicationAnswerCheckList.java
@@ -0,0 +1,29 @@
+package gg.data.recruit.application;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import gg.data.BaseTimeEntity;
+import gg.data.recruit.recruitment.CheckList;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class ApplicationAnswerCheckList extends BaseTimeEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "application_answer_id")
+	private ApplicationAnswer applicationAnswerId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "check_list_id")
+	private CheckList checkListId;
+}
