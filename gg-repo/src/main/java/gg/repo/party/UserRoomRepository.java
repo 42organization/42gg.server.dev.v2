cdebab073341112ec1374500ca2ecb74d83c8483
yes-ee <yes0112@naver.com>
2024-03-04 16:27:04 +0900

    [REFACTOR] checkstyle 에러 수정 (#638)

--- gg-repo/src/main/java/gg/repo/party/UserRoomRepository.java
+++ gg-repo/src/main/java/gg/repo/party/UserRoomRepository.java
@@ -3,7 +3,6 @@ package gg.repo.party;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -15,11 +14,14 @@ import io.lettuce.core.dynamic.annotation.Param;
 
 public interface UserRoomRepository extends JpaRepository<UserRoom, Long> {
 	List<UserRoom> findByUserId(Long userId);
+
 	List<UserRoom> findByRoomId(Long userId);
+
 	Optional<UserRoom> findByUserAndRoom(User user, Room room);
 
 	@Query("SELECT ur.room FROM UserRoom ur WHERE ur.user.id = :userId AND ur.isExist = true "
 		+ "AND ur.room.status = :status ORDER BY ur.room.dueDate ASC")
 	List<Room> findFinishRoomsByUserId(@Param("userId") Long userId, @Param("status") RoomType status);
+
 	Optional<UserRoom> findByUserIdAndRoomIdAndIsExistTrue(Long userId, Long roomId);
 }
