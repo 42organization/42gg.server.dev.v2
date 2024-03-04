5fd764609f8647cd4a719fab1c8a6a52cd1d0341
AreSain <tmdtm9949@gmail.com>
2024-03-04 16:18:04 +0900

    [fix] Error코드 수정

--- gg-pingpong-api/src/main/java/gg/party/api/user/comment/service/CommentService.java
+++ gg-pingpong-api/src/main/java/gg/party/api/user/comment/service/CommentService.java
@@ -37,7 +37,7 @@ public class CommentService {
 	@Transactional
 	public void createComment(Long roomId, CommentCreateReqDto reqDto, Long userId) {
 		Room room = roomRepository.findById(roomId)
-			.orElseThrow(() -> new RoomNotFoundException(ErrorCode.ROOM_NOT_FOUND));
+			.orElseThrow(() -> new RoomNotFoundException("방을 찾을 수 없습니다."));
 		if (LocalDateTime.now().isAfter(room.getDueDate())) {
 			throw new RoomUpdateException(ErrorCode.ROOM_FINISHED);
 		}
