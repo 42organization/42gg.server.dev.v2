5bcb1e1742f55831f0e633a2dbe11d2546fe0614
wken5577 <wken5577@hanmail.net>
2024-03-06 16:24:10 +0900

    [FEAT] #688 api module add

---
+++ gg-recruit-api/src/main/java/gg/recruit/api/TestController.java
@@ -0,0 +1,13 @@
+package gg.recruit.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping("/test")
+	public String test() {
+		return "test";
+	}
+}
