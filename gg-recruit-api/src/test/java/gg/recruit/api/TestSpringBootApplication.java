230ddc293f99ab0608403a22d0a25d489b707929
wken5577 <wken5577@hanmail.net>
2024-03-07 20:13:29 +0900

    [FIX] #558 checkstyle fix

--- gg-recruit-api/src/test/java/gg/recruit/api/TestSpringBootApplication.java
+++ gg-recruit-api/src/test/java/gg/recruit/api/TestSpringBootApplication.java
@@ -2,7 +2,7 @@ package gg.recruit.api;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"gg.recruit.api", "gg.utils", "gg.data", "gg.repo", "gg.admin.repo"
-	, "gg.auth", "gg.pingpong.api"})
+@SpringBootApplication(scanBasePackages = {"gg.recruit.api", "gg.utils", "gg.data", "gg.repo",
+	"gg.admin.repo", "gg.auth", "gg.pingpong.api"})
 public class TestSpringBootApplication {
 }
