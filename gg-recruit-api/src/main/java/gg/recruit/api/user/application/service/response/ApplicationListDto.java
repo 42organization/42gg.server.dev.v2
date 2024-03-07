cb1335b45652d17dc03b39279931d54e6ceec99d
wken5577 <wken5577@hanmail.net>
2024-03-07 14:05:04 +0900

    [FIX] #558 fix checkstyle, test

--- gg-recruit-api/src/main/java/gg/recruit/api/user/application/service/response/ApplicationListDto.java
+++ gg-recruit-api/src/main/java/gg/recruit/api/user/application/service/response/ApplicationListDto.java
@@ -1,6 +1,7 @@
 package gg.recruit.api.user.application.service.response;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import gg.data.recruit.application.Application;
 import lombok.Getter;
@@ -10,6 +11,7 @@ public class ApplicationListDto {
 	List<ApplicationDto> applications;
 
 	public ApplicationListDto(List<Application> applications) {
-		this.applications = applications.stream().map(ApplicationDto::new).toList();
+		this.applications = applications.stream().map(ApplicationDto::new)
+			.collect(Collectors.toList());
 	}
 }
