12c2a9d7d96e3d61eb43a014e5327d109139d7b3
jungjkim <jjkimfplc@gmail.com>
2024-03-07 17:26:48 +0900

    [refactor] 템플릿 error code 위치 수정

--- gg-utils/src/main/java/gg/utils/exception/ErrorCode.java
+++ gg-utils/src/main/java/gg/utils/exception/ErrorCode.java
@@ -159,13 +159,13 @@ public enum ErrorCode {
 	ROOM_REPORTED_ERROR(404, "PT103", "신고 상태로 접근이 불가능합니다."),
 	COMMENT_NOT_FOUND(404, "PT104", "존재하지 않는 댓글입니다."),
 	ROOMSTAT_NOT_FOUND(404, "PT105", "존재하지 않는 방 status입니다."),
+	TEMPLATE_NOT_FOUND(404, "PT106", "존재하지 않는 템플릿 입니다."),
 	ROOM_NOT_ENOUGH_PEOPLE(400, "PT201", "시작할 수 있는 인원이 아닙니다."),
 	USER_NOT_HOST(400, "PT203", "방장이 아닙니다"),
 	ROOM_SAME_STATUS(400, "PT204", "이미 처리된 방 입니다."),
 	ROOM_NOT_OPEN(400, "PT205", "모집중인 방이 아닙니다."),
 	ROOM_NOT_PARTICIPANT(400, "PT206", "참여하지 않은 방 입니다."),
-	USER_ALREADY_IN_ROOM(409, "PT301", "이미 참여한 방 입니다."),
-	TEMPLATE_NOT_FOUND(404, "PT106", "존재하지 않는 템플릿 입니다.");
+	USER_ALREADY_IN_ROOM(409, "PT301", "이미 참여한 방 입니다.");
 
 
 	private final int status;
