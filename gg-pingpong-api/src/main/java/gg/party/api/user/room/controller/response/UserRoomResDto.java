345996772b1bc05a0aab8644fb0014353d3a059b
AreSain <tmdtm9949@gmail.com>
2024-03-08 16:56:30 +0900

    [feature] 상세조회 복면가왕 API 추가

--- gg-pingpong-api/src/main/java/gg/party/api/user/room/controller/response/UserRoomResDto.java
+++ gg-pingpong-api/src/main/java/gg/party/api/user/room/controller/response/UserRoomResDto.java
@@ -8,9 +8,17 @@ import lombok.NoArgsConstructor;
 public class UserRoomResDto {
 	private Long roomUserId;
 	private String nickname;
+	private String intraId;
 
 	public UserRoomResDto(Long id, String nickname) {
 		this.roomUserId = id;
 		this.nickname = nickname;
+		this.intraId = null;
+	}
+
+	public UserRoomResDto(Long id, String nickname, String intraId) {
+		this.roomUserId = id;
+		this.nickname = nickname;
+		this.intraId = intraId;
 	}
 }
