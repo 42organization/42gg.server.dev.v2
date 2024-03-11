512ea32122589aeb9c4876caf04b2d9ba772d8bd
seungsje <111065574+AreSain@users.noreply.github.com>
2024-03-11 16:30:19 +0900

    [feature] Admin 방 상세 조회 api 작성 (#717)

--- gg-pingpong-api/src/main/java/gg/party/api/admin/room/controller/response/AdminCommentResDto.java
+++ gg-pingpong-api/src/main/java/gg/party/api/admin/room/controller/response/AdminCommentResDto.java
@@ -1,4 +1,4 @@
-package gg.party.api.user.room.controller.response;
+package gg.party.api.admin.room.controller.response;
 
 import java.time.LocalDateTime;
 
@@ -8,16 +8,20 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Getter
-public class CommentResDto {
+public class AdminCommentResDto {
 	private Long commentId;
 	private String nickname;
+	private String intraId;
+	private Boolean isExist;
 	private String content;
 	private Boolean isHidden;
 	private LocalDateTime createDate;
 
-	public CommentResDto(Comment comment) {
+	public AdminCommentResDto(Comment comment) {
 		this.commentId = comment.getId();
 		this.nickname = comment.getUserRoom().getNickname();
+		this.intraId = comment.getUser().getIntraId();
+		this.isExist = comment.getUserRoom().getIsExist();
 		this.content = comment.getContent();
 		this.isHidden = comment.isHidden();
 		this.createDate = comment.getCreatedAt();
