50ec4d1a18bd1f80d5b4f56b4aa3047c71299c89
Kimhannah <skqkdldhf98@naver.com>
2024-03-12 22:54:44 +0900

    :test_tube: test: makeMatch unit test

--- gg-pingpong-api/src/test/java/gg/pingpong/api/user/match/service/MatchServiceUnitTest.java
+++ gg-pingpong-api/src/test/java/gg/pingpong/api/user/match/service/MatchServiceUnitTest.java
@@ -1,11 +1,30 @@
 package gg.pingpong.api.user.match.service;
 
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 
+import gg.auth.UserDto;
+import gg.data.game.type.StatusType;
+import gg.data.manage.SlotManagement;
+import gg.data.match.RedisMatchUser;
+import gg.data.match.type.Option;
+import gg.data.rank.redis.RankRedis;
+import gg.data.season.Season;
+import gg.data.tournament.type.TournamentStatus;
 import gg.pingpong.api.user.manage.service.PenaltyService;
+import gg.pingpong.api.user.match.dto.GameAddDto;
 import gg.pingpong.api.user.season.service.SeasonFindService;
 import gg.repo.game.GameRepository;
 import gg.repo.manage.SlotManagementRepository;
@@ -44,18 +63,85 @@ public class MatchServiceUnitTest {
 	/**
 	 * makeMatch
 	 */
-	@Test
-	@DisplayName("")
-	void makeMatch() {
+	@Nested
+	@DisplayName("makeMatch() : 매칭큐에 RANK 옵션으로 신청한 유저 1명 있을 때")
+	class MatchTest {
+		UserDto userDto;
+		LocalDateTime startTime;
+		Season season;
+		RankRedis rank;
+		List<RedisMatchUser> allMatchUsers;
 
-	}
+		@BeforeEach
+		void setUp() {
+			userDto = Mockito.mock(UserDto.class);
+			startTime = LocalDateTime.of(2021, 1, 1, 0, 0);
+			season = Season.builder().pppGap(0).build();
+			rank = Mockito.mock(RankRedis.class);
+			allMatchUsers = List.of(new RedisMatchUser(2L, 1000, Option.RANK));
 
-	/**
-	 * cancelMatch
-	 */
-	@Test
-	@DisplayName("")
-	void cancelMatch() {
+			// checkValid
+			given(penaltyService.isPenaltyUser(userDto.getIntraId())).willReturn(false);
+			given(tournamentRepository.findAllByStatusIsNot(TournamentStatus.END)).willReturn(List.of());
+			SlotManagement slotManagment = SlotManagement.builder().gameInterval(15).build();
+			given(slotManagementRepository.findCurrent(any(LocalDateTime.class))).willReturn(
+				Optional.of(slotManagment));
+			given(gameRepository.findByStartTime(any(LocalDateTime.class))).willReturn(Optional.empty());
+			given(gameRepository.findByStatusTypeAndUserId(any(StatusType.class), anyLong())).willReturn(
+				Optional.empty());
+			given(redisMatchUserRepository.getUserTime(anyLong(), any(LocalDateTime.class))).willReturn(
+				Optional.empty());
+			given(redisMatchUserRepository.countMatchTime(anyLong())).willReturn(0);
+
+			// makeMatch
+			given(seasonFindService.findCurrentSeason(startTime)).willReturn(season);
+			given(rankRedisRepository.findRankByUserId(anyString(), anyLong())).willReturn(rank);
+			given(redisMatchTimeRepository.getAllMatchUsers(startTime)).willReturn(allMatchUsers);
+		}
+
+		@Test
+		@DisplayName("BOTH로 매칭 신청했을 때 season의 pppGap 차이로 매칭이 안 되고 매칭큐에 넣어준다.")
+		void addQueue1() {
+			// when
+			matchService.makeMatch(userDto, Option.BOTH, startTime);
+
+			// then
+			verify(redisMatchTimeRepository, times(1)).addMatchUser(any(LocalDateTime.class),
+				any(RedisMatchUser.class));
+			verify(redisMatchTimeRepository, times(1)).setMatchTimeWithExpiry(startTime);
+			verify(redisMatchUserRepository, times(1)).addMatchTime(anyLong(), any(LocalDateTime.class),
+				any(Option.class));
+		}
+
+		@Test
+		@DisplayName("BOTH로 매칭 신청했을 때 게임이 매칭된다.")
+		void makeMatch() {
+			// given
+			season.setPppGap(2000);
+			given(seasonFindService.findCurrentSeason(startTime)).willReturn(season);
+
+			// when
+			matchService.makeMatch(userDto, Option.BOTH, startTime);
+
+			// then
+			verify(gameUpdateService, times(1)).make(any(GameAddDto.class), anyLong());
+			verify(redisMatchTimeRepository, times(1)).addMatchUser(any(LocalDateTime.class),
+				any(RedisMatchUser.class));
+		}
+
+		@Test
+		@DisplayName("NORMAL로 신청하면 유저를 매칭큐에 넣어준다.")
+		void putQueue() {
+			// when
+			matchService.makeMatch(userDto, Option.NORMAL, startTime);
+
+			// then
+			verify(redisMatchTimeRepository, times(1)).addMatchUser(any(LocalDateTime.class),
+				any(RedisMatchUser.class));
+			verify(redisMatchTimeRepository, times(1)).setMatchTimeWithExpiry(startTime);
+			verify(redisMatchUserRepository, times(1)).addMatchTime(anyLong(), any(LocalDateTime.class),
+				any(Option.class));
+		}
 
 	}
 }
