3a43c69db2bbb6c8e41767d43ddf8764866a286a
기현(gkwon) <79272189+ghyen@users.noreply.github.com>
2024-03-13 17:53:18 +0900

    Update ErrorCode.java
    [FIX] errorcode fix

--- gg-utils/src/main/java/gg/utils/exception/ErrorCode.java
+++ gg-utils/src/main/java/gg/utils/exception/ErrorCode.java
@@ -178,10 +178,10 @@ public enum ErrorCode {
 	ROOM_NOT_OPEN(400, "PT205", "모집중인 방이 아닙니다."),
 	ROOM_NOT_PARTICIPANT(400, "PT206", "참여하지 않은 방 입니다."),
 	CATEGORY_DUPLICATE(400, "PT207", "중복된 카테고리 입니다."),
-	ON_PENALTY(403, "PT208", "패널티 상태입니다."),
 	USER_ALREADY_IN_ROOM(409, "PT301", "이미 참여한 방 입니다."),
 	ALREADY_REPORTED(409, "PT302", "이미 신고한 요청입니다."),
-	SELF_REPORT(409, "PT303", "자신을 신고할 수 없습니다.");
+	SELF_REPORT(409, "PT303", "자신을 신고할 수 없습니다."),
+	ON_PENALTY(403, "PT501", "패널티 상태입니다.");
 
 	private final int status;
 	private final String errCode;
