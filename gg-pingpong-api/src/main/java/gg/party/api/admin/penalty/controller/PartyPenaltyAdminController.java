9d3fdd718238020934f6737cef951a2350498088
seungsje <seungsje@student.42seoul.kr>
2024-03-19 11:00:22 +0900

    [fix] 컨플릭트 해결

--- gg-pingpong-api/src/main/java/gg/party/api/admin/penalty/controller/PartyPenaltyAdminController.java
+++ gg-pingpong-api/src/main/java/gg/party/api/admin/penalty/controller/PartyPenaltyAdminController.java
@@ -1,15 +1,18 @@
-package gg.party.api.admin.penalty.controller;
-
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import gg.party.api.admin.penalty.controller.request.PageReqDto;
+import gg.party.api.admin.penalty.controller.request.PartyPenaltyAdminReqDto;
 import gg.party.api.admin.penalty.controller.response.PartyPenaltyListAdminResDto;
 import gg.party.api.admin.penalty.service.PartyPenaltyAdminService;
 import lombok.RequiredArgsConstructor;
@@ -30,4 +33,27 @@ public class PartyPenaltyAdminController {
 		return ResponseEntity.status(HttpStatus.OK).body(penaltyAdminService.findAllPenalty(reqDto));
 	}
 
+	private final PartyPenaltyAdminService partyPenaltyAdminService;
+
+	/**
+	 * 패널티 부여
+	 */
+	@PostMapping()
+	public ResponseEntity<Void> addAdminPenalty(
+		@RequestBody PartyPenaltyAdminReqDto reqDto) {
+		partyPenaltyAdminService.addAdminPenalty(reqDto);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+
+	/**
+	 * 패널티 수정
+	 *
+	 * @param penaltyId 패널티 id
+	 */
+	@PatchMapping("/{penaltyId}")
+	public ResponseEntity<Void> modifyAdminPenalty(@PathVariable Long penaltyId,
+		@RequestBody PartyPenaltyAdminReqDto reqDto) {
+		partyPenaltyAdminService.modifyAdminPenalty(penaltyId, reqDto);
+		return ResponseEntity.status(HttpStatus.OK).build();
+	}
 }
--- gg-pingpong-api/src/main/java/gg/party/api/admin/penalty/service/PartyPenaltyAdminService.java
+++ gg-pingpong-api/src/main/java/gg/party/api/admin/penalty/service/PartyPenaltyAdminService.java
@@ -1,5 +1,6 @@
 package gg.party.api.admin.penalty.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
@@ -10,16 +11,23 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import gg.admin.repo.penalty.PartyPenaltyAdminRepository;
+import gg.data.party.PartyPenalty;
+import gg.data.user.User;
 import gg.party.api.admin.penalty.controller.request.PageReqDto;
+import gg.party.api.admin.penalty.controller.request.PartyPenaltyAdminReqDto;
 import gg.party.api.admin.penalty.controller.response.PartyPenaltyAdminResDto;
 import gg.party.api.admin.penalty.controller.response.PartyPenaltyListAdminResDto;
+import gg.repo.user.UserRepository;
+import gg.utils.exception.party.PartyPenaltyNotFoundException;
+import gg.utils.exception.user.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class PartyPenaltyAdminService {
 
-	private final PartyPenaltyAdminRepository penaltyAdminRepository;
+	private final PartyPenaltyAdminRepository partyPenaltyAdminRepository;
+	private final UserRepository userRepository;
 
 	/**
 	 * 패널티 전체 조회
@@ -33,7 +41,7 @@ public class PartyPenaltyAdminService {
 
 		Pageable pageable = PageRequest.of(page - 1, size, Sort.by(Sort.Direction.DESC, "createdAt"));
 
-		Page<PartyPenaltyAdminResDto> penaltyPage = penaltyAdminRepository.findAll(pageable)
+		Page<PartyPenaltyAdminResDto> penaltyPage = partyPenaltyAdminRepository.findAll(pageable)
 			.map(PartyPenaltyAdminResDto::new);
 
 		List<PartyPenaltyAdminResDto> penaltyList = penaltyPage.getContent();
@@ -41,4 +49,27 @@ public class PartyPenaltyAdminService {
 		return new PartyPenaltyListAdminResDto(penaltyList, penaltyPage.getTotalPages());
 	}
 
+	/**
+	 * 패널티 수정
+	 *
+	 * @param penaltyId 패널티 번호
+	 * @throws PartyPenaltyNotFoundException 유효하지 않은 패널티
+	 */
+	@Transactional
+	public void modifyAdminPenalty(Long penaltyId, PartyPenaltyAdminReqDto reqDto) {
+		PartyPenalty partyPenalty = partyPenaltyAdminRepository.findById(penaltyId)
+			.orElseThrow(PartyPenaltyNotFoundException::new);
+		partyPenalty.update(reqDto.getPenaltyType(), reqDto.getMessage(), reqDto.getPenaltyTime());
+	}
+
+	/**
+	 * 패널티 부여
+	 */
+	@Transactional
+	public void addAdminPenalty(PartyPenaltyAdminReqDto reqDto) {
+		User userEntity = userRepository.findById(reqDto.getReportee().getId()).orElseThrow(UserNotFoundException::new);
+		LocalDateTime startTime = LocalDateTime.now();
+		partyPenaltyAdminRepository.save(reqDto.toEntity(userEntity, reqDto.getPenaltyType(),
+			reqDto.getMessage(), startTime, reqDto.getPenaltyTime()));
+	}
 }
