fc3bd5584135c264148c18535a09ce71a3c820a0
기현(gkwon) <79272189+ghyen@users.noreply.github.com>
2024-03-19 10:35:14 +0900

    647 노쇼 신고 api (#723)
    Co-authored-by: seungsje <111065574+AreSain@users.noreply.github.com>
    Co-authored-by: seungsje <seungsje@student.42seoul.kr>

--- gg-repo/src/main/java/gg/repo/party/UserReportRepository.java
+++ gg-repo/src/main/java/gg/repo/party/UserReportRepository.java
@@ -1,13 +1,23 @@
 package gg.repo.party;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import gg.data.party.UserReport;
+import gg.data.user.User;
 
 public interface UserReportRepository extends JpaRepository<UserReport, Long> {
+	public Optional<UserReport> findByReporterAndReportee(User reporter, User reportee);
+
+	@Query("SELECT ur FROM UserReport ur JOIN FETCH ur.reportee "
+		+ "JOIN FETCH ur.room WHERE ur.reportee = :reportee AND ur.room.id = :roomId")
+	List<UserReport> findByReporteeAndRoomId(@Param("reportee") User reportee, @Param("roomId") Long roomId);
 
 	@Query(value = "SELECT ur FROM UserReport ur "
 		+ "JOIN FETCH ur.reporter "
