1e79c37aed1ab9b38a76b3bd2239cf065f83fd9a
Lee Yeseul <80022494+yes-ee@users.noreply.github.com>
2024-03-19 11:25:17 +0900

    [FEATURE] 패널티 리스트 조회 ADMIN API 추가 (#733)

--- gg-pingpong-api/src/main/java/gg/party/api/admin/penalty/controller/PartyPenaltyAdminController.java
+++ gg-pingpong-api/src/main/java/gg/party/api/admin/penalty/controller/PartyPenaltyAdminController.java
@@ -1,7 +1,11 @@
 package gg.party.api.admin.penalty.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import gg.party.api.admin.penalty.request.PartyPenaltyAdminReqDto;
+import gg.party.api.admin.penalty.controller.request.PageReqDto;
+import gg.party.api.admin.penalty.controller.request.PartyPenaltyAdminReqDto;
+import gg.party.api.admin.penalty.controller.response.PartyPenaltyListAdminResDto;
 import gg.party.api.admin.penalty.service.PartyPenaltyAdminService;
 import lombok.RequiredArgsConstructor;
 
@@ -17,13 +23,26 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/party/admin/penalties")
 public class PartyPenaltyAdminController {
+	private final PartyPenaltyAdminService penaltyAdminService;
+
+	/**
+	 * 패널티 조회
+	 * @param reqDto page size
+	 * @return PenaltyListAdminResponseDto penaltyList totalPage
+	 */
+	@GetMapping
+	public ResponseEntity<PartyPenaltyListAdminResDto> penaltyList(@ModelAttribute @Valid PageReqDto reqDto) {
+		return ResponseEntity.status(HttpStatus.OK).body(penaltyAdminService.findAllPenalty(reqDto));
+	}
+
 	private final PartyPenaltyAdminService partyPenaltyAdminService;
 
 	/**
 	 * 패널티 부여
 	 */
 	@PostMapping()
-	public ResponseEntity<Void> addAdminPenalty(@RequestBody PartyPenaltyAdminReqDto reqDto) {
+	public ResponseEntity<Void> addAdminPenalty(
+		@RequestBody PartyPenaltyAdminReqDto reqDto) {
 		partyPenaltyAdminService.addAdminPenalty(reqDto);
 		return ResponseEntity.status(HttpStatus.CREATED).build();
 	}
--- gg-pingpong-api/src/main/java/gg/party/api/admin/penalty/service/PartyPenaltyAdminService.java
+++ gg-pingpong-api/src/main/java/gg/party/api/admin/penalty/service/PartyPenaltyAdminService.java
@@ -1,15 +1,22 @@
 package gg.party.api.admin.penalty.service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
-import javax.transaction.Transactional;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import gg.admin.repo.penalty.PartyPenaltyAdminRepository;
 import gg.data.party.PartyPenalty;
 import gg.data.user.User;
-import gg.party.api.admin.penalty.request.PartyPenaltyAdminReqDto;
+import gg.party.api.admin.penalty.controller.request.PageReqDto;
+import gg.party.api.admin.penalty.controller.request.PartyPenaltyAdminReqDto;
+import gg.party.api.admin.penalty.controller.response.PartyPenaltyAdminResDto;
+import gg.party.api.admin.penalty.controller.response.PartyPenaltyListAdminResDto;
 import gg.repo.user.UserRepository;
 import gg.utils.exception.party.PartyPenaltyNotFoundException;
 import gg.utils.exception.user.UserNotFoundException;
@@ -18,8 +25,29 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class PartyPenaltyAdminService {
+
 	private final PartyPenaltyAdminRepository partyPenaltyAdminRepository;
 	private final UserRepository userRepository;
+
+	/**
+	 * 패널티 전체 조회
+	 * @param reqDto page size
+	 * @return PenaltyListAdminResDto penaltyList totalPage
+	 */
+	@Transactional(readOnly = true)
+	public PartyPenaltyListAdminResDto findAllPenalty(PageReqDto reqDto) {
+		int page = reqDto.getPage();
+		int size = reqDto.getSize();
+
+		Pageable pageable = PageRequest.of(page - 1, size, Sort.by(Sort.Direction.DESC, "createdAt"));
+
+		Page<PartyPenaltyAdminResDto> penaltyPage = partyPenaltyAdminRepository.findAll(pageable)
+			.map(PartyPenaltyAdminResDto::new);
+
+		List<PartyPenaltyAdminResDto> penaltyList = penaltyPage.getContent();
+
+		return new PartyPenaltyListAdminResDto(penaltyList, penaltyPage.getTotalPages());
+	}
 
 	/**
 	 * 패널티 수정
