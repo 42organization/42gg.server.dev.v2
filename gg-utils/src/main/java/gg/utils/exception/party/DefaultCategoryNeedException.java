618e1fb9eb36f93a214c7fa10705967a638f01c6
seungsje <seungsje@student.42seoul.kr>
2024-03-26 18:03:00 +0900

    [bug] etc -> 기타로 수정

--- gg-utils/src/main/java/gg/utils/exception/party/DefaultCategoryNeedException.java
+++ gg-utils/src/main/java/gg/utils/exception/party/DefaultCategoryNeedException.java
@@ -4,7 +4,7 @@ import gg.utils.exception.ErrorCode;
 import gg.utils.exception.custom.BusinessException;
 
 public class DefaultCategoryNeedException extends BusinessException {
-	public static final String DEFAULT_CATEGORY_NAME = "etc";
+	public static final String DEFAULT_CATEGORY_NAME = "기타";
 
 	public DefaultCategoryNeedException() {
 		super(ErrorCode.DEFAULT_CATEGORY_NEED.getMessage(), ErrorCode.DEFAULT_CATEGORY_NEED);
