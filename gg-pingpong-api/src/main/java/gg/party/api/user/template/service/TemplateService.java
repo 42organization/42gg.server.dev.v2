7fba105130dae021d6b086dd54f594b02b8dc781
seungsje <seungsje@student.42seoul.kr>
2024-03-26 16:36:08 +0900

    [feature] 템플릿 가나다순으로 정렬 기능 추가

--- gg-pingpong-api/src/main/java/gg/party/api/user/template/service/TemplateService.java
+++ gg-pingpong-api/src/main/java/gg/party/api/user/template/service/TemplateService.java
@@ -1,8 +1,9 @@
 package gg.party.api.user.template.service;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,12 +19,16 @@ public class TemplateService {
 
 	/**
 	 * 템플릿 전체 조회
-	 * @return 템플릿 전체 리스트 (name 순으로 오름차순 정렬)
+	 * @return 템플릿 전체 리스트 (가나다 순으로 정렬)
 	 */
 	@Transactional(readOnly = true)
 	public TemplateListResDto findTemplateList() {
-		return new TemplateListResDto(templateRepository.findAll(Sort.by(Sort.Direction.ASC, "name")).stream()
+		List<TemplateResDto> sortedTemplates = templateRepository.findAll()
+			.stream()
 			.map(TemplateResDto::new)
-			.collect(Collectors.toList()));
+			.sorted(Comparator.comparing(TemplateResDto::getGameName, Comparator.nullsLast(Comparator.naturalOrder())))
+			.collect(Collectors.toList());
+
+		return new TemplateListResDto(sortedTemplates);
 	}
 }
