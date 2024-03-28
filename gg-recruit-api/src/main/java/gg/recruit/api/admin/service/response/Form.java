db2461e566faf30922834c211977d50a85718174
Kimhannah <skqkdldhf98@naver.com>
2024-03-28 18:26:24 +0900

    :hammer: refactor: Recruitments -> Recruitment

--- gg-recruit-api/src/main/java/gg/recruit/api/admin/service/response/Form.java
+++ gg-recruit-api/src/main/java/gg/recruit/api/admin/service/response/Form.java
@@ -10,20 +10,27 @@ import javax.validation.constraints.Size;
 import gg.data.recruit.recruitment.Question;
 import gg.data.recruit.recruitment.Recruitment;
 import gg.data.recruit.recruitment.enums.InputType;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class Form {
 	@NotBlank(message = "질문을 입력해주세요.")
 	@Size(min = 1, max = 300, message = "질문은 300자 이내로 입력해주세요.")
 	String question;
 
-	@NotBlank(message = "입력타입을 선택해주세요.")
-	@Size(min = 1, max = 20, message = "입력타입은 20자 이내로 입력해주세요.")
 	InputType inputType;
 
-	@NotNull(message = "선택지를 입력해주세요.")
 	List<@NotNull @NotEmpty @Size(min = 1, max = 100, message = "100자 이내로 입력해주세요.") String> checkList;
+
+	@Builder
+	public Form(String question, InputType inputType, List<String> checkList) {
+		this.question = question;
+		this.inputType = inputType;
+		this.checkList = checkList;
+	}
 
 	public Question toQuestion(Recruitment recruitment, int sortNum) {
 		return new Question(recruitment, inputType, question, sortNum);
