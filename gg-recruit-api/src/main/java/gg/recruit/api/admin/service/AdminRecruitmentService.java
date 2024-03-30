8081320d5bc7a679a1da09e520aa11457c8d0a4a
Kimhannah <skqkdldhf98@naver.com>
2024-03-30 14:00:24 +0900

    :bug: fix: Recruitment에 대한 import 수정

--- gg-recruit-api/src/main/java/gg/recruit/api/admin/service/AdminRecruitmentService.java
+++ gg-recruit-api/src/main/java/gg/recruit/api/admin/service/AdminRecruitmentService.java
@@ -3,8 +3,8 @@ package gg.recruit.api.admin.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import gg.data.recruit.recruitment.Recruitments;
-import gg.repo.recruit.user.recruitment.RecruitmentRepository;
+import gg.data.recruit.recruitment.Recruitment;
+import gg.repo.recruit.recruitment.RecruitmentRepository;
 import gg.utils.exception.custom.NotExistException;
 import lombok.RequiredArgsConstructor;
 
@@ -15,7 +15,7 @@ public class AdminRecruitmentService {
 
 	@Transactional
 	public void updateRecruitStatus(UpdateRecruitStatusParam updateRecruitStatusParam) {
-		Recruitments recruitments = recruitmentRepository.findById(updateRecruitStatusParam.getRecruitId())
+		Recruitment recruitments = recruitmentRepository.findById(updateRecruitStatusParam.getRecruitId())
 			.orElseThrow(() -> new NotExistException("Recruitment not found."));
 		recruitments.setFinish(updateRecruitStatusParam.getFinish());
 	}
