9d6f0f1b99e36a3cedfe1aeebc9d0a8c786a46c3
이자훈 <jahlee@student.42seoul.kr>
2024-04-06 22:16:40 +0900

    :test_tube: test: CreateTournamentGameResult 메서드 완료

--- gg-pingpong-api/src/test/java/gg/pingpong/api/user/game/service/GameServiceUnitTest.java
+++ gg-pingpong-api/src/test/java/gg/pingpong/api/user/game/service/GameServiceUnitTest.java
@@ -7,6 +7,7 @@ import static org.mockito.BDDMockito.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -20,14 +21,21 @@ import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 
 import gg.data.pingpong.game.Game;
+import gg.data.pingpong.game.PChange;
 import gg.data.pingpong.game.Team;
 import gg.data.pingpong.game.TeamUser;
 import gg.data.pingpong.game.type.StatusType;
+import gg.data.pingpong.match.type.TournamentMatchStatus;
+import gg.data.pingpong.season.Season;
+import gg.data.pingpong.tournament.Tournament;
+import gg.data.pingpong.tournament.TournamentGame;
+import gg.data.pingpong.tournament.type.TournamentRound;
 import gg.data.user.User;
 import gg.data.user.type.RacketType;
 import gg.data.user.type.RoleType;
 import gg.data.user.type.SnsType;
 import gg.pingpong.api.user.game.controller.request.RankResultReqDto;
+import gg.pingpong.api.user.game.controller.request.TournamentResultReqDto;
 import gg.pingpong.api.user.match.service.MatchTournamentService;
 import gg.pingpong.api.user.rank.redis.RankRedisService;
 import gg.pingpong.api.user.rank.service.TierService;
@@ -40,6 +48,9 @@ import gg.repo.tournarment.TournamentGameRepository;
 import gg.utils.annotation.UnitTest;
 import gg.utils.exception.custom.InvalidParameterException;
 import gg.utils.exception.game.GameNotExistException;
+import gg.utils.exception.game.GameStatusNotMatchedException;
+import gg.utils.exception.game.ScoreAlreadyEnteredException;
+import gg.utils.exception.tournament.TournamentGameNotFoundException;
 
 @UnitTest
 class GameServiceUnitTest {
@@ -66,11 +77,20 @@ class GameServiceUnitTest {
 	@InjectMocks
 	GameService gameService;
 
+	List<GameTeamUserInfo> infos;
+	RankResultReqDto scoreDto;
+	Game game;
+	Season season;
+	List<TeamUser> teams;
+	List<PChange> pChanges;
+	Team myTeam;
+	Team enemyTeam;
+	User me;
+	User enemy;
+
 	@Nested
 	@DisplayName("getUserGameInfo 매서드 유닛 테스트")
 	class GetUserGameInfo {
-		List<GameTeamUserInfo> infos;
-
 		@BeforeEach
 		void beforeEach() {
 			int infoCnt = 5;
@@ -96,14 +116,6 @@ class GameServiceUnitTest {
 	@MockitoSettings(strictness = Strictness.LENIENT)
 	@DisplayName("createRankResult 매서드 유닛 테스트")
 	class CreateRankResult {
-		RankResultReqDto scoreDto;
-		Game game;
-		List<TeamUser> teams;
-		Team myTeam;
-		Team enemyTeam;
-		User me;
-		User enemy;
-
 		@BeforeEach
 		void beforeEach() {
 			game = new Game();
@@ -173,6 +185,118 @@ class GameServiceUnitTest {
 			// when, then
 			assertThatThrownBy(() -> gameService.createRankResult(scoreDto, enemy.getId()))
 				.isInstanceOf(InvalidParameterException.class);
+		}
+	}
+
+	@Nested
+	@MockitoSettings(strictness = Strictness.LENIENT)
+	@DisplayName("createTournamentGameResult 매서드 유닛 테스트")
+	class CreateTournamentGameResult {
+		TournamentGame tournamentGame;
+		TournamentResultReqDto tournamentResultReqDto;
+
+		@BeforeEach
+		void beforeEach() {
+			season = new Season(1L, "", LocalDateTime.now(), LocalDateTime.now(), 0, 0);
+			game = new Game();
+			setFieldWithReflection(game, "id", 1L);
+			setFieldWithReflection(game, "startTime", LocalDateTime.now());
+			setFieldWithReflection(game, "season", season);
+			myTeam = new Team(game, -1, true);
+			setFieldWithReflection(myTeam, "id", 1L);
+			enemyTeam = new Team(game, -1, false);
+			setFieldWithReflection(enemyTeam, "id", 2L);
+
+			me = new User("", "", "", RacketType.SHAKEHAND,
+				RoleType.USER, 0, SnsType.NONE, 1L);
+			enemy = new User("", "", "", RacketType.SHAKEHAND,
+				RoleType.USER, 0, SnsType.NONE, 1L);
+			setFieldWithReflection(me, "id", 1L);
+			setFieldWithReflection(enemy, "id", 2L);
+
+			teams = new ArrayList<>();
+			teams.add(new TeamUser(1L, myTeam, me));
+			teams.add(new TeamUser(2L, enemyTeam, enemy));
+
+			tournamentGame = new TournamentGame(game, mock(Tournament.class), TournamentRound.QUARTER_FINAL_1);
+			tournamentResultReqDto = new TournamentResultReqDto(1L, myTeam.getId(), 2,
+				enemyTeam.getId(), 1);
+
+			pChanges = new ArrayList<>();
+			pChanges.add(mock(PChange.class));
+
+			given(gameFindService.findGameWithPessimisticLockById(any())).willReturn(game);
+			given(teamUserRepository.findAllByGameId(anyLong())).willReturn(teams);
+			given(pChangeRepository.findPChangesByGameId(anyLong())).willReturn(pChanges);
+		}
+
+		@ParameterizedTest
+		@EnumSource(value = StatusType.class, names = {"WAIT", "LIVE"})
+		@DisplayName("success1")
+		void success1(StatusType status) {
+			// given
+			given(matchTournamentService.checkTournamentGame(any())).willReturn(TournamentMatchStatus.REQUIRED);
+			given(tournamentGameRepository.findByGameId(anyLong())).willReturn(Optional.of(tournamentGame));
+			setFieldWithReflection(game, "status", status);
+			// when
+			gameService.createTournamentGameResult(tournamentResultReqDto, me.getId());
+		}
+
+		@Test
+		@DisplayName("success2")
+		void success2() {
+			// given
+			given(matchTournamentService.checkTournamentGame(any())).willReturn(TournamentMatchStatus.UNNECESSARY);
+			setFieldWithReflection(game, "status", StatusType.LIVE);
+			// when
+			gameService.createTournamentGameResult(tournamentResultReqDto, me.getId());
+		}
+
+		@Test
+		@DisplayName("TournamentGameNotFoundException")
+		void tournamentGameNotFoundException() {
+			// given
+			given(matchTournamentService.checkTournamentGame(any())).willReturn(TournamentMatchStatus.REQUIRED);
+			given(tournamentGameRepository.findByGameId(anyLong())).willReturn(Optional.empty());
+			setFieldWithReflection(game, "status", StatusType.LIVE);
+			// when
+			assertThatThrownBy(() -> gameService.createTournamentGameResult(tournamentResultReqDto, me.getId()))
+				.isInstanceOf(TournamentGameNotFoundException.class);
+		}
+
+		@Test
+		@DisplayName("ScoreAlreadyEnteredException")
+		void scoreAlreadyEnteredException() {
+			// given
+			setFieldWithReflection(myTeam, "score", 2);
+			setFieldWithReflection(enemyTeam, "score", 1);
+			given(matchTournamentService.checkTournamentGame(any())).willReturn(TournamentMatchStatus.REQUIRED);
+			given(tournamentGameRepository.findByGameId(anyLong())).willReturn(Optional.of(tournamentGame));
+			setFieldWithReflection(game, "status", StatusType.LIVE);
+			// when
+			assertThatThrownBy(() -> gameService.createTournamentGameResult(tournamentResultReqDto, me.getId()))
+				.isInstanceOf(ScoreAlreadyEnteredException.class);
+		}
+
+		@Test
+		@DisplayName("InvalidParameterException")
+		void invalidParameterException() {
+			// given
+			setFieldWithReflection(game, "status", StatusType.LIVE);
+			// when
+			assertThatThrownBy(() -> gameService.createTournamentGameResult(tournamentResultReqDto, 0L))
+				.isInstanceOf(InvalidParameterException.class);
+		}
+
+		@ParameterizedTest
+		@EnumSource(value = StatusType.class, names = {"BEFORE", "END"})
+		@DisplayName("GameStatusNotMatchedException")
+		void gameStatusNotMatchedException(StatusType status) {
+			// given
+			setFieldWithReflection(game, "status", status);
+			// when
+			assertThatThrownBy(() -> gameService.createTournamentGameResult(tournamentResultReqDto, 0L))
+				.isInstanceOf(GameStatusNotMatchedException.class);
 		}
 	}
 }
