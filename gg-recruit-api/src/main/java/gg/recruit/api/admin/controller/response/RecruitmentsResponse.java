e73698c9f8d7f530a066cae8a6d6e4fbc31db29d
Kimhannah <skqkdldhf98@naver.com>
2024-04-06 16:37:47 +0900

    :hammer: refactor: 공고 전체 조회 response에 totalPage 추가

--- gg-recruit-api/src/main/java/gg/recruit/api/admin/controller/response/RecruitmentsResponse.java
+++ gg-recruit-api/src/main/java/gg/recruit/api/admin/controller/response/RecruitmentsResponse.java
@@ -4,17 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 import gg.data.recruit.recruitment.Recruitment;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class RecruitmentsResponse {
 	private List<RecruitmentDto> recruitmentDtoList = new ArrayList<>();
+	private int totalPage;
 
-	public RecruitmentsResponse(List<Recruitment> recruitments) {
+	public RecruitmentsResponse(List<Recruitment> recruitments, int totalPage) {
 		for (Recruitment recruitment : recruitments) {
 			recruitmentDtoList.add(RecruitmentDto.toRecruitmentDto(recruitment));
 		}
+		this.totalPage = totalPage;
 	}
 }
