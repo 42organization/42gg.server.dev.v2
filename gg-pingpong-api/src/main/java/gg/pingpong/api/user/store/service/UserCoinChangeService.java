def176bf9c17293ba1c7da0b054d12798be2ad97
sujin Park <chemd78@naver.com>
2024-04-07 23:30:54 +0900

    :green_heart: chore: javadoc 추가

--- gg-pingpong-api/src/main/java/gg/pingpong/api/user/store/service/UserCoinChangeService.java
+++ gg-pingpong-api/src/main/java/gg/pingpong/api/user/store/service/UserCoinChangeService.java
@@ -26,6 +26,13 @@ public class UserCoinChangeService {
 	private final UserRepository userRepository;
 	private final GameFindService gameFindService;
 
+	/**
+	 * 출석 코인을 제공합니다.
+	 * @param user 유저
+	 * @exception UserAlreadyAttendanceException 당일에 이미 출석을 한 경우
+	 * @exception CoinPolicyNotFoundException 코인 정책이 존재하지 않을 경우
+	 * @return 코인 증가량
+	 */
 	@Transactional
 	public int addAttendanceCoin(User user) {
 		if (coinHistoryService.hasAttendedToday(user)) {
@@ -38,6 +45,13 @@ public class UserCoinChangeService {
 		return coinIncrement;
 	}
 
+	/**
+	 * 유저가 코인을 사용하여 아이템을 구매합니다.
+	 * @param item 아이템
+	 * @param price 가격
+	 * @param userId 유저 아이디
+	 * @exception UserNotFoundException 유저가 존재하지 않을 경우
+	 */
 	@Transactional
 	public void purchaseItemCoin(Item item, Integer price, Long userId) {
 
@@ -49,6 +63,13 @@ public class UserCoinChangeService {
 		coinHistoryService.addPurchaseItemCoinHistory(user, item, price);
 	}
 
+	/**
+	 * 아이템 선물 후 코인 사용 내역을 저장합니다.
+	 * @param item 아이템
+	 * @param price 가격
+	 * @param user 유저
+	 * @param giftTarget 선물 받을 유저
+	 */
 	@Transactional
 	public void giftItemCoin(Item item, Integer price, User user, User giftTarget) {
 		user.payGgCoin(price);
@@ -56,6 +77,13 @@ public class UserCoinChangeService {
 		coinHistoryService.addGiftItemCoinHistory(user, giftTarget, item, price);
 	}
 
+	/**
+	 * 일반 게임 종료 후 코인을 부여합니다.
+	 * @param userId 유저 아이디
+	 * @exception UserNotFoundException 유저가 존재하지 않을 경우
+	 * @exception CoinPolicyNotFoundException 코인 정책이 존재하지 않을 경우
+	 * @return
+	 */
 	@Transactional
 	public UserGameCoinResultDto addNormalGameCoin(Long userId) {
 		User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
@@ -67,6 +95,13 @@ public class UserCoinChangeService {
 		return new UserGameCoinResultDto(user.getGgCoin(), coinIncrement);
 	}
 
+	/**
+	 * 랭크 게임 종료 후 코인을 부여합니다.
+	 * @param gameId 게임 아이디
+	 * @param userId 유저 아이디
+	 * @exception UserNotFoundException 유저가 존재하지 않을 경우
+	 * @return
+	 */
 	@Transactional
 	public UserGameCoinResultDto addRankGameCoin(Long gameId, Long userId) {
 		User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
