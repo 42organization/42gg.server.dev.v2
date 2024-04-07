71312eadb4952fadf00e612f6abbf9e807c122ca
wken5577 <wken5577@hanmail.net>
2024-04-07 15:00:47 +0900

    [FEAT] #811 admin recruit detail api

--- gg-admin-repo/src/main/java/gg/admin/repo/recruit/RecruitmentAdminRepository.java
+++ gg-admin-repo/src/main/java/gg/admin/repo/recruit/RecruitmentAdminRepository.java
@@ -1,11 +1,17 @@
 package gg.admin.repo.recruit;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import gg.data.recruit.recruitment.Recruitment;
 
 public interface RecruitmentAdminRepository extends JpaRepository<Recruitment, Long> {
 	Page<Recruitment> findAllByOrderByEndTimeDesc(Pageable pageable);
+
+	@Query("SELECT r FROM Recruitment r WHERE r.id = :recruitId AND r.isDeleted = false")
+	Optional<Recruitment> findNotDeletedRecruit(Long recruitId);
 }
