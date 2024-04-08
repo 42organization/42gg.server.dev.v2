a2e8fd90656f3fcf7c8d9d34bafb616cd510a5de
Kimhannah <skqkdldhf98@naver.com>
2024-04-08 21:51:00 +0900

    :bug: fix: 공고 전체 조회 response status -> isFinish

--- gg-recruit-api/src/main/java/gg/recruit/api/admin/controller/response/RecruitmentDto.java
+++ gg-recruit-api/src/main/java/gg/recruit/api/admin/controller/response/RecruitmentDto.java
@@ -3,26 +3,27 @@ package gg.recruit.api.admin.controller.response;
 import java.time.LocalDateTime;
 
 import gg.data.recruit.recruitment.Recruitment;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class RecruitmentDto {
 	private Long id;
 	private String title;
-	private String status;
+	private Boolean isFinish;
 	private String generation;
 	private LocalDateTime startDate;
 	private LocalDateTime endDate;
 
 	@Builder
-	public RecruitmentDto(Long id, String title, String status, String generation, LocalDateTime startDate,
+	public RecruitmentDto(Long id, String title, Boolean isFinish, String generation, LocalDateTime startDate,
 		LocalDateTime endDate) {
 		this.id = id;
 		this.title = title;
-		this.status = status;
+		this.isFinish = isFinish;
 		this.generation = generation;
 		this.startDate = startDate;
 		this.endDate = endDate;
@@ -32,6 +33,7 @@ public class RecruitmentDto {
 		return RecruitmentDto.builder()
 			.id(recruitment.getId())
 			.title(recruitment.getTitle())
+			.isFinish(recruitment.getIsFinsh())
 			.generation(recruitment.getGeneration())
 			.startDate(recruitment.getStartTime())
 			.endDate(recruitment.getEndTime())
