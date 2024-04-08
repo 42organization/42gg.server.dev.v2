d4e5e329ff9ddd84291558ebda5a9437834a5325
Kimhannah <skqkdldhf98@naver.com>
2024-04-08 18:26:27 +0900

    :bug: [Fix] 공고 전체 조회 response 필드명 수정

--- gg-recruit-api/src/main/java/gg/recruit/api/admin/controller/response/RecruitmentsResponse.java
+++ gg-recruit-api/src/main/java/gg/recruit/api/admin/controller/response/RecruitmentsResponse.java
@@ -11,12 +11,12 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class RecruitmentsResponse {
-	private List<RecruitmentDto> recruitmentDtoList = new ArrayList<>();
+	private List<RecruitmentDto> recruitments = new ArrayList<>();
 	private int totalPage;
 
 	public RecruitmentsResponse(List<Recruitment> recruitments, int totalPage) {
 		for (Recruitment recruitment : recruitments) {
-			recruitmentDtoList.add(RecruitmentDto.toRecruitmentDto(recruitment));
+			this.recruitments.add(RecruitmentDto.toRecruitmentDto(recruitment));
 		}
 		this.totalPage = totalPage;
 	}
