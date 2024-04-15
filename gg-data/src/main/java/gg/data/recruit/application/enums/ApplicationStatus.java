b15869285267284c676716121461fc02503dee44
wken5577 <wken5577@hanmail.net>
2024-04-15 19:16:50 +0900

    [FIX] application status 수정

--- gg-data/src/main/java/gg/data/recruit/application/enums/ApplicationStatus.java
+++ gg-data/src/main/java/gg/data/recruit/application/enums/ApplicationStatus.java
@@ -7,8 +7,9 @@ public enum ApplicationStatus {
 
 	PROGRESS_DOCS("progress", "서류 진행중", false),
 	PROGRESS_INTERVIEW("progress", "면접 진행중", false),
-	PASS("pass", "합격", true),
-	FAIL("fail", "불합격", true);
+	APPLICATION_FAIL("fail", "서류 탈락", true),
+	INTERVIEW_FAIL("fail", "면접 탈락", true),
+	PASS("pass", "합격", true);
 
 	private final String status;
 	private final String desc;
