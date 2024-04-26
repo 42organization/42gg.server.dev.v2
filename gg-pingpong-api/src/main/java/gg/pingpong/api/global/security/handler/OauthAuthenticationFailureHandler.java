337e6b4758096d50d49812ed44f5e2f26f318e9e
wken5577 <wken5577@hanmail.net>
2024-04-26 10:42:16 +0900

    [FIX] #833 error redir page설정

--- gg-pingpong-api/src/main/java/gg/pingpong/api/global/security/handler/OauthAuthenticationFailureHandler.java
+++ gg-pingpong-api/src/main/java/gg/pingpong/api/global/security/handler/OauthAuthenticationFailureHandler.java
@@ -10,13 +10,19 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
+import gg.pingpong.api.global.utils.ApplicationYmlRead;
+import lombok.RequiredArgsConstructor;
+
 @Component
+@RequiredArgsConstructor
 public class OauthAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+	private final ApplicationYmlRead applicationYmlRead;
 
 	@Override
 	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
 		AuthenticationException exception) throws IOException, ServletException {
 		exception.printStackTrace();
-		response.sendRedirect("/login-error");
+		response.sendRedirect(applicationYmlRead.getFrontUrl() + "/404");
 	}
 }
