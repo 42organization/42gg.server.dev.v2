3648766f3b6c405783c53dbbd58796be7090ec02
seungsje <seungsje@student.42seoul.kr>
2024-07-01 13:44:01 +0900

    [Refactoring] 디렉토리 이름변경

--- gg-agenda-api/src/main/java/gg/agenda/GgAgendaApiApplication.java
+++ gg-agenda-api/src/main/java/gg/agenda/GgAgendaApiApplication.java
@@ -1,4 +1,4 @@
-package gg.ggagendaapi;
+package gg.agenda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- gg-agenda-api/src/test/java/gg/agenda/GgAgendaApiApplicationTests.java
+++ gg-agenda-api/src/test/java/gg/agenda/GgAgendaApiApplicationTests.java
@@ -1,4 +1,4 @@
-package gg.ggagendaapi;
+package gg.agenda;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
