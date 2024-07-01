c980e9713b1f2f8139c0f7cbb3f7bf56ff01e033
seungsje <seungsje@student.42seoul.kr>
2024-07-01 13:49:02 +0900

    [Fix] main 삭제

--- gg-agenda-api/src/main/java/gg/agenda/GgAgendaApiApplication.java
+++ gg-agenda-api/src/main/java/gg/agenda/GgAgendaApiApplication.java
@@ -1,13 +1,9 @@
 package gg.agenda;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class GgAgendaApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GgAgendaApiApplication.class, args);
+		;
 	}
 
 }
