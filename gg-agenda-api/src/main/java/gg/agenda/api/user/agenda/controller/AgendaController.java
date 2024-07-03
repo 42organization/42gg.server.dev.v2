dc86b1b309154a30a1abb33b1e60b2c135b40ce6
yhames <wjddn494@naver.com>
2024-07-03 10:41:30 +0900

    docs: add api docs

--- gg-agenda-api/src/main/java/gg/agenda/api/user/agenda/controller/AgendaController.java
+++ gg-agenda-api/src/main/java/gg/agenda/api/user/agenda/controller/AgendaController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import gg.agenda.api.user.agenda.controller.dto.AgendaResponseDto;
 import gg.agenda.api.user.agenda.service.AgendaService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -20,6 +22,11 @@ public class AgendaController {
 	private final AgendaService agendaService;
 
 	@GetMapping
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "Agenda 상세 조회 성공"),
+		@ApiResponse(responseCode = "400", description = "Agenda 조회 요청이 잘못됨"),
+		@ApiResponse(responseCode = "404", description = "Agenda를 찾을 수 없음")
+	})
 	public ResponseEntity<AgendaResponseDto> agendaDetails(@RequestParam("agenda_id") UUID agendaKey) {
 		AgendaResponseDto agendaDto = agendaService.findAgendaWithLatestAnnouncement(agendaKey);
 		return ResponseEntity.ok(agendaDto);
