7b9c04dfeac2426fea71e565cccae9629d0b813b
yhames <wjddn494@naver.com>
2024-07-03 13:31:10 +0900

    feature: add agendaListCurrent

---
+++ gg-agenda-api/src/main/java/gg/agenda/api/user/agenda/controller/dto/AgendaSimpleResponseDto.java
@@ -0,0 +1,74 @@
+package gg.agenda.api.user.agenda.controller.dto;
+
+import java.util.UUID;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import gg.data.agenda.Agenda;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AgendaSimpleResponseDto {
+	private String agendaTitle;
+
+	private String agendaDeadLine;
+
+	private String agendaStartTime;
+
+	private String agendaEndTime;
+
+	private int agendaCurrentTeam;
+
+	private int agendaMaxTeam;
+
+	private int agendaMinPeople;
+
+	private int agendaMaxPeople;
+
+	private String agendaLocation;
+
+	private UUID agendaKey;
+
+	private boolean isOfficial;
+
+	@Builder
+	public AgendaSimpleResponseDto(String agendaTitle, String agendaDeadLine, String agendaStartTime,
+		String agendaEndTime, int agendaCurrentTeam, int agendaMaxTeam, int agendaMinPeople, int agendaMaxPeople,
+		String agendaLocation, UUID agendaKey, boolean isOfficial) {
+		this.agendaTitle = agendaTitle;
+		this.agendaDeadLine = agendaDeadLine;
+		this.agendaStartTime = agendaStartTime;
+		this.agendaEndTime = agendaEndTime;
+		this.agendaCurrentTeam = agendaCurrentTeam;
+		this.agendaMaxTeam = agendaMaxTeam;
+		this.agendaMinPeople = agendaMinPeople;
+		this.agendaMaxPeople = agendaMaxPeople;
+		this.agendaLocation = agendaLocation;
+		this.agendaKey = agendaKey;
+		this.isOfficial = isOfficial;
+	}
+
+	@Mapper
+	public interface MapStruct {
+		AgendaSimpleResponseDto.MapStruct INSTANCE = Mappers.getMapper(AgendaSimpleResponseDto.MapStruct.class);
+
+		@Mapping(target = "agendaTitle", source = "title")
+		@Mapping(target = "agendaDeadLine", source = "deadline")
+		@Mapping(target = "agendaStartTime", source = "startTime")
+		@Mapping(target = "agendaEndTime", source = "endTime")
+		@Mapping(target = "agendaCurrentTeam", source = "currentTeam")
+		@Mapping(target = "agendaMaxTeam", source = "maxTeam")
+		@Mapping(target = "agendaMinPeople", source = "minTeam")
+		@Mapping(target = "agendaMaxPeople", source = "maxTeam")
+		@Mapping(target = "agendaLocation", source = "location")
+		@Mapping(target = "agendaKey", source = "agendaKey")
+		@Mapping(target = "isOfficial", source = "isOfficial")
+		AgendaSimpleResponseDto toDto(Agenda agenda);
+	}
+}
--- gg-agenda-api/src/main/java/gg/agenda/api/user/agenda/service/AgendaService.java
+++ gg-agenda-api/src/main/java/gg/agenda/api/user/agenda/service/AgendaService.java
@@ -2,14 +2,17 @@ package gg.agenda.api.user.agenda.service;
 
 import static gg.utils.exception.ErrorCode.*;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import gg.agenda.api.user.agenda.controller.dto.AgendaResponseDto;
+import gg.agenda.api.user.agenda.controller.dto.AgendaSimpleResponseDto;
 import gg.data.agenda.Agenda;
 import gg.data.agenda.AgendaAnnouncement;
+import gg.data.agenda.type.AgendaStatus;
 import gg.repo.agenda.AgendaAnnouncementRepository;
 import gg.repo.agenda.AgendaRepository;
 import gg.utils.exception.custom.NotExistException;
@@ -30,5 +33,11 @@ public class AgendaService {
 		AgendaAnnouncement announcement = agendaAnnouncementRepository
 			.findLatestByAgenda(agenda).orElse(null);
 		return AgendaResponseDto.MapStruct.INSTANCE.toDto(agenda, announcement);
+	}
+
+	@Transactional(readOnly = true)
+	public List<AgendaSimpleResponseDto> findCurrentAgendaList() {
+		List<Agenda> agendas = agendaRepository.findAllByStatusIs(AgendaStatus.ON_GOING);
+		return agendas.stream().map(AgendaSimpleResponseDto.MapStruct.INSTANCE::toDto).toList();
 	}
 }
