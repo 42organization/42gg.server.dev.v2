72bdac024bc5efd0829b48b76aa95c37041f7646
yhames <wjddn494@naver.com>
2024-07-04 12:10:59 +0900

    stash: add stash

--- gg-agenda-api/src/main/java/gg/agenda/api/user/agenda/controller/AgendaController.java
+++ gg-agenda-api/src/main/java/gg/agenda/api/user/agenda/controller/AgendaController.java
@@ -1,6 +1,5 @@
 package gg.agenda.api.user.agenda.controller;
 
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import gg.agenda.api.user.agenda.controller.dto.AgendaResponseDto;
-import gg.agenda.api.user.agenda.controller.dto.AgendaSimpleResponseDto;
 import gg.agenda.api.user.agenda.service.AgendaService;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -23,21 +21,14 @@ public class AgendaController {
 
 	private final AgendaService agendaService;
 
+	@GetMapping
 	@ApiResponses(value = {
 		@ApiResponse(responseCode = "200", description = "Agenda 상세 조회 성공"),
 		@ApiResponse(responseCode = "400", description = "Agenda 조회 요청이 잘못됨"),
 		@ApiResponse(responseCode = "404", description = "Agenda를 찾을 수 없음")
 	})
-	@GetMapping
 	public ResponseEntity<AgendaResponseDto> agendaDetails(@RequestParam("agenda_key") UUID agendaKey) {
 		AgendaResponseDto agendaDto = agendaService.findAgendaWithLatestAnnouncement(agendaKey);
 		return ResponseEntity.ok(agendaDto);
-	}
-
-	@ApiResponse(responseCode = "200", description = "현재 진행중인 Agenda 목록 조회 성공")
-	@GetMapping("/list")
-	public ResponseEntity<List<AgendaSimpleResponseDto>> agendaListCurrent() {
-		List<AgendaSimpleResponseDto> agendaList = agendaService.findCurrentAgendaList();
-		return ResponseEntity.ok(agendaList);
 	}
 }
--- gg-agenda-api/src/main/java/gg/agenda/api/user/agenda/service/AgendaService.java
+++ gg-agenda-api/src/main/java/gg/agenda/api/user/agenda/service/AgendaService.java
@@ -2,19 +2,14 @@ package gg.agenda.api.user.agenda.service;
 
 import static gg.utils.exception.ErrorCode.*;
 
-import java.util.Comparator;
-import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import gg.agenda.api.user.agenda.controller.dto.AgendaResponseDto;
-import gg.agenda.api.user.agenda.controller.dto.AgendaSimpleResponseDto;
 import gg.data.agenda.Agenda;
 import gg.data.agenda.AgendaAnnouncement;
-import gg.data.agenda.type.AgendaStatus;
 import gg.repo.agenda.AgendaAnnouncementRepository;
 import gg.repo.agenda.AgendaRepository;
 import gg.utils.exception.custom.NotExistException;
@@ -44,5 +39,12 @@ public class AgendaService {
 				.thenComparing(Agenda::getDeadline, Comparator.reverseOrder()))
 			.map(AgendaSimpleResponseDto.MapStruct.INSTANCE::toDto)
 			.collect(Collectors.toList());
+	}
+
+	@Transactional
+	public AgendaKeyResponseDto addAgenda(AgendaCreateDto agendaCreateDto, UserDto user) {
+		Agenda newAgenda = AgendaCreateDto.MapStruct.INSTANCE.toEntity(agendaCreateDto, user);
+		Agenda savedAgenda = agendaRepository.save(newAgenda);
+		return AgendaKeyResponseDto.builder().agendaKey(savedAgenda.getAgendaKey()).build();
 	}
 }
