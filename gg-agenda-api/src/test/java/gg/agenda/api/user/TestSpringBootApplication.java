8dd7adad5cd1073f3f24e42115206afaf8f89f42
seungsje <seungsje@student.42seoul.kr>
2024-07-04 12:58:05 +0900

    [Refactoring] 불필요한 파일 제거

--- gg-agenda-api/src/test/java/gg/agenda/api/user/TestSpringBootApplication.java
+++
@@ -1,8 +0,0 @@
-package gg.agenda.api.user;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication(scanBasePackages = {"gg.recruit.api", "gg.utils", "gg.data", "gg.repo",
-	"gg.admin.repo", "gg.auth", "gg.pingpong.api", "gg.agenda.api"})
-public class TestSpringBootApplication {
-}
