da9a212b425a5e1f8ee161fc51c8df6c554fad55
yhames <wjddn494@naver.com>
2024-07-12 11:32:11 +0900

    feat: add validation

--- gg-agenda-api/src/main/java/gg/agenda/api/user/agenda/controller/request/AgendaTeamAwardDto.java
+++ gg-agenda-api/src/main/java/gg/agenda/api/user/agenda/controller/request/AgendaTeamAwardDto.java
@@ -1,5 +1,9 @@
 package gg.agenda.api.user.agenda.controller.request;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,14 +12,20 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AgendaTeamAwardDto {
+
+	@NotNull
+	@NotEmpty
 	private String teamName;
 
+	@NotNull
+	@NotEmpty
 	private String awardName;
 
-	private Integer awardPriority;
+	@Min(1)
+	private int awardPriority;
 
 	@Builder
-	public AgendaTeamAwardDto(String teamName, String awardName, Integer awardPriority) {
+	public AgendaTeamAwardDto(String teamName, String awardName, int awardPriority) {
 		this.teamName = teamName;
 		this.awardName = awardName;
 		this.awardPriority = awardPriority;
