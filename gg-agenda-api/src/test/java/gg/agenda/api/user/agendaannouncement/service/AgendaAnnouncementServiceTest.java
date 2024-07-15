8fff51895f57bb5cf91b2db18382fe5cb7cfa78f
yhames <wjddn494@naver.com>
2024-07-15 13:42:36 +0900

    test: add unit test

--- gg-agenda-api/src/test/java/gg/agenda/api/user/agendaannouncement/service/AgendaAnnouncementServiceTest.java
+++ gg-agenda-api/src/test/java/gg/agenda/api/user/agendaannouncement/service/AgendaAnnouncementServiceTest.java
@@ -1,11 +1,20 @@
 package gg.agenda.api.user.agendaannouncement.service;
 
+import static org.mockito.Mockito.*;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.data.domain.Pageable;
 
+import gg.agenda.api.user.agendaannouncement.controller.request.AgendaAnnouncementCreateReqDto;
+import gg.data.agenda.Agenda;
+import gg.data.agenda.AgendaAnnouncement;
 import gg.repo.agenda.AgendaAnnouncementRepository;
 import gg.utils.annotation.UnitTest;
 import lombok.extern.slf4j.Slf4j;
@@ -28,8 +37,17 @@ public class AgendaAnnouncementServiceTest {
 		@DisplayName("AgendaAnnouncement 생성 성공")
 		void createAgendaAnnouncementSuccess() {
 		    // given
+			Agenda agenda = mock(Agenda.class);
+			AgendaAnnouncement newAnnounce = mock(AgendaAnnouncement.class);
+			AgendaAnnouncementCreateReqDto dto = AgendaAnnouncementCreateReqDto.builder()
+				.title("title").content("content").build();
+			when(agendaAnnouncementRepository.save(any())).thenReturn(newAnnounce);
+
 		    // when
+			agendaAnnouncementService.addAgendaAnnouncement(dto, agenda);
+
 		    // then
+			verify(agendaAnnouncementRepository, times(1)).save(any());
 		}
 	}
 
@@ -41,16 +59,16 @@ public class AgendaAnnouncementServiceTest {
 		@DisplayName("AgendaAnnouncement 전체 조회 성공")
 		void getAgendaAnnouncementListSuccess() {
 			// given
-			// when
-			// then
-		}
+			Agenda agenda = mock(Agenda.class);
+			Pageable pageable = mock(Pageable.class);
+			when(agendaAnnouncementRepository.findAllByAgendaAndIsShowIsTrue(pageable, agenda))
+				.thenReturn(List.of());
 
-		@Test
-		@DisplayName("AgendaAnnouncement 전체 조회 성공 - 데이터 없는 경우")
-		void getAgendaAnnouncementListSuccessWhenNoEntity() {
-			// given
 			// when
+			agendaAnnouncementService.findAnnouncementListByAgenda(pageable, agenda);
+
 			// then
+			verify(agendaAnnouncementRepository, times(1)).findAllByAgendaAndIsShowIsTrue(pageable, agenda);
 		}
 	}
 
@@ -62,16 +80,14 @@ public class AgendaAnnouncementServiceTest {
 		@DisplayName("마지막 AgendaAnnouncement 조회 성공")
 		void getAgendaAnnouncementLatestSuccess() {
 			// given
-			// when
-			// then
-		}
+			Agenda agenda = mock(Agenda.class);
+			AgendaAnnouncement announcement = mock(AgendaAnnouncement.class);
+			when(agendaAnnouncementRepository.findLatestByAgenda(agenda)).thenReturn(Optional.of(announcement));
 
-		@Test
-		@DisplayName("마지막 AgendaAnnouncement 조회 성공 - 데이터 없는 경우")
-		void getAgendaAnnouncementLatestSuccessWhenNoEntity() {
-			// given
 			// when
+			agendaAnnouncementService.findAgendaWithLatestAnnouncement(agenda);
 			// then
+			verify(agendaAnnouncementRepository, times(1)).findLatestByAgenda(agenda);
 		}
 	}
 }
