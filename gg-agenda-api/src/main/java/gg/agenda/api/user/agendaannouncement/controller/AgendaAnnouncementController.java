491637c59188d51791344b8ec4459dca86dc5067
yhames <wjddn494@naver.com>
2024-07-15 12:15:59 +0900

    feat: add AgendaAnnouncementCreate

--- gg-agenda-api/src/main/java/gg/agenda/api/user/agendaannouncement/controller/AgendaAnnouncementController.java
+++ gg-agenda-api/src/main/java/gg/agenda/api/user/agendaannouncement/controller/AgendaAnnouncementController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,7 +30,7 @@ public class AgendaAnnouncementController {
 
 	private final AgendaAnnouncementService agendaAnnouncementService;
 
-	@GetMapping
+	@PostMapping
 	public ResponseEntity<Void> agendaAnnouncementAdd(@Login UserDto user, @RequestParam("agenda_key") UUID agendaKey,
 		@RequestBody @Valid AgendaAnnouncementCreateReqDto agendaAnnouncementCreateReqDto) {
 		Agenda agenda = agendaService.findAgendaByAgendaKey(agendaKey);
