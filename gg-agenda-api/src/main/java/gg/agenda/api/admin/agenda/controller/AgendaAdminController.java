6dc9c2b1d55b58ed49109966040cb35b80044af6
yhames <wjddn494@naver.com>
2024-07-16 14:21:57 +0900

    feat: fix startTime to id

--- gg-agenda-api/src/main/java/gg/agenda/api/admin/agenda/controller/AgendaAdminController.java
+++ gg-agenda-api/src/main/java/gg/agenda/api/admin/agenda/controller/AgendaAdminController.java
@@ -17,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 import gg.agenda.api.admin.agenda.controller.response.AgendaAdminResDto;
 import gg.agenda.api.admin.agenda.service.AgendaAdminService;
 import gg.utils.dto.PageRequestDto;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -26,12 +28,15 @@ public class AgendaAdminController {
 
 	private final AgendaAdminService agendaAdminService;
 
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "Agenda 요청 리스트 조회 성공")
+	})
 	@GetMapping("/request/list")
 	public ResponseEntity<List<AgendaAdminResDto>> getAgendaRequestList(
 		@RequestBody @Valid PageRequestDto pageDto) {
 		int page = pageDto.getPage();
 		int size = pageDto.getSize();
-		Pageable pageable = PageRequest.of(page - 1, size, Sort.by("startTime").descending());
+		Pageable pageable = PageRequest.of(page - 1, size, Sort.by("id").descending());
 		List<AgendaAdminResDto> agendaDtos = agendaAdminService.getAgendaRequestList(pageable).stream()
 			.map(AgendaAdminResDto.MapStruct.INSTANCE::toAgendaAdminResDto)
 			.collect(Collectors.toList());
