0bc4003e0c62e76cbbd9a5d0ca7be8b0cc856319
yhames <wjddn494@naver.com>
2024-07-24 13:39:28 +0900

    test: add unit test

--- gg-agenda-api/src/test/java/gg/agenda/api/admin/agendaannouncement/service/AgendaAnnouncementAdminServiceTest.java
+++ gg-agenda-api/src/test/java/gg/agenda/api/admin/agendaannouncement/service/AgendaAnnouncementAdminServiceTest.java
@@ -2,12 +2,18 @@ package gg.agenda.api.admin.agendaannouncement.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-
+import gg.admin.repo.agenda.AgendaAdminRepository;
+import gg.admin.repo.agenda.AgendaAnnouncementAdminRepository;
+import gg.agenda.api.admin.agendaannouncement.controller.request.AgendaAnnouncementAdminUpdateReqDto;
+import gg.data.agenda.Agenda;
+import gg.data.agenda.AgendaAnnouncement;
+import gg.utils.annotation.UnitTest;
+import gg.utils.exception.custom.NotExistException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -17,14 +23,6 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-
-import gg.admin.repo.agenda.AgendaAdminRepository;
-import gg.admin.repo.agenda.AgendaAnnouncementAdminRepository;
-import gg.data.agenda.Agenda;
-import gg.data.agenda.AgendaAnnouncement;
-import gg.utils.annotation.UnitTest;
-import gg.utils.exception.custom.NotExistException;
-import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @UnitTest
@@ -94,6 +92,33 @@ public class AgendaAnnouncementAdminServiceTest {
 			// expected
 			assertThrows(NotExistException.class,
 				() -> agendaAnnouncementAdminService.getAgendaAnnouncementList(UUID.randomUUID(), pageable));
+		}
+	}
+
+	@Nested
+	@DisplayName("Admin AgendaAnnouncement 수정 및 삭제")
+	class UpdateAgendaAnnouncementAdmin {
+		@Test
+		@DisplayName("Admin AgendaAnnouncement 수정 성공")
+		void updateAgendaAnnouncementAdminSuccess() {
+			// given
+			AgendaAnnouncement announcement = AgendaAnnouncement.builder().build();
+			when(agendaAnnouncementAdminRepository.findById(any(Long.class))).thenReturn(Optional.of(announcement));
+
+			// expected
+			assertDoesNotThrow(() -> agendaAnnouncementAdminService.updateAgendaAnnouncement(
+					AgendaAnnouncementAdminUpdateReqDto.builder().id(1L).build()));
+		}
+
+		@Test
+		@DisplayName("Admin AgendaAnnouncement 수정 실패 - AgendaAnnouncement가 없는 경우")
+		void updateAgendaAnnouncementAdminFailed() {
+			// given
+			when(agendaAnnouncementAdminRepository.findById(any(Long.class))).thenReturn(Optional.empty());
+
+			// expected
+			assertThrows(NotExistException.class, () -> agendaAnnouncementAdminService.updateAgendaAnnouncement(
+					AgendaAnnouncementAdminUpdateReqDto.builder().id(1L).build()));
 		}
 	}
 }
