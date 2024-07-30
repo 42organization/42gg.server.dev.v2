f7acd0b9e4683702abcd1b2faa93a331c72d6a37
yhames <wjddn494@naver.com>
2024-07-30 15:27:28 +0900

    fix: checkstyle

--- gg-agenda-api/src/main/java/gg/agenda/api/user/agenda/controller/request/AgendaTeamAward.java
+++ gg-agenda-api/src/main/java/gg/agenda/api/user/agenda/controller/request/AgendaTeamAward.java
@@ -6,11 +6,12 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+import org.hibernate.validator.constraints.Length;
+
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
