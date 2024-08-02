942146f0c1b257888e66843bb4a3ff0085a00958
jkim3 <62086003+kimjieun0301@users.noreply.github.com>
2024-08-02 21:20:58 +0900

    ✨ [Feature] 내가 참여 했던 대회 보기 API #855 (#932)

--- gg-agenda-api/src/main/java/gg/agenda/api/user/agendaprofile/controller/AgendaProfileController.java
+++ gg-agenda-api/src/main/java/gg/agenda/api/user/agendaprofile/controller/AgendaProfileController.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,13 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
 import gg.agenda.api.user.agendaprofile.controller.request.AgendaProfileChangeReqDto;
 import gg.agenda.api.user.agendaprofile.controller.response.AgendaProfileDetailsResDto;
 import gg.agenda.api.user.agendaprofile.controller.response.AgendaProfileInfoDetailsResDto;
+import gg.agenda.api.user.agendaprofile.controller.response.AttendedAgendaListResDto;
 import gg.agenda.api.user.agendaprofile.controller.response.CurrentAttendAgendaListResDto;
 import gg.agenda.api.user.agendaprofile.service.AgendaProfileFindService;
 import gg.agenda.api.user.agendaprofile.service.AgendaProfileService;
 import gg.auth.UserDto;
 import gg.auth.argumentresolver.Login;
 import gg.data.user.type.RoleType;
-import gg.repo.user.UserRepository;
+import gg.utils.dto.PageRequestDto;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 
@@ -33,8 +36,6 @@ import lombok.RequiredArgsConstructor;
 public class AgendaProfileController {
 	private final AgendaProfileFindService agendaProfileFindService;
 	private final AgendaProfileService agendaProfileService;
-	private final UserRepository userRepository;
-	private static final Logger log = LoggerFactory.getLogger(AgendaProfileController.class);
 
 	/**
 	 * AgendaProfile 상세 조회 API
@@ -91,6 +92,23 @@ public class AgendaProfileController {
 		List<CurrentAttendAgendaListResDto> currentAttendAgendaList = agendaProfileFindService.findCurrentAttendAgenda(
 			intraId);
 		return ResponseEntity.ok(currentAttendAgendaList);
+	}
+
+	/**
+	 * 과거에 참여했던 Agenda 목록 조회하는 메서드
+	 * @param pageRequest 페이지네이션 요청 정보, agendaId 아젠다 아이디
+	 */
+	@GetMapping("/history/list")
+	public ResponseEntity<List<AttendedAgendaListResDto>> getAttendedAgendaList(
+		@Login @Parameter(hidden = true) UserDto user, @ModelAttribute @Valid PageRequestDto pageRequest) {
+		int page = pageRequest.getPage();
+		int size = pageRequest.getSize();
+		String intraId = user.getIntraId();
+
+		Pageable pageable = PageRequest.of(page - 1, size, Sort.by("id").descending());
+		List<AttendedAgendaListResDto> attendedAgendaList = agendaProfileFindService.findAttendedAgenda(
+			intraId, pageable);
+		return ResponseEntity.ok(attendedAgendaList);
 	}
 }
 
