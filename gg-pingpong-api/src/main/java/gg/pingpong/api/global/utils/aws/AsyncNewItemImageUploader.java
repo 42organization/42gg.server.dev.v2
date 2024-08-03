e1491c44d32477d1514023db79066f178d5b107a
yhames <wjddn494@naver.com>
2024-08-03 23:18:34 +0900

    refactor: change to use ItemImageHandler to ImageHandler

--- gg-pingpong-api/src/main/java/gg/pingpong/api/global/utils/aws/AsyncNewItemImageUploader.java
+++ gg-pingpong-api/src/main/java/gg/pingpong/api/global/utils/aws/AsyncNewItemImageUploader.java
@@ -1,6 +1,7 @@
 package gg.pingpong.api.global.utils.aws;
 
 import java.io.IOException;
+import java.net.URL;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -8,27 +9,27 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import gg.data.pingpong.store.Item;
+import gg.utils.file.handler.ImageHandler;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class AsyncNewItemImageUploader {
 
-	private final ItemImageHandler itemImageHandler;
+	private final ImageHandler imageHandler;
 
 	@Value("${info.image.itemNotFoundUrl}")
 	private String defaultImageUrl;
 
-	public AsyncNewItemImageUploader(ItemImageHandler itemImageHandler) {
-		this.itemImageHandler = itemImageHandler;
-	}
-
 	@Transactional
-	public void upload(Item item,
-		MultipartFile multipartFile) throws IOException {
-		String s3ImageUrl = itemImageHandler.updateAndGetS3ImageUri(multipartFile, item);
-		if (s3ImageUrl == null) {
+	public void upload(Item item, MultipartFile multipartFile) throws IOException {
+		if (item.getName().equals("small_default.jpeg")) {
 			item.imageUpdate(defaultImageUrl);
-		} else {
-			item.imageUpdate(s3ImageUrl);
+			return;
 		}
+		URL s3ImageUrl = imageHandler.uploadImage(multipartFile, item.getName());
+		item.imageUpdate(s3ImageUrl.toString());
 	}
 }
