68a7ccefe8838adb332e1be2fc6b45614573eea4
yhames <wjddn494@naver.com>
2024-08-05 12:59:10 +0900

    test: add MultiValueMapConverter for Test

--- gg-utils/src/testFixtures/java/gg/utils/converter/MultiValueMapConverter.java
+++ gg-utils/src/testFixtures/java/gg/utils/converter/MultiValueMapConverter.java
@@ -3,23 +3,27 @@ package gg.utils.converter;
 import java.util.Map;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.testcontainers.shaded.com.fasterxml.jackson.core.type.TypeReference;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
+@Slf4j
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class MultiValueMapConverter {
 
 	public static MultiValueMap<String, String> convert(ObjectMapper objectMapper, Object dto) { // (2)
 		try {
 			MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-			Map<String, String> map = objectMapper.convertValue(dto, new Type<Map<String, String>>() {}); // (3)
+			Map<String, String> map = objectMapper.convertValue(dto, new TypeReference<Map<String, String>>() {
+			}); // (3)
 			params.setAll(map); // (4)
-
 			return params;
 		} catch (Exception e) {
 			log.error("Url Parameter 변환중 오류가 발생했습니다. requestDto={}", dto, e);
 			throw new IllegalStateException("Url Parameter 변환중 오류가 발생했습니다.");
 		}
 	}
-
 }
