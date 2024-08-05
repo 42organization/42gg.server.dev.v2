25e0372daddf182d5c6f872d9729b5024e20174e
yhames <wjddn494@naver.com>
2024-08-05 12:20:24 +0900

    fix: check multipartfile original name

--- gg-pingpong-api/src/main/java/gg/pingpong/api/global/utils/aws/AsyncNewItemImageUploader.java
+++ gg-pingpong-api/src/main/java/gg/pingpong/api/global/utils/aws/AsyncNewItemImageUploader.java
@@ -2,6 +2,7 @@ package gg.pingpong.api.global.utils.aws;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -23,11 +24,18 @@ public class AsyncNewItemImageUploader {
 
 	@Transactional
 	public void upload(Item item, MultipartFile multipartFile) throws IOException {
-		if (item.getName().equals("small_default.jpeg")) {
+		if (isDefaultImage(multipartFile)) {
 			item.imageUpdate(defaultImageUrl);
 			return;
 		}
 		URL s3ImageUrl = imageHandler.uploadImage(multipartFile, item.getName());
 		item.imageUpdate(s3ImageUrl.toString());
+	}
+
+	private static boolean isDefaultImage(MultipartFile multipartFile) {
+		if (Objects.isNull(multipartFile.getOriginalFilename())) {
+			return false;
+		}
+		return multipartFile.getOriginalFilename().equals("small_default.jpeg");
 	}
 }
