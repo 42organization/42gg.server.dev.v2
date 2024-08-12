d80cdde6e7981673a8dc811be1fa0cce06ef21b7
seungsje <seungsje@student.42seoul.kr>
2024-08-12 13:00:33 +0900

    [Fix] Redis 관련 코드 사용하지 않게 수정

--- gg-data/src/main/java/gg/data/agenda/Auth42Token.java
+++ gg-data/src/main/java/gg/data/agenda/Auth42Token.java
@@ -1,9 +1,11 @@
 package gg.data.agenda;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Deprecated
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Auth42Token {
@@ -11,6 +13,7 @@ public class Auth42Token {
 	private String accessToken;
 	private String refreshToken;
 
+	@Builder
 	public Auth42Token(String intra42Id, String accessToken, String refreshToken) {
 		this.intra42Id = intra42Id;
 		this.accessToken = accessToken;
