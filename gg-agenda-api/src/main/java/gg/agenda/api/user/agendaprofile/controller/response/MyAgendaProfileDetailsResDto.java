be72f0c677d8c4581ef0225a82c4cc9b27bdfe2c
yhames <wjddn494@naver.com>
2024-08-26 12:28:18 +0900

    feat: add find AgendaProfile with intraId

---
+++ gg-agenda-api/src/main/java/gg/agenda/api/user/agendaprofile/controller/response/MyAgendaProfileDetailsResDto.java
@@ -0,0 +1,46 @@
+package gg.agenda.api.user.agendaprofile.controller.response;
+
+import gg.agenda.api.user.agendaprofile.service.intraprofile.IntraProfile;
+import gg.data.agenda.AgendaProfile;
+import gg.data.agenda.type.Coalition;
+import gg.data.agenda.type.Location;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class MyAgendaProfileDetailsResDto {
+
+	private String userIntraId;
+	private String userContent;
+	private String userGithub;
+	private Coalition userCoalition;
+	private Location userLocation;
+	private int ticketCount;
+	private IntraProfile intraProfile;
+
+	@Builder
+	public MyAgendaProfileDetailsResDto(String userIntraId, String userContent, String userGithub,
+		Coalition userCoalition, Location userLocation, int ticketCount, IntraProfile intraProfile) {
+		this.userIntraId = userIntraId;
+		this.userContent = userContent;
+		this.userGithub = userGithub;
+		this.userCoalition = userCoalition;
+		this.userLocation = userLocation;
+		this.ticketCount = ticketCount;
+		this.intraProfile = intraProfile;
+	}
+
+	public static MyAgendaProfileDetailsResDto toDto(AgendaProfile profile, int ticketCount, IntraProfile intraProfile) {
+		return MyAgendaProfileDetailsResDto.builder()
+			.userIntraId(profile.getIntraId())
+			.userContent(profile.getContent())
+			.userGithub(profile.getGithubUrl())
+			.userCoalition(profile.getCoalition())
+			.userLocation(profile.getLocation())
+			.ticketCount(ticketCount)
+			.intraProfile(intraProfile)
+			.build();
+	}
+}
