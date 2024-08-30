18874954516ef34e46aa13922a2d33ce51f42a62
yhames <wjddn494@naver.com>
2024-08-30 14:45:54 +0900

    fix: unit test

--- gg-repo/src/main/java/gg/repo/agenda/AgendaRepository.java
+++ gg-repo/src/main/java/gg/repo/agenda/AgendaRepository.java
@@ -20,8 +20,6 @@ public interface AgendaRepository extends JpaRepository<Agenda, Long> {
 	@Query("SELECT a FROM Agenda a WHERE a.status = :status")
 	List<Agenda> findAllByStatusIs(AgendaStatus status);
 
-	Page<Agenda> findAllByStatusIs(AgendaStatus status, Pageable pageable);
-
 	@Query("SELECT a FROM Agenda a WHERE a.status = :status1 OR a.status = :status2")
 	Page<Agenda> findAllByStatusIs(AgendaStatus status1, AgendaStatus status2, Pageable pageable);
 
