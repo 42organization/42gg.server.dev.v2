1c2323b08dfce8e85828299fb1876f24d52e8fa1
seykim <kksy0917@gmail.com>
2025-03-14 14:53:56 +0900

    🔨 [Refactoring] 안쓰는 패키지 및 파일 삭제

--- gg-calendar-api/src/main/java/gg/calendar/api/admin/custom/controller/CalendarCustomAdminController.java
+++
@@ -1,4 +0,0 @@
-package gg.calendar.api.admin.custom.controller;
-
-public class CalendarCustomAdminController {
-}
--- gg-calendar-api/src/main/java/gg/calendar/api/admin/custom/controller/request/CalendarCustomAdminReqDto.java
+++
@@ -1,4 +0,0 @@
-package gg.calendar.api.admin.custom.controller.request;
-
-public class CalendarCustomAdminReqDto {
-}
