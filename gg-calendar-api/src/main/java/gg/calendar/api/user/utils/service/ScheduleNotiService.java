4e7ca24f3b3eb6b6a1057eb91e5bb836d4aa11fc
seykim <kksy0917@gmail.com>
2025-04-03 21:38:57 +0900

    🔨 [Refactoring] ScheduleNotiService에서 메시지 수정

--- gg-calendar-api/src/main/java/gg/calendar/api/user/utils/service/ScheduleNotiService.java
+++ gg-calendar-api/src/main/java/gg/calendar/api/user/utils/service/ScheduleNotiService.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import gg.data.calendar.PrivateSchedule;
-import gg.data.calendar.type.DetailClassification;
 import gg.data.calendar.type.ScheduleStatus;
 import gg.repo.calendar.PrivateScheduleRepository;
 import gg.repo.calendar.PublicScheduleRepository;
@@ -21,10 +20,10 @@ public class ScheduleNotiService {
 	private final PublicScheduleRepository publicScheduleRepository;
 	private final PrivateScheduleRepository privateScheduleRepository;
 	private final MessageSender messageSender;
-	private static final String SCHEDULE_MESSAGE_D_DAY = "📆일정요정🧚으로부터 알림이 도착했습니다.\n"
-		+ "📧42gg캘린더📧와 <<오늘>>의 일정을 확인해보세요!\n";
-	private static final String SCHEDULE_MESSAGE_BEFORE_D_DAY = "📅일정요정🧚으로부터 알림이 도착했습니다.\n\n"
-		+ "📧42gg캘린더📧와 >>내일<<의 일정을 확인해보세요!\n";
+	private static final String SCHEDULE_MESSAGE_D_DAY =
+		"📆일정요정🧚으로부터 알림이 도착했습니다.\n" + "📧42gg캘린더📧와 <<오늘>>의 일정을 확인해보세요!\n";
+	private static final String SCHEDULE_MESSAGE_BEFORE_D_DAY =
+		"📅일정요정🧚으로부터 알림이 도착했습니다.\n\n" + "📧42gg캘린더📧와 >>내일<<의 일정을 확인해보세요!\n";
 
 	@Transactional
 	public void sendScheduleNotifications() {
@@ -37,19 +36,18 @@ public class ScheduleNotiService {
 		List<PrivateSchedule> alarmSchedule = privateScheduleRepository.findSchedulesWithAlarmForBothDays(startOfDay,
 			endOfDay, startDday, endDday, ScheduleStatus.ACTIVATE);
 		for (PrivateSchedule schedule : alarmSchedule) {
-			String message = schedule.getPublicSchedule().getStartTime()
-				.isBefore(currentTime.plusDays(1))
-				? SCHEDULE_MESSAGE_D_DAY : SCHEDULE_MESSAGE_BEFORE_D_DAY;
+			String message =
+				schedule.getPublicSchedule().getStartTime().isBefore(currentTime.plusDays(1)) ? SCHEDULE_MESSAGE_D_DAY :
+					SCHEDULE_MESSAGE_BEFORE_D_DAY;
 			messageSender.send(schedule.getUser().getIntraId(),
-				message + "printf('일정: " + schedule.getPublicSchedule().getTitle() + "');\n"
-					+ "System.out.println(" + makeLink(schedule) + ");\n\n");
+				message + "printf('일정: " + schedule.getPublicSchedule().getTitle() + "');\n");
 		}
 	}
 
-	public String makeLink(PrivateSchedule schedule) {
-		boolean flag = schedule.getPublicSchedule().getClassification() == DetailClassification.JOB_NOTICE;
-		return flag ? schedule.getPublicSchedule().getLink() :
-			"https://gg.42seoul.kr/calendar/detail/" + schedule.getId();
-	}
+	// public String makeLink(PrivateSchedule schedule) {
+	// 	boolean flag = schedule.getPublicSchedule().getClassification() == DetailClassification.JOB_NOTICE;
+	// 	return flag ? schedule.getPublicSchedule().getLink() :
+	// 		"https://gg.42seoul.kr/calendar/detail/" + schedule.getId();
+	// }
 }
 
